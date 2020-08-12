fb991d6da906256df066cc2f1a285cdacf4407dd
TomoeYasuda <tomoe.pierrot3@gmail.com>
2020-08-12 09:31:30 +0900

    [mod]welcomepage issue#21

--- view/vue-project/src/views/Welcome.vue
+++ view/vue-project/src/views/Welcome.vue
@@ -1,21 +1,25 @@
 <template>
-  <div>
-    <br><br><br>
-    <h1>Welcome</h1>
-    <br>
-    <div class="text-center">
-      <v-btn rounded color="#033" dark @click="onClickSignUp">Sign Up</v-btn>
-      <Signup ref="signup"/> 
-    </div>
-    <br>
-    <div class="text-center">
-      <v-btn rounded color="#033" dark @click="onClickSignIn">Sign In</v-btn>
-      <Signin ref="signin"/> 
+  <div class="background">
+     <div>
+      <br><br><br>
+      <h1>Welcome</h1>
+      <br><br>
+      <div class="text-center">
+        <v-btn depressed color="#5C6BC0" dark @click="onClickSignUp">Sign Up</v-btn>
+        <Signup ref="signup"/> 
+      </div>
+      <br>
+      <div class="text-center">
+        <v-btn depressed color="#5C6BC0" dark @click="onClickSignIn">Sign In</v-btn>
+        <Signin ref="signin"/> 
+        <br>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import AssetsImage from "../assets/welcome.png"
 import Signup from '../components/SignUp.vue'
 import Signin from '../components/SignIn.vue'
 export default {
@@ -39,10 +43,16 @@ export default {
 h1 {
   text-align: center;
   font-family: "Monotype Corsiva";
-  color: #008b8b;
-  font-size: 80px;
+  color: #5E35B1;
+  font-size: 125px;
+}
+.background{
+ background-image: url("~@/assets/welcome.png");
+ background-size: contain;
+ background-position: center center;
 }
 .text-center {
   text-align: center;
 }
+
 </style>
