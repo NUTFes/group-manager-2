1155257b042b045dee6bc132e391b4e0747bd471
mashita1023 <coloring45@gmail.com>
2020-09-06 19:21:54 +0900

    [mod] text change japanese

--- view/vue-project/src/components/SignIn.vue
+++ view/vue-project/src/components/SignIn.vue
@@ -3,18 +3,18 @@
     <v-dialog v-model="show" max-width="600px">
       <v-card>
         <v-card-title>
-          <span class="headline">Sign In</span>
+          <span class="headline">ログイン</span>
         </v-card-title>
         <v-card-text>
           <v-container>
-            <v-text-field label="Email" v-model="email" required></v-text-field>
-            <v-text-field label="Password" v-model="password" type="password" required></v-text-field>
+            <v-text-field label="メールアドレス" v-model="email" required></v-text-field>
+            <v-text-field label="パスワード" v-model="password" type="password" required></v-text-field>
           </v-container>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="show = false">Close</v-btn>
-          <v-btn color="blue darken-1" text @click="signIn">Sign In</v-btn>
+          <v-btn color="blue darken-1" text @click="show = false">キャンセル</v-btn>
+          <v-btn color="blue darken-1" text @click="signIn">ログイン</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
