34592a4ef235ddfb8c8c6f5c0c6d216cd1615135
TomoeYasuda <tomoe.pierrot3@gmail.com>
2020-10-13 01:26:41 +0900

    [add]ログの作成(#67)]

--- view/vue-project/src/components/SignUp.vue
+++ view/vue-project/src/components/SignUp.vue
@@ -135,7 +135,7 @@ export default {
           localStorage.setItem('client', response.headers['client'])
           localStorage.setItem('uid', response.headers['uid'])
           localStorage.setItem('token-type', response.headers['token-type'])
-          this.$router.push('MyPage')
+          this.$router.push('register_user_detail')
         },
         (error) => {
           this.message = '登録に失敗しました。<br>Failed to SignUp'
