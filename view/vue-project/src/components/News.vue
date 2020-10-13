460cb35dc506cb69ea2e779df834bc348fb5818a
Kugue99A <hisabisa99yohey@gmail.com>
2020-10-13 22:41:47 +0900

    [add] assets componemts {#03}

---
+++ view/vue-project/src/components/News.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-row>
+    <v-col cols="1"></v-col>
+    <v-col cols="10">
+      <v-card
+        class = "mx-auto"
+        color = "white"
+        height = "310"
+      >
+      <v-row 
+        align="center"
+        justify="space-around"
+      >
+        <v-col cols = "2"><v-card-title class = "title font-weight-bold">お知らせ</v-card-title></v-col>
+        <v-col cols = "8"></v-col>
+        <v-col cols = "2" align = "right">    
+          <v-btn
+            class="button font-weight-midium"
+            text
+            color="pink lighten-2"
+          >
+          ▶︎more
+          </v-btn>
+        </v-col>
+        </v-row>
+        <v-divider class="mx-4"></v-divider>
+        <v-list>
+          <v-list-item 
+          v-for = "item in items"
+          :key="item.title"
+          link
+          >
+            <v-list-item-content class = "font-weight-medium">
+              <v-col cols = "2">{{item.data}}</v-col>
+              <v-col cols = "8"><v-list-item-title>{{ item.title }}</v-list-item-title></v-col>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-col>
+    <v-col cols="1"></v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { data: "2020.09.20", title: "第３回参加団体説明会のお知らせ"},
+          { data: "2020.09.20", title: "マイページ更新のお知らせ"},
+          { data: "2020.09.20", title: "第２回参加団体説明会の配布資料"},
+        ],
+        right: null,
+      }
+    },
+  }
+</script>
---
+++ view/vue-project/src/components/UserNav.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>org</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>nutfes</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' },
+        ],
+      }
+    },
+  }
+</script>
