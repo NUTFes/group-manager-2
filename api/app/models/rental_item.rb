936dd35d72ce448c01bed6d621630b4cd6216052
mizu-ryo <mukusuke421@gmail.com>
2020-11-29 12:26:47 +0900

    [mod]rental_order rental_item group relationship(#108)

--- api/app/models/rental_item.rb
+++ api/app/models/rental_item.rb
@@ -1,3 +1,4 @@
 class RentalItem < ApplicationRecord
     has_many :rental_item_allow_lists
+    has_many :rental_orders
 end
--- api/app/models/rental_order.rb
+++ api/app/models/rental_order.rb
@@ -1,2 +1,4 @@
 class RentalOrder < ApplicationRecord
+    belongs_to :group
+    belongs_to :rental_item
 end
