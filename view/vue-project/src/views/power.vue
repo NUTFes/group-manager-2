dae23fd3bd495861ed70bfcd65944b21035599c3
mashita1023 <coloring45@gmail.com>
2020-12-08 22:12:45 +0900

    add stepper

--- view/vue-project/src/views/power.vue
+++ view/vue-project/src/views/power.vue
@@ -1,190 +1,139 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="2"></v-col>
-    <v-col cols="8">
-      <v-card>
-        <v-container class="justify-content-center">
-          <v-row>
-            <v-col cols="2"></v-col>
-            <v-col cols="8" align="center">
-              <v-card-title class="justify-center">
-                <h1>電力登録</h1>
-              </v-card-title>
-              <v-card-text>
-                <v-form ref="form">
-                  <v-select
-                    label="団体"
-                    ref="group"
-                    v-model="groupId"
-                    :rules="[rules.required]"
-                    :items="group"
-                    :menu-props="{
-                      top: true,
-                      offsetY: true,
-                    }"
-                    item-text="name"
-                    item-value="id"
-                    outlined
-                  ></v-select>
-                  <v-text-field
-                    label="製品名"
-                    ref="item"
-                    v-model="item"
-                    :rules="[rules.required]"
-                    text
-                    outlined
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    label="電力量"
-                    ref="power"
-                    v-model="power"
-                    type="number"
-                    :rules="[rules.required,
-                      rules.max]"
-                    text
-                    outlined
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    label="メーカー"
-                    ref="manufacturer"
-                    v-model="manufacturer"
-                    :rules="[rules.required]"
-                    text
-                    outlined
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    label="型番"
-                    ref="model"
-                    v-model="model"
-                    :rules="[rules.required]"
-                    text
-                    outlined
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    label="製品URL"
-                    ref="itemUrl"
-                    v-model="itemUrl"
-                    :rules="[rules.required]"
-                    text
-                    outlined
-                    required
-                  ></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-action>
-                <v-btn color="blue darken-1" block @click="submit">登録</v-btn>
-                <v-btn color="blue darken-1" text block @click="cancel">リセット</v-btn>
-              </v-card-action>
-            </v-col>
-            <v-col cols="2"></v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-col>
-    <v-col cols="2"></v-col>
-  </v-row>
+  <v-card>
+    <v-row justify="space-around">
+      <v-col cols="5"></v-col>
+      <v-col cols="2">
+        <h1>電力登録</h1>
+        <v-select
+          v-model="steps"
+          :items="[1, 2, 3, 4, 5]"
+          label="登録製品数"
+        ></v-select>
+      </v-col>
+      <v-col cols="5"></v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="2"></v-col>
+      <v-col cols="8">
+        <v-stepper v-model="e1" :vertical="horizontal">
+          <template v-for="step in steps">
+            <v-stepper-step
+              :key="`${step}-step`"
+              :complete="e1 > step"
+              :step="step"
+              :editable="true"
+            >
+              Step {{ n }}
+            </v-stepper-step>
+            <v-stepper-content :key="`${step}-content`" :step="step">
+              <PowerCard ref="child" :key="step" />
+            </v-stepper-content>
+          </template>
+        </v-stepper>
+        <v-btn color="blue darken-1" block @click="all_submit">登録</v-btn>
+      </v-col>
+      <v-col cols="2"></v-col>
+    </v-row>
+  </v-card>
 </template>
 
 <script>
-
-import axios from 'axios'
+import axios from "axios";
+import PowerCard from "../components/PowerCard";
 export default {
-  data () {
+  components: {
+    PowerCard
+  },
+  data() {
     return {
       rules: {
-        required: value => !!value || '入力してください',
-        max: value => value <= 1000 || '大きすぎます',
+        required: value => !!value || "入力してください",
+        max: value => value <= 1000 || "大きすぎます"
       },
       group: [],
+      e1: 1,
+      steps: 2
+    };
+  },
+  watch: {
+    steps(val) {
+      if (this.e1 > val) this.e1 = val;
     }
-    },
-    computed: {
-    form () {
+  },
+  computed: {
+    form() {
       return {
-        item: null,
-        power: null,
-        manufacturer: null,
-        model: null,
-        itemUrl: null,
-        groupId: null
-      }
-    },
+        item: [],
+        power: [],
+        manufacturer: [],
+        model: [],
+        itemUrl: [],
+        groupId: []
+      };
+    }
   },
   methods: {
-    cancel: function() {
-      this.$refs.form.reset();
+    onInput: val => {
+      this.steps = parseInt(val);
     },
-    submit: function() {
-      if ( !this.$refs.form.validate() ) return;
-
-      const url = process.env.VUE_APP_URL + '/power_orders'
-      let params = new URLSearchParams();
-      params.append('group_id', this.groupId);
-      params.append('item', this.item);
-      params.append('power', this.power);
-      params.append('manufacturer', this.manufacturer);
-      params.append('model', this.model);
-      params.append('item_url', this.itemUrl);
-
-      axios.defaults.headers.common['Content-Type'] = 'application/json';
-      axios.post(url, params).then(
-        (response) => {
-          console.log('response:', response)
-          this.$router.push('MyPage')
-        },
-        (error) => {
-          console.log('登録できませんでした')
-          return error;
-        }
-      )
+    nextStep: n => {
+      if (n === this.steps) {
+        this.e1 = 1;
+      } else {
+        this.e1 = n + 1;
+      }
+    },
+    all_submit: function() {
+      for (let i = 0; i < this.steps; i++) {
+        this.$refs.child[i].submit();
+      }
     },
   },
 
   mounted() {
-    const url = process.env.VUE_APP_URL + '/api/v1/users/show'
-    axios.get(url, {
-      headers: {
-        "Content-Type": "application/json",
-        "access-token": localStorage.getItem('access-token'),
-        "client": localStorage.getItem('client'),
-        "uid": localStorage.getItem('uid')
-      }
-    }).then(
-      (response) => {
-        this.user = response.data.data
-        console.log(this.user)
-        console.log(this.user.id)
-      },
-      (error) => {
-        console.error(error)
-        return error;
-      }
-    )
-    const groupUrl = process.env.VUE_APP_URL + '/api/v1/current_user/groups'
-    axios.get(groupUrl, {
-      headers: {
-        "Content-Type": "application/json",
-        "access-token": localStorage.getItem('access-token'),
-        "client": localStorage.getItem('client'),
-        "uid": localStorage.getItem('uid')
-      }
-    }).then(
-      (response) => {
-        for(let i=0;i<response.data.length;i++){
-          this.group.push(response.data[i])
+    const url = process.env.VUE_APP_URL + "/api/v1/users/show";
+    axios
+      .get(url, {
+        headers: {
+          "Content-Type": "application/json",
+          "access-token": localStorage.getItem("access-token"),
+          client: localStorage.getItem("client"),
+          uid: localStorage.getItem("uid")
         }
-        console.log(this.group)
-      },
-      (error) => {
-        console.error(error)
-        return error;
-      }
-    )
-  },
-}
-
+      })
+      .then(
+        response => {
+          this.user = response.data.data;
+          console.log(this.user);
+          console.log(this.user.id);
+        },
+        error => {
+          console.error(error);
+          return error;
+        }
+      );
+    const groupUrl = process.env.VUE_APP_URL + "/api/v1/current_user/groups";
+    axios
+      .get(groupUrl, {
+        headers: {
+          "Content-Type": "application/json",
+          "access-token": localStorage.getItem("access-token"),
+          client: localStorage.getItem("client"),
+          uid: localStorage.getItem("uid")
+        }
+      })
+      .then(
+        response => {
+          for (let i = 0; i < response.data.length; i++) {
+            this.group.push(response.data[i]);
+          }
+          console.log(this.group);
+        },
+        error => {
+          console.error(error);
+          return error;
+        }
+      );
+  }
+};
 </script>
