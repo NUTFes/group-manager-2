e0de96b84b40bcd17cba287b43e5459c658edb3c
Kugue99A <hisabisa99yohey@gmail.com>
2021-01-13 15:06:19 +0900

    [mod] redesign user tab

--- admin_view/nuxt-project/pages/users/_id.vue
+++ admin_view/nuxt-project/pages/users/_id.vue
@@ -1,24 +1,23 @@
 <template>
   <div>
     <Header/>
-      <v-container>
-        <v-card
-          class="mx-auto"
-        >
-          <v-container>
-            <v-row justify="end" align="center">
-              <v-col> 
-                <v-card-title class="font-weight-bold"><v-icon>mdi-account</v-icon>{{ user.name }}</v-card-title>
-                <div v-if="user.role_id == 1"><v-chip color="red" text-color="white" x-small class="mt-n8 mx-4">developer</v-chip></div>
-                <div v-if="user.role_id == 2"><v-chip color="green" text-color="white" x-small class="mt-n8 mx-4">manager</v-chip></div>
-                <div v-if="user.role_id == 3"><v-chip color="blue" text-color="white" x-small class="mt-n8 mx-4">user</v-chip></div>
-             </v-col>
-              <v-col align="right">
+      <v-row>
+      <v-col cols="1"></v-col>
+      <v-col cols="10">
+        <v-card>
+          <v-row>
+            <v-col cols="1"></v-col>
+            <v-col cols="10"> 
+              <v-card-title class="font-weight-bold mt-3">
+                <v-icon v-if="user.role_id == 1" color="red" class="ma-1">mdi-account-cog</v-icon>
+                <v-icon v-if="user.role_id == 2" color="green">mdi-account-tie</v-icon>
+                <v-icon v-if="user.role_id == 3" color="blue">mdi-account</v-icon>
+                {{ user.name }}
+                <v-spacer></v-spacer>
                 <v-btn text @click="dialog = true"><v-icon class="ma-5" color="#E040FB">mdi-pencil</v-icon></v-btn>
-              </v-col>
-            </v-row>
-            <v-divider></v-divider>
-             <v-simple-table>
+              </v-card-title>
+              <hr class="mt-n3">
+              <v-simple-table class="my-9">
               <template v-slot:default>
                 <tbody>
                   <tr>
@@ -34,6 +33,10 @@
                     <td class="caption">{{ department }}</td>
                   </tr>
                   <tr>
+                    <th>所属団体：</th>
+                    <td class="caption">{{ department }}</td>
+                  </tr>
+                  <tr>
                     <th>電話番号：</th>
                     <td class="caption">{{ detail.tel }}</td>
                   </tr>
@@ -44,24 +47,87 @@
                   <tr>
                     <th>編集日時：</th>
                     <td class="caption">{{ user.updated_at | format-date }}</td>
-                    <td v-if="currentUserRole == 1"><v-icon color="#E91E63">mdi-pencil</v-icon></td>
-                    <td v-if="currentUserRole == 2"><v-icon color="#E91E63">mdi-eye</v-icon></td>
+                    <td v-if="rights == 1"><v-icon color="#E91E63">mdi-pencil</v-icon></td>
+                    <td v-if="rights == 2"><v-icon color="#E91E63">mdi-eye</v-icon></td>
                 	</tr>
                 </tbody>
               </template>
             </v-simple-table>
-           </v-container>
-          </v-card>
-        </v-container>
-
+          </v-col>
+          <v-col cols="1"></v-col>
+        </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="1"></v-col>
+      </v-row>
         <!-- modal window to edit -->
         <v-dialog
           v-model="dialog"
+          width="1200"
         >
           <v-card>
-            <v-card-title class="font-weight-bold"><v-icon>mdi-account</v-icon>{{ user.name }}</v-card-title>
-            <v-container>
-            </v-container>
+              <v-row>
+                <v-col cols="2"></v-col>
+                <v-col cols="8">
+                <v-card-title class="font-weight-bold"><v-icon class="pa-2">mdi-pencil</v-icon>登録情報の編集</v-card-title>
+                  <v-text-field
+                    label="氏名"
+                    background-color="white"
+                    outlined
+                    v-model="student_id"
+                    filled
+                    clearable
+                  ></v-text-field>
+                  <v-select
+                    label="権限"
+                    ref="groupCategory"
+                    v-model="groupCategoryId"
+                    :menu-props="{
+                                 top: true,
+                                 offsetY: true,
+                                 }"
+                    item-text="name"
+                    item-value="id"
+                    outlined
+                  ></v-select>
+                  <v-text-field
+                    label="学籍番号８桁"
+                    background-color="white"
+                    outlined
+                    v-model="student_id"
+                    counter="8"
+                    filled
+                    clearable
+                  ></v-text-field>
+                  <v-text-field
+                    label="課程（専攻）"
+                    background-color="white"
+                    outlined
+                    v-model="student_id"
+                    filled
+                    clearable
+                  ></v-text-field>
+                  <v-text-field
+                    label="団体"
+                    background-color="white"
+                    outlined
+                    v-model="student_id"
+                    filled
+                    clearable
+                  ></v-text-field>
+                  <v-text-field
+                    label="電話番号"
+                    background-color="white"
+                    outlined
+                    v-model="student_id"
+                    filled
+                    clearable
+                  ></v-text-field>
+                  <v-btn color="blue darken-1" block dark @click="submit">登録</v-btn>
+                  <v-btn color="blue darken-1" text block @click="cancel">リセット</v-btn>
+                </v-col>
+                <v-col cols="2"></v-col>
+              </v-row>
           </v-card>
         </v-dialog>
     </div>
@@ -70,11 +136,17 @@
   <script>
   import Header from '~/components/Header.vue'
   import axios from 'axios'
-  import { mapAction, mapGetters } from 'vuex'
-
+  import { mapState } from 'vuex'
+  
   export default {
     components: {
       Header,
+    },
+    fetch({ store }) {
+      store.dispatch('getRights')
+    },
+    computed: {
+      ...mapState(['rights'])
     },
     data() {
       return {
@@ -84,18 +156,7 @@
         department: [],
         detail: [],
         expand: false,
-        currentUserRole: [],
         dialog: false,
-      }
-    },
-    computed: {
-      currentUserRole() {
-        return this.$store.state.currentUserRole;
-      }
-    },
-    methods: {
-      getCurrentUserRole() {
-        this.$store.dispatch("getCurrentUserRoleAction");
       }
     },
     mounted() {
@@ -113,7 +174,7 @@
         this.department = response.data.department
         this.detail = response.data.detail
       })
-  }
+    }
 }
 </script>
   
--- admin_view/nuxt-project/pages/users/index.vue
+++ admin_view/nuxt-project/pages/users/index.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
     <Header/>
-      <v-container>
+        <v-row>
+        <v-col cols="1"></v-col>
+        <v-col cols="10">
         <v-card>
-          <v-container>
-            <v-row>
-              <v-col>
-                <v-card-title><v-icon>mdi-account-multiple</v-icon>ユーザー一覧</v-card-title>
-              </v-col>
-            </v-row>
-            <v-divider></v-divider>
-            <br>
-            <v-row>
-              <v-col>
-                <template>
-                  <v-simple-table>
+          <v-row>
+            <v-col cols="1"></v-col>
+            <v-col cols="10">
+            <v-card-title class="font-weight-bold mt-3"><v-icon>mdi-account-multiple</v-icon>ユーザー一覧</v-card-title>
+            <hr class="mt-n3">
+
+              <!-- Comment
+  						<v-data-table
+                :headers="headers"
+                :items="users"
+                :items-per-page="5"
+                class="elevation-1"
+              ></v-data-table>
+              -->
+              <template>
+                  <v-simple-table class="my-9">
                     <template v-slot:default>
                       <thead>
                         <tr>
@@ -38,7 +44,6 @@
                           </th>
                           <th class="text-center">
                           <th v-for="(header, i) in headers" :key="i" class="text-center">
-                            {{ header.text }}
                           </th>
                         </tr>
                       </thead>
@@ -52,31 +57,32 @@
                           <td>{{ user.id }}</td>
                           <td>{{ user.name }}</td>
                           <td>{{ user.email }}</td>
-                          <td v-if="user.role_id == 1"><v-chip color="red" text-color="white" small>developer</v-chip></td>
-                          <td v-if="user.role_id == 2"><v-chip color="green" text-color="white" small>manager</v-chip></td>
-                          <td v-if="user.role_id == 3"><v-chip color="blue" text-color="white" small>user</v-chip></td>
+                          <td v-if="user.role_id == 1"><v-chip color="red" text-color="white" small><v-icon class="mr-1">mdi-account-cog</v-icon>developer</v-chip></td>
+                          <td v-if="user.role_id == 2"><v-chip color="green" text-color="white" small><v-icon class="mr-1">mdi-account-tie</v-icon>manager</v-chip></td>
+                          <td v-if="user.role_id == 3"><v-chip color="blue" text-color="white" small><v-icon class="mr-1">mdi-account</v-icon>user</v-chip></td>
                           <td>{{ user.created_at | format-date }}</td>
                           <td>{{ user.updated_at | format-date}}</td>
                           <td><v-icon color="#E91E63">mdi-eye</v-icon></td>
-                          <td v-if="currentUserRole == 1"><v-icon color="#E91E63">mdi-pencil</v-icon></td>
-                          <td v-if="currentUserRole == 2"><v-icon color="#E91E63">mdi-eye</v-icon></td>
+                          <td v-if="rights == 1"><v-icon color="#E91E63">mdi-pencil</v-icon></td>
+                          <td v-if="rights == 2"><v-icon color="#E91E63">mdi-eye</v-icon></td>
                         </tr>
                       </tbody>
                     </template>
                   </v-simple-table>
                 </template>
               </v-col>
-            </v-row>
-          </v-container>
+            <v-col cols="1"></v-col>
+        </v-row>
         </v-card>
-      </v-container>
+        </v-col>
+        <v-col cols="1"></v-col>
+        </v-row>
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
 import axios from 'axios'
-import { mapAction,  mapMutation } from 'vuex'
 
 export default {
   components: {
@@ -85,20 +91,17 @@ export default {
   data() {
     return {
       users: [],
-      currentUserRole: [],
+     /* headers:[
+        { text: 'id', value: 'id' },
+        { text: '名前', value: 'name' },
+        { text: 'メールアドレス', value: 'email' },
+        { text: '権限', value: 'role_id' },
+        { text: '日時', value: 'created_at' },
+        { text: '編集日時', value: 'updated_at' },
+      ],*/
     }
   },
-  computed: {
-    currentUserRole() {
-      return this.$store.state.currentUserRole;
-    }
-  },
-  methods: {
-    getCurrentUserRole() {
-      this.$store.dispatch("getCurrentUserRoleAction");
-    },
-  },
- mounted() {
+  mounted() {
     this.$axios.get('api/v1/users/index', {
       headers: { 
         "Content-Type": "application/json", 
