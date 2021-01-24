141468c66454bf8d233530699ad994eea70ef4a5
oba18 <m.oba.0426@gmail.com>
2021-01-25 08:30:34 +0900

    [mod] 印刷プレビューページの追加 (#167)

--- admin_view/nuxt-project/pages/users/print/index.vue
+++ admin_view/nuxt-project/pages/users/print/index.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="sheets">
-    <div>
-      <Header/>
-        <v-row>
-          <v-col cols=2>
-            <Menu/>
-          </v-col>
-          <v-col cols=10>
-            <v-row>
-              <v-col>
-                <v-card-text><router-link to="/users">ユーザー一覧</router-link> > 印刷</v-card-text>
-                <v-btn rounded height="50" width="200" color="primary" @click="handlePrint"><v-icon class="mr-3">mdi-printer</v-icon>印刷する</v-btn>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-    </div>
+    <v-row>
+      <v-col cols=1></v-col>
+      <v-col cols=11>
+        <v-card-text><router-link to="/users">ユーザー一覧</router-link> > 印刷</v-card-text>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols=1></v-col>
+      <v-col cols=10>
+        <h1>印刷プレビュー</h1>
+      </v-col>
+      <v-col cols=1></v-col>
+    </v-row>
+    <v-row>
+      <v-col cols=1></v-col>
+      <v-col cols=11>
+        <v-btn rounded color="blue" height="50" width="200" to="/users"><v-icon class="mr-3" style="color:white">mdi-arrow-left-bold</v-icon><div style="color:white">管理者ページに戻る</div></v-btn>
+        <v-btn rounded height="50" width="200" color="primary" @click="handlePrint"><v-icon class="mr-3">mdi-printer</v-icon>印刷する</v-btn>
+      </v-col>
+    </v-row>
 
     <div class="sheet">
       <h2>ユーザー一覧</h2>
@@ -40,15 +44,12 @@
       </v-simple-table>
     </div>
 
-    <div>
-      <Header/>
-        <v-row>
-          <v-col cols=2></v-col>
-          <v-col cols=10>
-            <v-btn text color="white" to="/users"><v-icon color="#333333">mdi-arrow-left-bold</v-icon><div style="color:#333333">ユーザー一覧に戻る</div></v-btn>
-          </v-col>
-        </v-row>
-    </div>
+    <v-row>
+      <v-col cols=1></v-col>
+      <v-col cols=10>
+        <v-btn text color="white" to="/users"><v-icon color="#333333">mdi-arrow-left-bold</v-icon><div style="color:#333333">ユーザー一覧に戻る</div></v-btn>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -69,6 +70,7 @@ export default {
   },
   mounted() {
     this.getList()
+    setTimeout(this.handlePrint, 100);
   },
   methods: {
     getList() {
@@ -89,13 +91,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+h1 {
+  border-bottom: solid 3px black;
+}
 .sheet {
   page-break-after: always;
 }
 
 /* hide in print */
 @media print {
-  .sheets > :not(.sheet) {
+  .sheets > :not(.sheet){
     display: none;
   }
 }
@@ -110,7 +115,7 @@ export default {
     padding: 5mm;
     background: white;
     box-shadow: 0 .5mm 2mm rgba(0,0,0,.3);
-    margin-left: 320px;
+    margin-left: 8%;
   }
 }
 </style>
