18310111ff33272177b2a5724132dfad41cbd6a1
sasadai <d.sasaki.nutfes@gmail.com>
2021-02-07 20:30:28 +0900

    [mod] WelcomeDetail.vue (#81)

--- view/vue-project/src/components/WelcomeDetail.vue
+++ view/vue-project/src/components/WelcomeDetail.vue
@@ -1,65 +1,146 @@
 <template>
-  <div>
+  <div style='background-color:rgb(237,237,232)'>
     <v-conteiner>
-      <v-row>
-        <v-col cols="2"></v-col>
-        <v-col cols="1" class="d-flex justify-end" >
-          <v-img contain  src="@/assets/40th_nutfes_logo_black.png" width="150" height="150"></v-img>
-        </v-col>
-        <v-col cols="9" class="d-flex align-center" >
-            <h1>技大祭に出展しませんか？？</h1>
-        </v-col>
-      </v-row>
-      <v-row>
+     <v-row>
       
-      </v-row>
-      <v-row>
-        <v-col cols="2"></v-col>
-        <v-col cols="8">
-          <v-row>
-          <hr width='100%' style='background-color:#aaa'>
-          </v-row>
-          <v-row>
-            <v-col cols="6">
-              <h1 style='text-align:left'>模擬店</h1>
-              <v-img src="../assets/mogiten.jpg"></v-img>
-              <p>模擬店の紹介</p>
-            </v-col>
-            <v-col cols="6">
-              <h1 >研究室紹介</h1>
-              <v-img src="../assets/lab2.jpg"></v-img>
-              <p>研究室の紹介</p>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="6">
-              <v-img src="../assets/stage.jpg"></v-img>
-            </v-col>
-            <v-col cols="6">
-              <h1 style='text-align:left'>ステージ企画</h1>
-              <p>ステージ企画の紹介</p>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="6">
-              <h1>展示・体験</h1>
-              <p>展示・体験の紹介</p>
-            </v-col>
-            <v-col cols="6">
-              <v-img src="../assets/taiken1.jpg"></v-img>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="6">
-              
-            </v-col>
-            <v-col cols="6">
+      
+      <v-col cols="2.5"></v-col>
 
+      <v-col cols="7">
+    
+        <v-row>
+
+          <v-row>
+            <v-col cols="2"></v-col>
+            <v-col cols="1" class="d-flex justify-end" >
+              <v-img contain  src="@/assets/40th_nutfes_logo_black.png" width="150" height="150"></v-img>
+            </v-col>
+            <v-col cols="9" class="d-flex align-center" >
+              <span class='welcometitle'>技大祭に出展しませんか？</span>
             </v-col>
           </v-row>
-        </v-col>
-        <v-col cols="2"></v-col>
-      </v-row>
+
+
+          <hr width='100%' style='background-color:#aaa'>
+        </v-row>
+
+<v-col cols="12"></v-col>
+
+        <v-row class='welcomerow'>
+          <v-col cols='7' class='welcomecol'>
+            <v-img src="../assets/mogiten.jpg"  height='500'></v-img>
+          </v-col>
+
+          <v-col cols='5'class='welcomecol'>
+          <v-row>
+              <v-col cols='2'></v-col>
+              <v-col cols='8'>
+                  <v-row>
+                    <v-col cols='12' class='welcometitleback'>
+                     <span class='welcometitle'>模擬店</span>
+                    </v-col>
+                    <v-col cold='12'class='welcomesentence'>
+                      <p>hoge</p>
+                    </v-col>  
+                  </v-row>
+              </v-col>
+              <v-col cols='2'></v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
+
+        <v-row class='welcomerow'>
+
+          <v-col cols='7' class='welcomecol'>
+            <v-img src="../assets/stage.jpg"  height='500'></v-img>
+          </v-col>
+
+          <v-col cols='5'class='welcomecol'>
+          <v-row>
+              <v-col cols='2'></v-col>
+              <v-col cols='8'>
+                  <v-row>
+                    <v-col cols='12' class='welcometitleback'>
+                     <span class='welcometitle'>ステージ企画</span>
+                    </v-col>
+                    <v-col cold='12'class='welcomesentence'>
+                      <p>hoge</p>
+                    </v-col>  
+                  </v-row>
+              </v-col>
+              <v-col cols='2'></v-col>
+            </v-row>
+          </v-col>
+
+        </v-row>
+
+
+
+        <v-row class='welcomerow'>
+
+          <v-col cols='7' class='welcomecol'>
+            <v-img src="../assets/taiken1.jpg"  height='500'></v-img>
+          </v-col>
+
+          <v-col cols='5'class='welcomecol'>
+          <v-row>
+              <v-col cols='2'></v-col>
+              <v-col cols='8'>
+                  <v-row>
+                    <v-col cols='12' class='welcometitleback'>
+                     <span class='welcometitle'>展示・体験</span>
+                    </v-col>
+                    <v-col cold='12'class='welcomesentence'>
+                      <p>hoge</p>
+                    </v-col>  
+                  </v-row>
+              </v-col>
+              <v-col cols='2'></v-col>
+            </v-row>
+          </v-col>
+
+        </v-row>
+
+
+
+        <v-row class='welcomerow'>
+
+          <v-col cols='7' class='welcomecol'>
+            <v-img src="../assets/lab2.jpg"  height='500'></v-img>
+          </v-col>
+
+          <v-col cols='5'class='welcomecol'>
+          <v-row>
+              <v-col cols='2'></v-col>
+              <v-col cols='8'>
+                  <v-row>
+                    <v-col cols='12' class='welcometitleback'>
+                     <span class='welcometitle'>研究室公開</span>
+                    </v-col>
+                    <v-col cold='12'class='welcomesentence'>
+                      <p>hoge</p>
+                    </v-col>  
+                  </v-row>
+              </v-col>
+              <v-col cols='2'></v-col>
+            </v-row>
+          </v-col>
+
+        </v-row>
+
+
+
+
+
+
+    </v-col>
+
+    <v-col cols="2.5"></v-col>
+    </v-row>
+
+
+
     </v-conteiner>
   </div>
 </template>
@@ -74,8 +155,46 @@ export default {
 </script>
 
 <style>
-h1
-{
-  text-align:left;
+
+.welcomecol{
+background:#fff;
+padding:0;
+margin:0;
 }
+
+.welcometitleback
+{
+  height:260px;
+  margin:40px 0 0;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  border-top:groove;
+}
+.welcomesentence{
+  height:136px;
+  margin:0 0 40px;
+  border-bottom:groove;
+
+}
+
+.welcometitle{
+  text-align:left;
+  font-size:25pt;
+ 
+}
+
+.welcomeback
+{
+background-color:#dcedc8;
+height:500px;
+}
+
+
+.welcomerow{
+
+margin:150px 0;
+
+}
+
 </style>
