028e0cd9e81edfb9338780080234421b7bfe0b4c
oba18 <m.oba.0426@gmail.com>
2021-02-27 22:21:55 +0900

    [add] 副代表の編集機能の追加(#259)

---
+++ admin_view/nuxt-project/components/NoData.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>
+        <div class="card">
+          <v-card flat>
+            <v-row>
+              <v-col cols=1></v-col>
+              <v-col cols=10>
+                <v-card-title>
+                  データが取得できませんでした
+                </v-card-title>
+              </v-col>
+              <v-col cols=1></v-col>
+            </v-row>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
