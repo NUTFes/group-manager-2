78201fbc31f036e27317a9f28ea6eff6dbcf79b6
sasadai <d.sasaki.nutfes@gmail.com>
2021-03-05 15:17:50 +0900

    [mod] ステージオプション申請の編集をできるようにした 264

--- admin_view/nuxt-project/pages/stage_common_options/_id.vue
+++ admin_view/nuxt-project/pages/stage_common_options/_id.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div v-if="stage_common_option.length===0">
+          <NoData/>
+    </div>
+    <div v-else>
     <v-row>
       <v-col>
         <div class="card">
@@ -21,10 +25,35 @@
         <v-card flat>
           <v-row>
             <v-col cols="1"></v-col>
-            <v-col cols="10"> 
+            <v-col cols="10">
               <v-card-title class="font-weight-bold mt-3">{{ group }}
                 <v-spacer></v-spacer>
-                <v-btn text @click="dialog = true"><v-icon class="ma-5" color="#E040FB">mdi-pencil</v-icon></v-btn>
+                <v-tooltip top>
+                  <template v-slot:activator="{ on, attrs  }">
+                      <v-btn 
+                      text 
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="edit_dialog_open" 
+                      fab>
+                      <v-icon class="ma-5">mdi-pencil</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>編集</span>
+                </v-tooltip>
+                <v-tooltip top>
+                  <template v-slot:activator="{ on, attrs  }">
+                    <v-btn 
+                      text 
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="delete_dialog = true" 
+                      fab>
+                      <v-icon class="ma-5">mdi-delete</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>削除</span>
+                </v-tooltip>
               </v-card-title>
               <hr class="mt-n3">
               <v-simple-table class="my-9">
@@ -35,7 +64,7 @@
                       <td class="caption">{{ stage_common_option.id }}</td>
                     </tr>
                     <tr>
-                      <th>所持機器の仕様：</th>
+                      <th>所持機器の使用：</th>
                       <td>
                     <v-chip v-if="stage_common_option.own_equipment == true" color="red" text-color="white" small>使用</v-chip>
                     <v-chip v-if="stage_common_option.own_equipment == false" color="blue" text-color="white" small>使用しない</v-chip>
@@ -93,92 +122,209 @@
       </v-col>
     </v-row>
 
-    <!-- modal window to edit -->
+<!-- 編集ダイアログ -->
+
     <v-dialog
-      v-model="dialog"
-      width="1200"
+      v-model="edit_dialog"
+      width="500"
       >
       <v-card>
+        <v-card-title class="headline blue-grey darken-3">
+          <div style="color: white">
+            <v-icon class="ma-5" dark>mdi-pencil</v-icon>編集
+          </div>
+          <v-spacer></v-spacer>
+          <v-btn text @click="edit_dialog = false" fab dark>
+            ​ <v-icon>mdi-close</v-icon>
+          </v-btn>
+      </v-card-title>
+
+      <v-card-text>
         <v-row>
-          <v-col cols="2"></v-col>
-          <v-col cols="8">
-            <v-card-title class="font-weight-bold"><v-icon class="pa-2">mdi-pencil</v-icon>登録情報の編集</v-card-title>
-            <v-text-field
-              label="氏名"
-              background-color="white"
-              outlined
-              v-model="student_id"
-              filled
-              clearable
-              ></v-text-field>
-            <v-select
-              label="権限"
-              ref="groupCategory"
-              v-model="groupCategoryId"
-              :menu-props="{
-                             top: true,
-                             offsetY: true,
-                             }"
-              item-text="name"
-              item-value="id"
-              outlined
-              ></v-select>
-            <v-text-field
-              label="学籍番号８桁"
-              background-color="white"
-              outlined
-              v-model="student_id"
-              counter="8"
-              filled
-              clearable
-              ></v-text-field>
-            <v-text-field
-              label="課程（専攻）"
-              background-color="white"
-              outlined
-              v-model="student_id"
-              filled
-              clearable
-              ></v-text-field>
-            <v-text-field
-              label="団体"
-              background-color="white"
-              outlined
-              v-model="student_id"
-              filled
-              clearable
-              ></v-text-field>
-            <v-text-field
-              label="電話番号"
-              background-color="white"
-              outlined
-              v-model="student_id"
-              filled
-              clearable
-              ></v-text-field>
-            <v-btn color="blue darken-1" block dark @click="submit">登録</v-btn>
-            <v-btn color="blue darken-1" text block @click="cancel">リセット</v-btn>
+          <v-col>
+            <v-form ref="form">
+              <v-select
+                label="参加団体"
+                v-model="group_id"
+                :items="groups"
+                item-text="name"
+                item-value="id"
+                outlined
+                />
+              <v-select
+                label="所持機器の使用"
+                v-model="own_equipment"
+                :items="enable_items"
+                item-text="label"
+                item-value="value"
+                outlined
+                />    
+                <v-select
+                label="音楽をかける"
+                v-model="bgm"
+                :items="enable_items"
+                item-text="label"
+                item-value="value"
+                outlined
+                />    
+                <v-select
+                label="撮影許可"
+                v-model="camera_permission"
+                :items="allow_items"
+                item-text="label"
+                item-value="value"
+                outlined
+                />    
+                <v-select
+                label="大きな音"
+                v-model="loud_sound"
+                :items="sound_items"
+                item-text="label"
+                item-value="value"
+                outlined
+                />
+                <v-textarea
+                label="ステージ内容"
+                v-model="stage_content"
+                clearable
+                outlined
+                :rules="[rules.required]"
+                />    
+            </v-form>
           </v-col>
-          <v-col cols="2"></v-col>
         </v-row>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="#78909C"
+          dark
+          @click="edit"
+          >
+          編集する
+        </v-btn>
+      </v-card-actions>
       </v-card>
-    </v-dialog>
+    </v-dialog> 
+
+    <!-- 削除ダイアログ -->
+    <v-dialog
+      v-model="delete_dialog"
+      width="500"
+      >
+      <v-card>
+        <v-card-title class="headline blue-grey darken-3">
+          <div style="color: white">
+            <v-icon class="ma-5" dark>mdi-delete</v-icon>削除
+          </div>
+          <v-spacer></v-spacer>
+          <v-btn text @click="delete_dialog = false" fab dark>
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+
+      <v-card-title>
+        削除してよろしいですか？
+      </v-card-title>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          flat
+          color="red"
+          dark
+          @click="delete_yes"
+          >
+          はい
+        </v-btn>
+        <v-btn
+          flat
+          color="blue"
+          dark
+          @click="delete_dialog = false"
+          >
+          いいえ
+        </v-btn>
+      </v-card-actions>
+      </v-card>
+    </v-dialog> 
+
+    <!-- 編集成功SnackBar -->
+    <v-snackbar
+      v-model="success_snackbar"
+      color="blue-grey"
+      top
+      elevation="24"
+    >
+      編集しました
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="white"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+        <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>  
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-
+import NoData from "../../components/NoData.vue" ;
 export default {
+  components :{
+    NoData
+  },
   data() {
     return {
+      id: [],
       stage_common_option: [],
       group: [],
+      group_id: [],
+      own_equipment: [],
+      bgm: [],
+      camera_permission: [],
+      loud_sound:[],
+      stage_content: [],
       expand: false,
-      dialog: false,
+      edit_dialog: false,
+      delete_dialog: false,
+      enable_items :[
+        {label:"使用",value:true},
+        {label:"使用しない",value:false}
+        ],
+      allow_items :[
+        {label:"許可",value:true},
+        {label:"許可しない",value:false}
+        ],
+      sound_items :[
+        {label:"出す",value:true},
+        {label:"出さない",value:false}
+        ],
+         rules: {
+        required: value => !!value || '入力してください',
+      },
     }
   },
   mounted() {
+    this.$axios.get('/groups', {
+        headers: { 
+          "Content-Type": "application/json", 
+        },
+      })
+      .then(response => {
+        this.groups = response.data
+      })
     const url = "/api/v1/get_stage_common_options_with_group/" + this.$route.params.id;
     this.$axios.get(url, {
       headers: { 
@@ -187,10 +333,66 @@ export default {
     }
     )
       .then(response => {
+        this.data =response.data
         this.stage_common_option = response.data.stage_common_option
+        this.id = response.data.stage_common_option.id
         this.group = response.data.group
+        this.group_id = response.data.stage_common_option.group_id
+        this.own_equipment = response.data.stage_common_option.own_equipment
+        this.bgm = response.data.stage_common_option.bgm
+        this.loud_sound = response.data.stage_common_option.loud_sound
+        this.camera_permission = response.data.stage_common_option.camera_permission
+        this.stage_content = response.data.stage_common_option.stage_content
+        this.stage_common_option = response.data.stage_common_option
+
+
         console.log(this.group)
       })
+  },
+  methods: {
+    reload: function(){
+      console.log("reload")
+      const url = "/api/v1/get_stage_common_options_with_group/" + this.$route.params.id;
+      this.$axios.get(url, {
+        headers: { 
+          "Content-Type": "application/json", 
+        }
+      }
+      )
+        .then(response => {
+        this.data =response.data
+        this.stage_common_option = response.data.stage_common_option
+        this.id = response.data.stage_common_option.id
+        this.group = response.data.group
+        this.group_id = response.data.stage_common_option.group_id
+        this.own_equipment = response.data.stage_common_option.own_equipment
+        this.bgm = response.data.stage_common_option.bgm
+        this.loud_sound = response.data.stage_common_option.loud_sound
+        this.camera_permission = response.data.stage_common_option.camera_permission
+        this.stage_content = response.data.stage_common_option.stage_content
+        this.stage_common_option = response.data.stage_common_option
+        })
+    },
+    edit_dialog_open: function() {
+      this.edit_dialog = true
+    },
+    edit: function() {
+      const edit_url = '/stage_common_options/' + this.id + '?group_id=' + this.group_id + '&own_equipment=' + this.own_equipment + '&bgm=' + this.bgm + '&camera_permission=' + this.camera_permission + '&loud_sound=' + this.loud_sound +'&stage_content=' + this.stage_content 
+      console.log(edit_url)
+      this.$axios.put(edit_url , {
+        headers: { 
+          "Content-Type": "application/json", 
+        }
+      }).then(response => {
+        console.log(response)
+        this.reload()
+        this.edit_dialog = false
+        this.success_snackbar = true
+      })
+    },
+    delete_yes: function() {
+      this.$router.push('/stage_common_options')
+    }
   }
 }
 </script>
