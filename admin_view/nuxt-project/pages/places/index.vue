554a53861d1b896888ab088fe1698347a9e37d26
oba18 <m.oba.0426@gmail.com>
2021-03-05 19:28:02 +0900

    [fix] インデントの修正 (#341)

--- admin_view/nuxt-project/pages/places/index.vue
+++ admin_view/nuxt-project/pages/places/index.vue
@@ -12,14 +12,14 @@
                 <v-tooltip top>
                   <template v-slot:activator="{ on, attrs  }">
                     <v-btn 
-                            class="mx-2" 
-                            fab 
-                            text
-                            v-bind="attrs"
-                            v-on="on"
-                            @click="dialog=true"
-                            >
-                            <v-icon dark>mdi-plus-circle-outline</v-icon>
+                      class="mx-2" 
+                      fab 
+                      text
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="dialog=true"
+                      >
+                      <v-icon dark>mdi-plus-circle-outline</v-icon>
                     </v-btn>
                   </template>
                   <span>会場の追加</span>
@@ -27,14 +27,14 @@
                 <v-tooltip top>
                   <template v-slot:activator="{ on, attrs  }">
                     <v-btn 
-                            class="mx-2" 
-                            fab 
-                            text
-                            v-bind="attrs"
-                            v-on="on"
-                            @click="reload"
-                            >
-                            <v-icon dark>mdi-reload</v-icon>
+                      class="mx-2" 
+                      fab 
+                      text
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="reload"
+                      >
+                      <v-icon dark>mdi-reload</v-icon>
                     </v-btn>
                   </template>
                   <span>更新する</span>
@@ -42,65 +42,65 @@
                 <v-tooltip top>
                   <template v-slot:activator="{ on, attrs  }">
                     <v-btn 
-                            class="mx-2" 
-                            fab 
-                            text
-                            v-bind="attrs"
-                            v-on="on"
-                            to="/users/print"
-                            >
-                            <v-icon dark>mdi-printer</v-icon>
+                      class="mx-2" 
+                      fab 
+                      text
+                      v-bind="attrs"
+                      v-on="on"
+                      to="/users/print"
+                      >
+                      <v-icon dark>mdi-printer</v-icon>
                     </v-btn>
                   </template>
                   <span>印刷する</span>
                 </v-tooltip>
               </v-card-title>
-            <v-dialog
-              v-model="dialog"
-              width="500"
-            >
-              <v-card>
-        <v-card-title class="headline blue-grey darken-3">
-          <div style="color: white">
-            <v-icon class="ma-5" dark>mdi-map-marker-check-outline</v-icon>
-             会場の追加
-          </div>
-          <v-spacer></v-spacer>
-          <v-btn text @click="dialog = false" fab dark>
-            ​ <v-icon>mdi-close</v-icon>
-          </v-btn>
-      </v-card-title>
+              <v-dialog
+                v-model="dialog"
+                width="500"
+                >
+                <v-card>
+                  <v-card-title class="headline blue-grey darken-3">
+                    <div style="color: white">
+                      <v-icon class="ma-5" dark>mdi-map-marker-check-outline</v-icon>
+                      会場の追加
+                    </div>
+                    <v-spacer></v-spacer>
+                    <v-btn text @click="dialog = false" fab dark>
+                      ​ <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                  </v-card-title>
 
-      <v-card-text>
-        <v-row>
-          <v-col>
-            <v-form ref="form">
-              <v-text-field
-                label="場所名"
-                v-model="name"
-                outlined
-                clearable
-                />
-            </v-form>
-          </v-col>
-        </v-row>
-      </v-card-text>
+                  <v-card-text>
+                    <v-row>
+                      <v-col>
+                        <v-form ref="form">
+                          <v-text-field
+                            label="場所名"
+                            v-model="name"
+                            outlined
+                            clearable
+                            />
+                        </v-form>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
 
-      <v-divider></v-divider>
+                  <v-divider></v-divider>
 
-                            <v-card-actions>
-                              <v-btn
-                                flatk
-                                large
-                                block
-                                dark
-                                color="blue"
-                                @click="register"
-                                >登録 ​
-                              </v-btn>
-      </v-card-actions>
-      </v-card>
-    </v-dialog>
+                  <v-card-actions>
+                    <v-btn
+                      flatk
+                      large
+                      block
+                      dark
+                      color="blue"
+                      @click="register"
+                      >登録 ​
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
               <hr class="mt-n3">
               <template>
                 <div class="text-center" v-if="places.length === 0">
@@ -112,22 +112,22 @@
                   <br><br>
                 </div>
                 <div v-else>
-                <v-data-table
-                  :headers="headers"
-                  :items="places"
-                  class="elevation-0 my-9"
-                  @click:row="
-                               (data) =>
-                               $router.push({ path: `/places/${data.id}`})
-                               "
-                  >
-                  <template v-slot:item.created_at="{ item }">
-                    {{ item.created_at | format-date }}
-                  </template>
-                  <template v-slot:item.updated_at="{ item }">
-                    {{ item.updated_at | format-date }}
-                  </template>
-                </v-data-table>                      
+                  <v-data-table
+                    :headers="headers"
+                    :items="places"
+                    class="elevation-0 my-9"
+                    @click:row="
+                      (data) =>
+                      $router.push({ path: `/places/${data.id}`})
+                      "
+                    >
+                    <template v-slot:item.created_at="{ item }">
+                      {{ item.created_at | format-date }}
+                    </template>
+                    <template v-slot:item.updated_at="{ item }">
+                      {{ item.updated_at | format-date }}
+                    </template>
+                  </v-data-table>                      
                 </div>
               </template>
             </v-col>
