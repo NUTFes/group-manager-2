f0810cc63184e9b4094145d0b4b57a762a56fd37
sasadai <d.sasaki.nutfes@gmail.com>
2021-03-05 17:15:19 +0900

    [mod] 従業員申請の編集をできるようにした 265

--- admin_view/nuxt-project/pages/employees/_id.vue
+++ admin_view/nuxt-project/pages/employees/_id.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div v-if="employee.length===0">
+          <NoData/>
+    </div>
+    <div v-else>
     <v-row>
       <v-col>
         <div class="card">
@@ -24,7 +28,32 @@
               <v-card-title class="font-weight-bold mt-3">
                 {{employee.name}}
                 <v-spacer></v-spacer>
-                <v-btn text @click="dialog = true"><v-icon class="ma-5" color="#E040FB">mdi-pencil</v-icon></v-btn>
+                <v-tooltip top>
+                  <template v-slot:activator="{ on, attrs  }">
+                      <v-btn 
+                      text 
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="edit_dialog_open" 
+                      fab>
+                      <v-icon class="ma-5">mdi-pencil</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>編集</span>
+                </v-tooltip>
+                <v-tooltip top>
+                  <template v-slot:activator="{ on, attrs  }">
+                    <v-btn 
+                      text 
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="delete_dialog = true" 
+                      fab>
+                      <v-icon class="ma-5">mdi-delete</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>削除</span>
+                </v-tooltip>
               </v-card-title>
               <hr class="mt-n3">
               <v-simple-table class="my-9">
@@ -69,74 +98,229 @@
       </v-col>
     </v-row>
 
-    </v-col>
-    <v-col cols="1"></v-col>
-    </v-row>
+    <!-- 編集ダイアログ -->
 
-    <!-- modal window to edit -->
     <v-dialog
-      v-model="dialog"
-      width="1200"
+      v-model="edit_dialog"
+      width="500"
       >
       <v-card>
+        <v-card-title class="headline blue-grey darken-3">
+          <div style="color: white">
+            <v-icon class="ma-5" dark>mdi-pencil</v-icon>編集
+          </div>
+          <v-spacer></v-spacer>
+          <v-btn text @click="edit_dialog = false" fab dark>
+            ​ <v-icon>mdi-close</v-icon>
+          </v-btn>
+      </v-card-title>
+
+      <v-card-text>
         <v-row>
-          <v-col cols="2"></v-col>
-          <v-col cols="8">
-            <v-card-title class="font-weight-bold"><v-icon class="pa-2">mdi-pencil</v-icon>登録情報の編集</v-card-title>
-            <v-text-field
-              label="グループ名"
-              background-color="white"
-              outlined
-              v-model="name"
-              filled
-              clearable
-              ></v-text-field>
-            <v-text-field
-              label="企画名"
-              background-color="white"
-              outlined
-              v-model="project_name"
-              filled
-              clearable
-              ></v-text-field>
-            <v-text-field
-              label="企画内容"
-              background-color="white"
-              outlined
-              v-model="activity"
-              filled
-              clearable
-              ></v-text-field>
-            <v-btn color="blue darken-1" block dark @click="submit">登録</v-btn>
-            <v-btn color="blue darken-1" text block @click="cancel">リセット</v-btn>
+          <v-col>
+            <v-form ref="form">
+              <v-text-field
+                label="名前"
+                v-model="name"
+                text
+                outlined
+                clearable
+                :rules="[rules.required]"
+                />
+              <v-select
+                label="参加団体"
+                v-model="group_id"
+                :items="groups"
+                item-text="name"
+                item-value="id"
+                outlined
+                />
+              <v-text-field
+                label="学籍番号"
+                v-model="student_id"
+                text
+                outlined
+                clearable
+                :rules="[rules.required]"
+                />
+              
+            </v-form>
           </v-col>
-          <v-col cols="2"></v-col>
         </v-row>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="#78909C"
+          dark
+          @click="edit"
+          >
+          編集する
+        </v-btn>
+      </v-card-actions>
       </v-card>
-    </v-dialog>
+    </v-dialog> 
+
+    <!-- 削除ダイアログ -->
+    <v-dialog
+      v-model="delete_dialog"
+      width="500"
+      >
+      <v-card>
+        <v-card-title class="headline blue-grey darken-3">
+          <div style="color: white">
+            <v-icon class="ma-5" dark>mdi-delete</v-icon>削除
+          </div>
+          <v-spacer></v-spacer>
+          <v-btn text @click="delete_dialog = false" fab dark>
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+
+      <v-card-title>
+        削除してよろしいですか？
+      </v-card-title>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          flat
+          color="red"
+          dark
+          @click="delete_yes"
+          >
+          はい
+        </v-btn>
+        <v-btn
+          flat
+          color="blue"
+          dark
+          @click="delete_dialog = false"
+          >
+          いいえ
+        </v-btn>
+      </v-card-actions>
+      </v-card>
+    </v-dialog> 
+
+    <!-- 編集成功SnackBar -->
+    <v-snackbar
+      v-model="success_snackbar"
+      color="blue-grey"
+      top
+      elevation="24"
+    >
+      編集しました
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="white"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+        <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+  </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import NoData from "../../components/NoData.vue" ;
 export default {
+  components :{
+    NoData
+  },
   data() {
     return {
+      data: [],
+      id: [],
+      group: [],
       employee: [],
+      student_id: [],
+      name: [],
+      group_id: [],
       expand: false,
-      dialog: false,
-    }
+      edit_dialog: false,
+      delete_dialog: false,
+      rules: {
+        required: value => !!value || '入力してください',
+      },
+    };
   },
   mounted() {
+    this.$axios.get('/groups', {
+        headers: { 
+          "Content-Type": "application/json", 
+        },
+      })
+      .then(response => {
+        this.groups = response.data
+      })
     const url = "/api/v1/get_employee/" + this.$route.params.id;
     this.$axios.get(url, {
       headers: { 
         "Content-Type": "application/json", 
-      }
+      },
     })
       .then(response => {
+        this.data = response.data
         this.employee = response.data.employee
+        this.id = response.data.employee.id
         this.group = response.data.group
+        this.student_id = response.data.employee.student_id
+        this.group_id = response.data.employee.group_id
+        this.name = response.data.employee.name
       })
+  },
+  methods: {
+    reload: function(){
+      console.log("reload")
+      const url = "/api/v1/get_employee/" + this.$route.params.id;
+      this.$axios.get(url, {
+        headers: { 
+          "Content-Type": "application/json", 
+        }
+      }
+      )
+        .then(response => {
+        this.employee = response.data.employee
+        this.id = response.data.employee.id
+        this.group = response.data.group
+        this.student_id = response.data.employee.student_id
+        this.group_id = response.data.employee.group_id
+        this.name = response.data.employee.name
+        })
+    },
+    edit_dialog_open: function() {
+      this.edit_dialog = true
+    },
+    edit: function() {
+      const edit_url = '/employees/' + this.id + '?group_id=' + this.group_id + '&name=' + this.name + '&student_id=' + this.student_id
+      console.log(edit_url)
+      this.$axios.put(edit_url , {
+        headers: { 
+          "Content-Type": "application/json", 
+        }
+      }).then(response => {
+        console.log(response)
+        this.reload()
+        this.edit_dialog = false
+        this.success_snackbar = true
+      })
+    },
+    delete_yes: function() {
+      this.$router.push('/employees')
+    }
   }
 }
 </script>
