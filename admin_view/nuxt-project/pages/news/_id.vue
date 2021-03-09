d2c6409dab2e834edc543f531c83b6f6c5fb70f9
sasadai <d.sasaki.nutfes@gmail.com>
2021-03-09 15:24:06 +0900

    [mod] お知らせの編集・削除をできるようにした 268

--- admin_view/nuxt-project/pages/news/_id.vue
+++ admin_view/nuxt-project/pages/news/_id.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div v-if="news.length===0">
+          <NoData/>
+    </div>
+    <div v-else>  
     <v-row>
       <v-col>
         <div class="card">
@@ -18,7 +22,32 @@
                 <v-card-title class="font-weight-bold mt-3">
                   {{ news.title }}
                   <v-spacer></v-spacer>
-                  <v-btn text @click="dialog = true"><v-icon class="ma-5" color="#E040FB">mdi-pencil</v-icon></v-btn>
+                <v-tooltip top>
+                  <template v-slot:activator="{ on, attrs  }">
+                      <v-btn 
+                      text 
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="edit_dialog_open" 
+                      fab>
+                      <v-icon class="ma-5">mdi-pencil</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>編集</span>
+                </v-tooltip>
+                <v-tooltip top>
+                  <template v-slot:activator="{ on, attrs  }">
+                    <v-btn 
+                      text 
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="delete_dialog = true" 
+                      fab>
+                      <v-icon class="ma-5">mdi-delete</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>削除</span>
+                </v-tooltip>
                 </v-card-title>
                 <hr class="mt-n3">
                 <v-simple-table class="my-9">
@@ -62,58 +91,147 @@
       </v-col>
     </v-row>
 
-    <!-- modal window to edit -->
+<!-- 編集ダイアログ -->
+
     <v-dialog
-      v-model="dialog"
-      width="1200"
+      v-model="edit_dialog"
+      width="500"
       >
-      <v-card flat>
+      <v-card>
+        <v-card-title class="headline blue-grey darken-3">
+          <div style="color: white">
+            <v-icon class="ma-5" dark>mdi-pencil</v-icon>編集
+          </div>
+          <v-spacer></v-spacer>
+          <v-btn text @click="edit_dialog = false" fab dark>
+            ​ <v-icon>mdi-close</v-icon>
+          </v-btn>
+      </v-card-title>
+
+      <v-card-text>
         <v-row>
-          <v-col cols="2"></v-col>
-          <v-col cols="8">
-            <v-card-title class="font-weight-bold"><v-icon class="pa-2">mdi-pencil</v-icon>登録情報の編集</v-card-title>
-            <v-text-field
-              label="グループ名"
-              background-color="white"
-              outlined
-              v-model="name"
-              filled
-              clearable
-              ></v-text-field>
-            <v-text-field
-              label="企画名"
-              background-color="white"
-              outlined
-              v-model="project_name"
-              filled
-              clearable
-              ></v-text-field>
-            <v-text-field
-              label="企画内容"
-              background-color="white"
-              outlined
-              v-model="activity"
-              filled
-              clearable
-              ></v-text-field>
-            <v-btn color="blue darken-1" block dark @click="submit">登録</v-btn>
-            <v-btn color="blue darken-1" text block @click="cancel">リセット</v-btn>
+          <v-col>
+            <v-form ref="form">
+                <v-text-field
+                label="タイトル"
+                v-model="title"
+                clearable
+                outlined
+                :rules="[rules.required]"
+                />
+                <v-text-field
+                label="内容"
+                v-model="body"
+                clearable
+                outlined
+                :rules="[rules.required]"
+                />
+            </v-form>
           </v-col>
-          <v-col cols="2"></v-col>
         </v-row>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="#78909C"
+          dark
+          @click="edit"
+          >
+          編集する
+        </v-btn>
+      </v-card-actions>
       </v-card>
-    </v-dialog>
+    </v-dialog> 
+
+    <!-- 削除ダイアログ -->
+    <v-dialog
+      v-model="delete_dialog"
+      width="500"
+      >
+      <v-card>
+        <v-card-title class="headline blue-grey darken-3">
+          <div style="color: white">
+            <v-icon class="ma-5" dark>mdi-delete</v-icon>削除
+          </div>
+          <v-spacer></v-spacer>
+          <v-btn text @click="delete_dialog = false" fab dark>
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+
+      <v-card-title>
+        削除してよろしいですか？
+      </v-card-title>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          flat
+          color="red"
+          dark
+          @click="delete_yes"
+          >
+          はい
+        </v-btn>
+        <v-btn
+          flat
+          color="blue"
+          dark
+          @click="delete_dialog = false"
+          >
+          いいえ
+        </v-btn>
+      </v-card-actions>
+      </v-card>
+    </v-dialog> 
+
+    <!-- 編集成功SnackBar -->
+    <v-snackbar
+      v-model="success_snackbar"
+      color="blue-grey"
+      top
+      elevation="24"
+    >
+      編集しました
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="white"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+        <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
   </div>
 </template>
 
 <script>
-
+import NoData from "../../components/NoData.vue" ;
 export default {
+  components :{
+    NoData
+  },
   data() {
     return {
       news: [],
+      id : [],
+      title: [],
+      body: [],
       expand: false,
-      dialog: false,
+      edit_dialog: false,
+      delete_dialog: false,
+       rules: {
+        required: value => !!value || '入力してください',
+      },
     }
   },
   mounted() {
@@ -126,7 +244,50 @@ export default {
     )
       .then(response => {
         this.news = response.data
+        this.id = response.data.id
+        this.title = response.data.title
+        this.body = response.data.body
       })
+  },
+  methods: {
+    reload: function(){
+      console.log("reload")
+      const url = "/news/" + this.$route.params.id;
+      this.$axios.get(url, {
+        headers: { 
+          "Content-Type": "application/json", 
+        }
+      }
+      )
+        .then(response => {
+        this.news = response.data
+        this.id = response.data,id
+        this.title = response.data.title
+        this.body = response.data.body
+        })
+    },
+    edit_dialog_open: function() {
+      this.edit_dialog = true
+    },
+    edit: function() {
+      const edit_url = '/news/' + this.id + '?title=' + this.title + '&body=' + this.body
+      console.log(edit_url)
+      this.$axios.put(edit_url , {
+        headers: { 
+          "Content-Type": "application/json", 
+        }
+      }).then(response => {
+        console.log(response)
+        this.reload()
+        this.edit_dialog = false
+        this.success_snackbar = true
+      })
+    },
+    delete_yes: function() {
+      const url = "/news/" + this.$route.params.id;
+      this.$axios.delete(url)
+      this.$router.push('/news')
+    }
   }
 }
 </script>
