c2528af5418115be58ecae98a014914ab8341b84
YushiroDodo63 <yushiro0603@gmail.com>
2021-03-09 20:08:32 +0900

    [mod] 使用可能物品の追加機能を実装した(#350)

--- admin_view/nuxt-project/pages/rental_item_allow_lists/index.vue
+++ admin_view/nuxt-project/pages/rental_item_allow_lists/index.vue
@@ -9,6 +9,36 @@
             <v-card-title class="font-weight-bold mt-3">
               <v-icon class="mr-5">mdi-table-furniture</v-icon>使用可能物品一覧
               <v-spacer></v-spacer>
+                <v-tooltip top>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      text
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="open_add_dialog"
+                    >
+                      <v-icon dark>mdi-plus-circle-outline</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>使用可能物品の追加</span>
+                </v-tooltip>
+                <v-tooltip top>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      text
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="reload"
+                    >
+                      <v-icon dark>mdi-reload</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>更新する</span>
+                </v-tooltip>
               <v-tooltip top>
                 <template v-slot:activator="{ on, attrs  }">
                   <v-btn 
@@ -25,6 +55,66 @@
                 <span>印刷する</span>
               </v-tooltip>
             </v-card-title>
+
+                <v-dialog v-model="dialog" max-width="500">
+                  <v-card>
+                    <v-card-title class="headline blue-grey darken-3">
+                      <div style="color: white">
+                        <v-icon class="ma-2" dark>mdi-table-furniture</v-icon
+                        >使用可能物品の追加
+                      </div>
+                      <v-spacer></v-spacer>
+                      <v-btn text @click="dialog = false" fab dark>
+                        ​ <v-icon>mdi-close</v-icon>
+                      </v-btn>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-row>
+                        <v-col>
+                          <v-form ref="form">
+                            <v-select
+                              label="物品"
+                              v-model="rentalItemId"
+                              :items="rental_items"
+                              :menu-props="{
+                                top: true,
+                                offsetY: true,
+                              }"
+                              item-text="name"
+                              item-value="id"
+                              outlined
+                            ></v-select>
+                            <v-select
+                              label="参加団体名"
+                              v-model="groupCategoryId"
+                              :items="group_categories"
+                              :menu-props="{
+                                top: true,
+                                offsetY: true,
+                              }"
+                              item-text="name"
+                              item-value="id"
+                              outlined
+                            ></v-select>
+                            <v-card-actions>
+                              <v-btn
+                                flatk
+                                large
+                                block
+                                dark
+                                color="blue"
+                                @click="register()"
+                                >登録 ​
+                              </v-btn>
+                            </v-card-actions>
+                          </v-form>
+                        </v-col>
+                      </v-row>
+                    </v-card-text>
+                    <br />
+                  </v-card>
+                </v-dialog>
+
             <hr class="mt-n3">
             <template>
               <div class="text-center" v-if="rental_item_allow_list.length === 0">
@@ -69,7 +159,6 @@
       </div>
     </v-col>
   </v-row>
-  </div>
 </template>
 
 <script>
@@ -77,7 +166,11 @@ export default {
   data() {
     return {
       rental_item_allow_list: [],
+      rental_items: [],
       group_categories: [],
+      dialog: false,
+      rentalItemId: [],
+      groupCategoryId: [],
       category: [],
       headers:[
         { text: 'ID', value: 'rental_item_allow_list.id' },
@@ -109,6 +202,59 @@ export default {
         this.rental_item_allow_list = response.data
       })
   },
+
+  methods: {
+    open_add_dialog: function (){
+      const url = "/api/v1/get_rental_item_allow_lists";
+      this.$axios
+        .get(url, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then(
+          (response) => {
+            this.rental_item_allow_list = response.data;
+          },
+          (error) => {
+            console.error(error);
+            return error;
+          }
+        );
+      this.$axios.get('/rental_items', {
+        headers: { 
+          "Content-Type": "application/json", 
+        }
+      }).then(response => {
+        this.rental_items = response.data
+      })
+        this.dialog = true
+    },
+    reload: function () {
+      this.$axios
+        .get("/api/v1/get_rental_item_allow_lists", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((response) => {
+          this.rental_item_allow_list = response.data;
+        });
+    },
+    register: function () {
+      this.$axios.defaults.headers.common["Content-Type"] = "application/json";
+      var params = new URLSearchParams();
+      params.append("rental_item_id", this.rentalItemId);
+      params.append("group_category_id", this.groupCategoryId);
+      this.$axios.post("/rental_item_allow_lists", params).then((response) => {
+        console.log(response);
+        this.dialog = false;
+        this.reload();
+        this.rentalItemId = "";
+        this.groupCategoryId = "";
+      });
+    },
+  }
 }
 </script>
 
