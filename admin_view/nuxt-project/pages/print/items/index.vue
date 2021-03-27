83db32ea6610533ee08c0e3efb3af8d02fa3be10
oba18 <m.oba.0426@gmail.com>
2021-03-27 18:11:16 +0900

    [add] 貸出物品リストの印刷機能の追加(#423)

--- admin_view/nuxt-project/pages/print/items/index.vue
+++ admin_view/nuxt-project/pages/print/items/index.vue
@@ -19,65 +19,261 @@
     </v-row>
 
     <div class="sheet">
-      <h2>ユーザー一覧</h2>
-      <h3>テーブルを印刷する</h3>
-      <v-simple-table dense>
-        <template v-slot:default>
+      <br>
+      <h2>貸出物品リスト - 模擬店(食品販売)</h2>
+      <hr>
+      <br>
+      <table border=solid>
           <thead>
             <tr>
-              <th class="text-left">ID</th>
-              <th class="text-left">name</th>
-              <th class="text-left">email</th>
+              <th class="text-left">団体ID</th>
+              <th class="text-left">カテゴリー</th>
+              <th class="text-left">参加団体名</th>
+              <th class="text-left">物品</th>
+              <th class="text-left">場所</th>
+              <th class="text-left">個数</th>
             </tr>
           </thead>
-          <tbody>
-            <tr v-for="user in users" :key="user.id">
-              <td>{{ user.id }}</td>
-              <td>{{ user.name }}</td>
-              <td>{{ user.email }}</td>
+          <tbody v-for="group in group_1" :key="group.group.id">
+            <tr>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.id }}</td>
+              <td :rowspan=group.assign_rental_item.length>食品販売</td>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.name }}</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].name }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].place }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].num }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+            </tr>
+            <tr v-for="i in group.assign_rental_item.slice(1)" v-if="group.assign_rental_item.slice(1) !== 0">
+              <td>{{ i.name }}</td>
+              <td>{{ i.place }}</td>
+              <td>{{ i.num }}</td>
             </tr>
           </tbody>
-        </template>
-      </v-simple-table>
+      </table>
     </div>
 
-    <v-row>
-      <v-col cols=4></v-col>
-      <v-col>
-        <v-btn text color="white" to="/users"><v-icon color="#333333">mdi-arrow-left-bold</v-icon><div style="color:#333333">ユーザー一覧に戻る</div></v-btn>
-      </v-col>
-    </v-row>
+    <div class="sheet">
+      <br>
+      <h2>貸出物品リスト - 模擬店(物品販売)</h2>
+      <hr>
+      <br>
+      <table border=solid>
+          <thead>
+            <tr>
+              <th class="text-left">団体ID</th>
+              <th class="text-left">カテゴリー</th>
+              <th class="text-left">参加団体名</th>
+              <th class="text-left">物品</th>
+              <th class="text-left">場所</th>
+              <th class="text-left">個数</th>
+            </tr>
+          </thead>
+          <tbody v-for="group in group_2" :key="group.group.id">
+            <tr>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.id }}</td>
+              <td :rowspan=group.assign_rental_item.length>物品販売</td>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.name }}</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].name }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].place }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].num }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+            </tr>
+            <tr v-for="i in group.assign_rental_item.slice(1)" v-if="group.assign_rental_item.slice(1) !== 0">
+              <td>{{ i.name }}</td>
+              <td>{{ i.place }}</td>
+              <td>{{ i.num }}</td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
+
+    <div class="sheet">
+      <br>
+      <h2>貸出物品リスト - ステージ企画</h2>
+      <hr>
+      <br>
+      <table border=solid>
+          <thead>
+            <tr>
+              <th class="text-left">団体ID</th>
+              <th class="text-left">カテゴリー</th>
+              <th class="text-left">参加団体名</th>
+              <th class="text-left">物品</th>
+              <th class="text-left">場所</th>
+              <th class="text-left">個数</th>
+            </tr>
+          </thead>
+          <tbody v-for="group in group_3" :key="group.group.id">
+            <tr>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.id }}</td>
+              <td :rowspan=group.assign_rental_item.length>ステージ</td>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.name }}</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].name }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].place }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].num }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+            </tr>
+            <tr v-for="i in group.assign_rental_item.slice(1)" v-if="group.assign_rental_item.slice(1) !== 0">
+              <td>{{ i.name }}</td>
+              <td>{{ i.place }}</td>
+              <td>{{ i.num }}</td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
+
+    <div class="sheet">
+      <br>
+      <h2>貸出物品リスト - 展示・体験</h2>
+      <hr>
+      <br>
+      <table border=solid>
+          <thead>
+            <tr>
+              <th class="text-left">団体ID</th>
+              <th class="text-left">カテゴリー</th>
+              <th class="text-left">参加団体名</th>
+              <th class="text-left">物品</th>
+              <th class="text-left">場所</th>
+              <th class="text-left">個数</th>
+            </tr>
+          </thead>
+          <tbody v-for="group in group_4" :key="group.group.id">
+            <tr>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.id }}</td>
+              <td :rowspan=group.assign_rental_item.length>展示・体験</td>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.name }}</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].name }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].place }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].num }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+            </tr>
+            <tr v-for="i in group.assign_rental_item.slice(1)" v-if="group.assign_rental_item.slice(1) !== 0">
+              <td>{{ i.name }}</td>
+              <td>{{ i.place }}</td>
+              <td>{{ i.num }}</td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
+
+    <div class="sheet">
+      <br>
+      <h2>貸出物品リスト - 研究室公開</h2>
+      <hr>
+      <br>
+      <table border=solid>
+          <thead>
+            <tr>
+              <th class="text-left">団体ID</th>
+              <th class="text-left">カテゴリー</th>
+              <th class="text-left">参加団体名</th>
+              <th class="text-left">物品</th>
+              <th class="text-left">場所</th>
+              <th class="text-left">個数</th>
+            </tr>
+          </thead>
+          <tbody v-for="group in group_5" :key="group.group.id">
+            <tr>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.id }}</td>
+              <td :rowspan=group.assign_rental_item.length>研究室公開</td>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.name }}</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].name }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].place }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].num }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+            </tr>
+            <tr v-for="i in group.assign_rental_item.slice(1)" v-if="group.assign_rental_item.slice(1) !== 0">
+              <td>{{ i.name }}</td>
+              <td>{{ i.place }}</td>
+              <td>{{ i.num }}</td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
+
+    <div class="sheet">
+      <br>
+      <h2>貸出物品リスト - その他</h2>
+      <hr>
+      <br>
+      <table border=solid>
+          <thead>
+            <tr>
+              <th class="text-left">団体ID</th>
+              <th class="text-left">カテゴリー</th>
+              <th class="text-left">参加団体名</th>
+              <th class="text-left">物品</th>
+              <th class="text-left">場所</th>
+              <th class="text-left">個数</th>
+            </tr>
+          </thead>
+          <tbody v-for="group in group_6" :key="group.group.id">
+            <tr>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.id }}</td>
+              <td :rowspan=group.assign_rental_item.length>その他</td>
+              <td :rowspan=group.assign_rental_item.length>{{ group.group.name }}</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].name }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].place }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+              <td v-if="group.assign_rental_item.length !== 0">{{ group.assign_rental_item[0].num }}</td>
+              <td v-else-if="group.assign_rental_item.length === 0">未登録</td>
+            </tr>
+            <tr v-for="i in group.assign_rental_item.slice(1)" v-if="group.assign_rental_item.slice(1) !== 0">
+              <td>{{ i.name }}</td>
+              <td>{{ i.place }}</td>
+              <td>{{ i.num }}</td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
+
   </div>
 </template>
 
 <script>
-import Header from '~/components/Header.vue'
-import Menu from '~/components/Menu.vue'
-
 export default {
   data() {
     return {
-      list: null,
-      users: null
+      lists: [],
+      group_1: [],
+      group_2: [],
+      group_3: [],
+      group_4: [],
+      group_5: [],
+      group_6: [],
     }
-  },
-  components:{
-    Header,
-    Menu,
   },
   mounted() {
     this.getList()
-    setTimeout(this.handlePrint, 100);
   },
   methods: {
     getList() {
-      this.$axios.get('api/v1/users/index', {
+      this.$axios.get('api/v1/get_print_items', {
         headers: { 
           "Content-Type": "application/json", 
         }
       })
       .then(response => {
-        this.users = response.data.data
+        this.lists = response.data
+        this.group_1 = this.lists.group_1
+        this.group_2 = this.lists.group_2
+        this.group_3 = this.lists.group_3
+        this.group_4 = this.lists.group_4
+        this.group_5 = this.lists.group_5
+        this.group_6 = this.lists.group_6
       })
     },
     handlePrint() {
@@ -122,5 +318,16 @@ h1 {
   BODY {
     background: #eee;
   }
+}
+table {
+  border-collapse:collapse;
+}
+th {
+  padding: 0.3%;
+  width: 150px
+}
+td {
+  padding: 0.3%;
+  width: 150px
 }
 </style>
--- api/db/fixtures/assign_rental_item.rb
+++ api/db/fixtures/assign_rental_item.rb
@@ -3,5 +3,55 @@ AssignRentalItem.seed( :id,
               rental_item_id: 1 ,
               num: 1,
               stocker_place_id: 1
-    }
+    },
+    { id: 2,  group_id: 2 ,
+              rental_item_id: 1 ,
+              num: 1,
+              stocker_place_id: 1
+    },
+    { id: 3,  group_id: 3 ,
+              rental_item_id: 1 ,
+              num: 1,
+              stocker_place_id: 1
+    },
+    { id: 4,  group_id: 4 ,
+              rental_item_id: 1 ,
+              num: 1,
+              stocker_place_id: 1
+    },
+    { id: 5,  group_id: 5 ,
+              rental_item_id: 1 ,
+              num: 1,
+              stocker_place_id: 1
+    },
+    { id: 6,  group_id: 6 ,
+              rental_item_id: 1 ,
+              num: 1,
+              stocker_place_id: 1
+    },
+    { id: 7,  group_id: 1 ,
+              rental_item_id: 2 ,
+              num: 10,
+              stocker_place_id: 2
+    },
+    { id: 8,  group_id: 1 ,
+              rental_item_id: 3 ,
+              num: 3,
+              stocker_place_id: 3
+    },
+    { id: 9,  group_id: 1 ,
+              rental_item_id: 5 ,
+              num: 15,
+              stocker_place_id: 5
+    },
+    { id: 10,  group_id: 2 ,
+              rental_item_id: 5 ,
+              num: 15,
+              stocker_place_id: 5
+    },
+    { id: 11,  group_id: 2 ,
+              rental_item_id: 2 ,
+              num: 10,
+              stocker_place_id: 2
+    },
 )
