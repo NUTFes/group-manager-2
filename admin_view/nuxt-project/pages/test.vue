9d452fa6247ca9e14a4b630ef57f343f628162ec
mashita1023 <coloring45@gmail.com>
2021-04-02 18:16:02 +0900

    [add] vuex test

---
+++ admin_view/nuxt-project/pages/test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols=3></v-col>
+      <v-col cols=9>
+    <h1>TODOLIST</h1>
+
+      <v-btn @click="getUser">click</v-btn>
+    <p>
+      {{ role }}
+    </p>
+    </v-col>
+    </v-row>
+</div>
+</template>
+
+<script>
+import { mapState, mapActions, mapMutations, mapGetters } from 'vuex'
+export default {
+
+  computed:{
+    ...mapGetters({
+      role: "users/getRole"
+    }),
+  },
+  methods: {
+    ...mapActions({
+      getUser: "users/getUser"
+    }),
+    ...mapMutations({
+      setRole: "users/setRole"
+    })
+  },
+}
+</script>
