c873d330469b886702b5d75a9494424c4733ab9a
tanaka-hina <t.hina145.amaou0141@gmail.com>
2021-09-16 22:23:12 +0900

    [mod]ユーザ詳細登録のモーダルのレイアウト作成(#560)

--- view/vue-project/src/views/mypage.vue
+++ view/vue-project/src/views/mypage.vue
@@ -20,48 +20,81 @@
             <div v-for="(regist, i) in regist_info" :key="i">
               <Regist :num="i" :regist="regist" @reload="reload()" />
               <v-container>
-            <v-row>
-              <v-col cols="4"></v-col>
-              <v-col cols="4">
-                <v-btn
-                  v-if='regist.group.group_category_id === 1 && regist.employees[0].name === "-9999" && addEmployee && addFoodProduct &&addPurchaseList'
-                  block dark color="purple accent-2"
-                  dark
-                  rounded
-                  elevation = "0"
-                  @click="set_group_id(regist.group.id)"
-                  >
-                  <v-icon class="pr-2 pb-1">mdi-baguette</v-icon>{{ regist.group.name }}の販売食品を追加する
-                </v-btn>
-              </v-col>
-              <v-col cols="4"></v-col>
-            </v-row>
-          </v-container>
+                <v-row>
+                  <v-col cols="4"></v-col>
+                  <v-col cols="4">
+                    <v-btn
+                      v-if="
+                        regist.group.group_category_id === 1 &&
+                        regist.employees[0].name === '-9999' &&
+                        addEmployee &&
+                        addFoodProduct &&
+                        addPurchaseList
+                      "
+                      block
+                      dark
+                      color="purple accent-2"
+                      dark
+                      rounded
+                      elevation="0"
+                      @click="set_group_id(regist.group.id)"
+                    >
+                      <v-icon class="pr-2 pb-1">mdi-baguette</v-icon
+                      >{{ regist.group.name }}の販売食品を追加する
+                    </v-btn>
+                  </v-col>
+                  <v-col cols="4"></v-col>
+                </v-row>
+              </v-container>
             </div>
           </v-col>
         </v-row>
       </v-col>
       <v-col cols="2"></v-col>
     </v-row>
-      <v-container>
-        <v-row>
-          <v-col cols="4"></v-col>
-          <v-col cols="4">
-            <v-btn
-              v-if="isRegistGroup"
-              block dark color="purple accent-2"
-               dark
-               rounded
-               elevation = "0"
-               to="/group"
-             >
-           <v-icon class="pr-2 pb-1">mdi-plus</v-icon>参加団体を追加する
-            </v-btn>
-          </v-col>
-          <v-col cols="4"></v-col>
-        </v-row>
-      </v-container>
-    <br>
+    <v-container>
+      <v-row>
+        <v-col cols="4"></v-col>
+        <v-col cols="4">
+          <v-btn
+            v-if="isRegistGroup"
+            block
+            dark
+            color="purple accent-2"
+            dark
+            rounded
+            elevation="0"
+            to="/group"
+          >
+            <v-icon class="pr-2 pb-1">mdi-plus</v-icon>参加団体を追加する
+          </v-btn>
+          <br>
+          <div class="text-center">
+            <v-dialog v-model="dialog" president width="1000">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn 
+                  color="purple accent-2"
+                  block
+                  rounded 
+                  dark v-bind="attrs" 
+                  v-on="on"
+                >
+                  <v-icon 
+                    class="pr-2 pb1"
+                  >
+                    mdi-plus
+                  </v-icon>
+                  ユーザー詳細の登録をする
+                </v-btn>
+              </template>
+              <UserDetail />
+            </v-dialog>
+          </div>
+        </v-col>
+        <v-col cols="4"></v-col>
+      </v-row>
+    </v-container>
+    <br />
   </div>
 </template>
 
@@ -71,6 +104,7 @@ import DashBoard from "@/components/DashBoard.vue";
 import UserInfo from "@/components/UserInfo.vue";
 import News from "@/components/News.vue";
 import Regist from "@/components/Regist.vue";
+import UserDetail from "@/components/UserDetail.vue";
 import axios from "axios";
 export default {
   components: {
@@ -78,14 +112,15 @@ export default {
     UserInfo,
     Header,
     News,
-    Regist
+    Regist,
+    UserDetail,
   },
   data() {
     return {
       data: [
         localStorage.getItem("access-token"),
         localStorage.getItem("client"),
-        localStorage.getItem("uid")
+        localStorage.getItem("uid"),
       ],
       users: [],
       regist_info: [],
@@ -93,10 +128,11 @@ export default {
       addEmployee: [],
       addFoodProduct: [],
       addPurchaseList: [],
+      dialog: false,
     };
   },
   methods: {
-    signOut: function() {
+    signOut: function () {
       const url = process.env.VUE_APP_URL + "/api/auth/sign_out";
       axios
         .delete(url, {
@@ -104,8 +140,8 @@ export default {
             "Content-Type": "application/json",
             "access-token": localStorage.getItem("access-token"),
             client: localStorage.getItem("client"),
-            uid: localStorage.getItem("uid")
-          }
+            uid: localStorage.getItem("uid"),
+          },
         })
         .then(
           this.$router.push("/"),
@@ -115,51 +151,55 @@ export default {
         );
     },
     reload() {
-      const regist_info_url = process.env.VUE_APP_URL + "/api/v1/current_user/regist_info";
-      axios.get(regist_info_url, {
+      const regist_info_url =
+        process.env.VUE_APP_URL + "/api/v1/current_user/regist_info";
+      axios
+        .get(regist_info_url, {
           headers: {
             "Content-Type": "application/json",
             "access-token": localStorage.getItem("access-token"),
-            "client": localStorage.getItem("client"),
-            "uid": localStorage.getItem("uid")
-          }
+            client: localStorage.getItem("client"),
+            uid: localStorage.getItem("uid"),
+          },
         })
-        .then(response => {
-          console.log(response)
+        .then((response) => {
+          console.log(response);
           this.regist_info = response.data;
         });
     },
-    set_group_id: function(group_id){
-      localStorage.setItem("group_id", group_id)
-      this.$router.push('regist_food_booths')
-    }
+    set_group_id: function (group_id) {
+      localStorage.setItem("group_id", group_id);
+      this.$router.push("regist_food_booths");
+    },
   },
   mounted() {
     const url = process.env.VUE_APP_URL + "/api/v1/users/show";
-    axios.get(url, {
-      headers: {
-        "Content-Type": "application/json",
-        "access-token": localStorage.getItem("access-token"),
-        client: localStorage.getItem("client"),
-        uid: localStorage.getItem("uid")
-      }
-    })
-    .then(response => {
-      this.users = response;
-    });
+    axios
+      .get(url, {
+        headers: {
+          "Content-Type": "application/json",
+          "access-token": localStorage.getItem("access-token"),
+          client: localStorage.getItem("client"),
+          uid: localStorage.getItem("uid"),
+        },
+      })
+      .then((response) => {
+        this.users = response;
+      });
 
-    const regist_info_url = process.env.VUE_APP_URL + "/api/v1/current_user/regist_info";
+    const regist_info_url =
+      process.env.VUE_APP_URL + "/api/v1/current_user/regist_info";
     axios
       .get(regist_info_url, {
         headers: {
           "Content-Type": "application/json",
           "access-token": localStorage.getItem("access-token"),
-          "client": localStorage.getItem("client"),
-          "uid": localStorage.getItem("uid")
-        }
+          client: localStorage.getItem("client"),
+          uid: localStorage.getItem("uid"),
+        },
       })
-      .then(response => {
-        console.log(response)
+      .then((response) => {
+        console.log(response);
         this.regist_info = response.data;
       });
 
@@ -169,10 +209,10 @@ export default {
         headers: {
           "Content-Type": "application/json",
           "access-token": localStorage.getItem("access-token"),
-          client: localStorage.getItem("client")
-        }
+          client: localStorage.getItem("client"),
+        },
       })
-      .then(response => {
+      .then((response) => {
         this.isRegistGroup = response.data[0].is_regist_group;
         this.addEmployee = response.data[0].add_employee;
         this.addFoodProduct = response.data[0].add_food_product;
@@ -181,6 +221,6 @@ export default {
       });
 
     console.log(this.isRegistGroup);
-  }
+  },
 };
 </script>
