38163ba1214096b88c28e6ef0a5f313098353e47
oba18 <m.oba.0426@gmail.com>
2021-12-12 10:16:27 +0900

    [mod] for文で書いてる部分とかN+1の解決

--- api/app/models/group.rb
+++ api/app/models/group.rb
@@ -12,8 +12,14 @@ class Group < ApplicationRecord
     has_many :rental_orders, dependent: :destroy
     has_many :assign_rental_items, dependent: :destroy
 
-    # Group(参加団体)があるかを確認する
+    # IDに対してgroup(参加団体)があるかを確認する
     def self.is_exists?(id)
       return self.exists?(id=id)
+    end
+
+    # groupとgroup_categoryを取得する
+    def self.get_group_with_group_category
+      @groups = Group.preload(:group_category).map{ |group| { "group": group, "group_category": group.group_category } }
+      return @groups
     end
 end
