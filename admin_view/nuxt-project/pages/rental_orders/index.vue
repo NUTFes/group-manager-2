84c7b248dad90b1903230d757267373a001aa543
oba18 <m.oba.0426@gmail.com>
2022-01-30 15:16:58 +0900

    [mod] 物品申請一覧の絞り込み機能の追加(#686)

--- admin_view/nuxt-project/pages/rental_orders/index.vue
+++ admin_view/nuxt-project/pages/rental_orders/index.vue
@@ -8,6 +8,31 @@
         CSVダウンロード
       </CommonButton>
     </SubHeader>
+
+    <SubSubHeader>
+      <template v-slot:refinement>
+      <SearchDropDown
+        :nameList="yearList"
+        :on_click="refinementRentalOrders"
+        value="year_num"
+      >
+        {{ refYears }}
+      </SearchDropDown>
+      <SearchDropDown
+        :nameList="rentalItemsList"
+        :on_click="refinementRentalOrders"
+        value="name"
+      >
+        {{ refRentalItems }}
+      </SearchDropDown>
+      </template>
+      <template v-slot:search>
+        <SearchBar>
+          <input v-model="searchText" @keypress.enter="searchRentalOrders" type="text" size="25" placeholder="search" />
+        </SearchBar>
+      </template>
+    </SubSubHeader>
+    
     <Card width="100%">
       <Table>
       <template v-slot:table-header>
@@ -40,19 +65,72 @@ export default {
   data() {
     return {
       headers: ["ID", "参加団体", "貸出物品", "個数", "登録日時", "編集日時"],
+      rentalOrders: [],
+      refYears: "Year",
+      refYearID: 0,
+      refRentalItems: "Items",
+      refRentalItemID: 0,
+      searchText: ""
     };
   },
   async asyncData({ $axios }) {
-    const url = "/api/v1/get_rental_order_index_for_admin_view";
-    const rentalOrdersRes = await $axios.$get(url);
+    const currentYearUrl = "/user_page_settings/1";
+    const currentYearRes = await $axios.$get(currentYearUrl);
+    
+    // const url = "/api/v1/get_rental_order_index_for_admin_view";
+    const url = "/api/v1/get_refinement_rental_orders?fes_year_id=" + currentYearRes.data.fes_year_id + "&rental_item_id=0";
+    const rentalOrdersRes = await $axios.$post(url);
     const yearsUrl = "/fes_years";
     const yearsRes = await $axios.$get(yearsUrl);
+    const rentalItemsUrl = "/rental_items";
+    const rentalItemsRes = await $axios.$get(rentalItemsUrl);
+    const currentYears = yearsRes.data.filter(function (element) {
+      return element.id == currentYearRes.data.fes_year_id;
+    });
     return {
       rentalOrders: rentalOrdersRes.data,
       yearList: yearsRes.data,
+      rentalItemsList: rentalItemsRes.data,
+      refYearID: currentYearRes.data.fes_year_id,
+      refYears: currentYears[0].year_num
     };
   },
   methods: {
+    async refinementRentalOrders(item_id, name_list){
+      // fes_yearで絞り込むとき
+      if (name_list.toString() == this.yearList.toString()){
+        this.refYearID = item_id
+        // ALLの時
+        if (item_id == 0){
+          this.refYears = "ALL"
+        }else{
+          this.refYears = name_list[item_id - 1].year_num
+        }
+      // group_categoryで絞り込むとき
+      }else if(name_list.toString() == this.rentalItemsList.toString()){
+        this.refRentalItemID = item_id
+        // ALLの時
+        if (item_id == 0){
+          this.refRentalItems = "ALL"
+        }else{
+          this.refRentalItems = name_list[item_id - 1].name
+        }
+      }
+      this.rentalOrders = []
+      const refUrl = "/api/v1/get_refinement_rental_orders?fes_year_id=" + this.refYearID + "&rental_item_id=" + this.refRentalItemID;
+      const refRes = await this.$axios.$post(refUrl);
+      for (const res of refRes.data){
+        this.rentalOrders.push(res)
+      }
+    },
+    async searchRentalOrders() {
+      this.rentalOrders = []
+      const searchUrl = "/api/v1/get_search_rental_orders?word=" + this.searchText
+      const refRes = await this.$axios.$post(searchUrl);
+      for (const res of refRes.data){
+        this.rentalOrders.push(res)
+      }
+    },
     reload: function () {
       this.$axios
         .get("/api/v1/get_rental_orders", {
