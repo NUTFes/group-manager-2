ae6f5fd975964bb61e03479fd8bb67641d1a2df0
oba18 <m.oba.0426@gmail.com>
2022-03-12 15:50:25 +0900

    [mod] お知らせページのCRUDの追加(#718)

--- admin_view/nuxt-project/pages/news/index.vue
+++ admin_view/nuxt-project/pages/news/index.vue
@@ -36,37 +36,30 @@
     <AddModal
       @close="closeAddModal"
       v-if="isOpenAddModal"
-      title="従業員申請の追加"
+      title="お知らせの追加"
     >
       <template v-slot:form>
         <div>
-          <h3>団体名</h3>
-          <select v-model="appGroup">
-            <option disabled value="">選択してください</option>
-            <option
-              v-for="group in groupList"
-              :key="group.id"
-              :value="group.id"
-            >
-              {{ group.name }}
-            </option>
-          </select>
+          <h3>タイトル</h3>
+          <input v-model="title" placeholder="入力してください" />
         </div>
         <div>
-          <h3>氏名</h3>
-          <input v-model="employeeName" placeholder="入力してください" />
-        </div>
-        <div>
-          <h3>学籍番号</h3>
-          <input v-model="employeeStudentId" placeholder="入力してください" />
+          <h3>内容</h3>
+          <textarea v-model="body" placeholder="入力してください" />
         </div>
       </template>
       <template v-slot:method>
-        <CommonButton iconName="add_circle" :on_click="submitEmployee"
+        <CommonButton iconName="add_circle" :on_click="submit"
           >登録</CommonButton
         >
       </template>
     </AddModal>
+    <SnackBar
+      v-if="isOpenSnackBar"
+      @close="closeSnackBar"
+    >
+      {{ message }}
+    </SnackBar>
   </div>
 </template>
 
@@ -77,6 +70,9 @@ export default {
     return {
       headers: ["ID", "タイトル", "日時", "編集日時"],
       isOpenAddModal: false,
+      isOpenSnackBar: false,
+      title: "",
+      body: ""
     };
   },
   async asyncData({ $axios }) {
@@ -94,29 +90,34 @@ export default {
     closeAddModal() {
       this.isOpenAddModal = false;
     },
-    reload() {
-      const employeeId = this.employees.slice(-1)[0].employee.id + 1;
-      const reUrl = "/api/v1/get_employee_show_for_admin_view/" + employeeId;
-      this.$axios.$get(reUrl).then((response) => {
-        this.employees.push(response.data);
+    openSnackBar(message) {
+      this.message = message;
+      this.isOpenSnackBar = true;
+      setTimeout(this.closeSnackBar, 2000);
+    },
+    closeSnackBar() {
+      this.isOpenSnackBar = false;
+    },
+    reload(id) {
+      const url = "/news/" + id;
+      this.$axios.$get(url).then((response) => {
+        this.news.unshift(response.data);
       });
     },
-    async submitEmployee() {
-      const postEmployeeUrl =
-        "/employees/" +
-        "?group_id=" +
-        this.appGroup +
-        "&name=" +
-        this.employeeName +
-        "&student_id=" +
-        this.employeeStudentId;
+    async submit() {
+      const url =
+        "/news/" +
+        "?title=" +
+        this.title +
+        "&body=" +
+        this.body
 
-      this.$axios.$post(postEmployeeUrl).then((response) => {
-        this.appGroup = "";
-        this.employeeName = "";
-        this.employeeStudentId = "";
-        this.reload();
+      this.$axios.$post(url).then((res) => {
+        this.title = "";
+        this.body = "";
+        this.reload(res.data.id);
         this.closeAddModal();
+        this.openSnackBar("お知らせを作成しました")
       });
     },
   },
