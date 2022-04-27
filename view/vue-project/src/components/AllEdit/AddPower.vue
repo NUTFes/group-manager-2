c644709ce32559b826fc6454d85fd359b75caa5b
TomoeYasuda <tomoe.pierrot3@gmail.com>
2022-04-27 16:40:44 +0900

    修正

--- view/vue-project/src/components/AllEdit/AddPower.vue
+++ view/vue-project/src/components/AllEdit/AddPower.vue
@@ -1,38 +1,22 @@
 <template>
   <transition name="fade" appear>
     <div class="add-modal">
-      <div class="add-modal__container">
-        <div class="add-modal__box">
-          <div class="add-modal_content">
-            <div id="card">
-              <h1>電力追加</h1>
-              <div class="Blank">
-              <div>使用物品名</div>
-                <input type="text" v-model="item" id="item">
-              </div>
-              <div class="Blank">
-                <div>最大定格電力[W]</div>
-                <input type="text" v-model="power" id="power" @change="validationPower">
-              </div>
-              <div class="Blank">
-                <div>メーカー</div>
-                <input type="text" v-model="model" id="model">
-              </div>
-              <div class="Blank">
-                <div>型番</div>
-                <input type="text" v-model="manufacturer" id="manufacturer">
-              </div>
-              <div class="Blank">
-                <div>URL</div>
-                <input type="text" v-model="url" id="url">
-              </div>
-            </div>
-          </div>
-          <div style="display:flex;">
-            <button id="btn" type="button" v-on:click="$emit('closeAddPower')">リセット</button>
-            <button id="btn" type="button" @click="register">✓登録</button>
-          </div>
-        </div>
+      <div class="add-modal_box">
+        <h1>電力追加<button id="btn2" v-on:click="$emit('closeAddPower')">✖</button></h1>
+        <div>使用物品名</div>
+        <input type="text" v-model="item" id="item">
+        <div>最大定格電力[W]</div>
+        <input type="text" v-model="power" id="power" @change="validationPower">
+        <div>メーカー</div>
+        <input type="text" v-model="model" id="model">
+        <div>型番</div>
+        <input type="text" v-model="manufacturer" id="manufacturer">
+        <div>URL</div>
+        <input type="text" v-model="url" id="url">
+        <span style="display:flex;">
+          <button id="btn" type="button" @click="reset">リセット</button>
+          <button id="btn" type="button" @click="register">✓登録</button>
+        </span>
       </div>
     </div>
   </transition>
@@ -67,6 +51,13 @@ export default {
     },
   },
   methods: {
+    reset: function() {
+      this.item = [],
+      this.power = [],
+      this.model = [],
+      this.manufacturer = [],
+      this.url = []
+    },
     onPowerValidation: function() {
       this.resultPower = true;
     },
@@ -136,21 +127,20 @@ export default {
 </script>
 
 <style scoped>
-  #card{
-    margin-right: 15%;
-    margin-left: 15%;
-  }
   #btn{
     background: #032030;
     color: white;
     font-size: 15px;
     font-weight: bold;
     cursor: pointer;
-    width: 80px;
+    width: 30%;
     height: 30px;
     display: block;
-    margin: 3%;
-  }
+    margin-right: 10%;
+    margin-left: 10%;
+    margin-top: 5%;
+    margin-bottom: 5%;
+}
   #btn:hover {
     box-shadow: -2px -2px 5px #FFF, 2px 2px 5px #BABECC;
     background-image: linear-gradient(90deg, rgba(247, 93, 139, 1), rgba(254, 220, 64, 1));
@@ -159,17 +149,29 @@ export default {
   #btn:active{
     box-shadow: inset 1px 1px 2px #BABECC, inset -1px -1px 2px #FFF;
   }
+  #btn2{
+    position: relative;
+    right: -30%;
+  }
   input{
     border: 1px solid silver;
     width: 100%;
   }
-  .add-modal_content div {
-    display: flex;
-    align-items: start;
+  .add-modal_box div {
     justify-content: center;
     flex-flow: column;
-    gap: 10px;
-    width: 100%;
+    width: 80%;
+    margin-top: 3%;
+  }
+  .add-modal_box {
+    display: flex;
+    padding: 10px 10px;
+    margin: 0 25%;
+    justify-content: center;
+    flex-flow: column;
+    background-color: #DADADA;
+    border-radius: 10px;
+    margin: 0 30%;
   }
   .add-modal {
     top: 0;
@@ -181,31 +183,5 @@ export default {
     z-index: 11;
     background-color: rgba(51, 51, 51, 0.3);
     overflow: auto;
-  }
-  .add-modal__container {
-    width: 100%;
-    min-height: 100%;
-    margin: 3% 0 ;
-    justify-content: center;
-    align-items: center;
-  }
-  .add-modal__box {
-    z-index: 15;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-flow: column;
-    padding: 10px 10px;
-    margin: 0 25%;
-    color: black;
-    background: radial-gradient(
-      ellipse at top left,
-      rgba(204,204,204,1),
-      rgba(251, 251, 251, 0.8)
-    );
-    background-color: #DADADA;
-    backdrop-filter: blur(4px);
-    gap: 30px;
-    border-radius: 10px;
   }
 </style>
