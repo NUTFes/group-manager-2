060436cb229493a320bfb2711404e80a54111da3
oba18 <m.oba.0426@gmail.com>
2022-05-07 03:16:43 +0900

    [mod] ステージ，ステージオプション，会場，ユーザーの申請ページの修正

--- view/vue-project/src/views/RegistGroupModel.vue
+++ view/vue-project/src/views/RegistGroupModel.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <router-link to="/mypage" style="text-decoration: none"><span class="regist-back-link">戻る</span></router-link>
+      <router-link to="/mypage" style="text-decoration: none"><span class="regist-back-link">マイページに戻る</span></router-link>
     <div class="regist-title">参加団体の登録</div>
     <div class="regist-card">
       <div class="regist-card-content">
