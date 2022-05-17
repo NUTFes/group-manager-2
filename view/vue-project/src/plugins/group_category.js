503a546ccbba90e50be056a8213e99f5284db1ef
oba18 <m.oba.0426@gmail.com>
2022-05-17 20:14:36 +0900

    [fix] 物品販売を追加

--- view/vue-project/src/plugins/group_category.js
+++ view/vue-project/src/plugins/group_category.js
@@ -1,6 +1,6 @@
 const groupCategoryList = [
   // { id: 1, name: "模擬店(食品販売)" },
-  // { id: 2, name: "模擬店(物品販売)" },
+  { id: 2, name: "模擬店(物品販売)" },
   { id: 3, name: "ステージ企画" },
   { id: 4, name: "展示・体験" },
   // { id: 5, name: "研究室公開" },
