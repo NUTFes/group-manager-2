8d24af0850b6cb511650877928db62a02f20c708
ryuseifujisaki <fujiryu1122@gmail.com>
2023-01-17 16:49:39 +0900

    app.vue削除

--- user_front/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    <h2 class="p-10">padding10pxになる</h2>
-    <NuxtWelcome />
-  </div>
-</template>
