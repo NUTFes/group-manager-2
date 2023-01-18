988f7ef0e5f80f74a4f02f530321e2a5011a8664
harata-t <22.t.harata.nutfes@gmail.com>
2023-01-18 14:55:27 +0900

    [add]フッターコンポーネントの追加

---
+++ user_front/components/footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+    ︎©︎長岡技術科学大学技大祭実行員
+  </div>
+</template>
+
+<style>
+  .footer{
+    font-family: Noto Sans JP;
+    @apply 
+    flex justify-center items-center 
+    absolute inset-x-0 bottom-0
+    border-t-2 mx-4
+    text-xs
+    h-[35px]
+    font-light
+    leading-4 ;
+  }
+</style>
