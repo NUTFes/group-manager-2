3e5eac51bbcd6982d2037778233098f80e064258
aratayo <theancientoceanempire@gmail.com>
2023-01-18 15:35:39 +0900

    rebase

---
+++ user_front/components/Modal.vue
@@ -0,0 +1,115 @@
+<template>
+	<h1 >
+		<!-- <div class="card-shadow bg-red-500 w-20 h-10" >
+
+			<button class="bg-indigo-700 font-semibold text-white rounded-[50%] w-5 h-5 ">×</button>
+			<button class="bg-indigo-700 font-semibold text-white rounded w-15 h-5" >リセット</button>
+			<button class="bg-indigo-700 font-semibold text-white rounded w-15 h-5">登録</button>
+		</div> -->
+		<!-- <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+  id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"> -->
+  <div class="modal-dialog relative w-auto pointer-events-none">
+    <div
+      class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+      <div
+        class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+        <h5 class="text-xl font-medium leading-normal text-gray-800" >申請登録</h5>
+        <button type="button"
+          class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+          data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-footer 
+	   flex 
+	   flex-shrink-0 
+	   flex-wrap 
+	   items-center 
+	   justify-end 
+	   p-4 
+	   <!-- border-t  -->
+	   border-gray-200 
+	   rounded-b-md">
+    	<button type="button" 
+		class="px-6 py-2.5
+      	bg-blue-600
+     	text-white
+      	font-medium
+      	text-xs
+      	leading-tight
+      	rounded
+      	hover:bg-blue-700 hover:shadow-lg
+      	focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+      	active:bg-blue-800 active:shadow-lg
+        " data-bs-dismiss="modal">リセット
+		</button> 
+        <button type="button" 
+		class="px-6 py-2.5
+      	bg-blue-600
+     	text-white
+      	font-medium
+      	text-xs
+      	leading-tight
+      	rounded
+      	hover:bg-blue-700 hover:shadow-lg
+      	focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+      	active:bg-blue-800 active:shadow-lg
+      	">登録
+		</button>
+       </div>
+    </div>
+  </div>
+<!-- </div> -->
+	</h1>
+</template>
+
+<!-- card
+	position: absolute;
+left: 66.76%;
+right: 11.42%;
+top: 4.51%;
+bottom: 28.09%;
+
+background: rgba(234, 234, 234, 0.95);
+border-radius: 10px;
+ -->
+
+<!-- 申請登録
+position: absolute;
+left: 31.59%;
+right: 31.59%;
+top: 10.69%;
+bottom: 84.01%;
+
+font-family: 'Noto Sans JP';
+font-style: normal;
+font-weight: 300;
+font-size: 36px;
+line-height: 52px;
+text-align: center;
+letter-spacing: 0.1em;
+
+color: #000000;
+ -->
+
+ <!-- 削除ボタン
+	position: absolute;
+left: 69.46%;
+right: 27.89%;
+top: 9.78%;
+bottom: 86.15%;
+  -->
+
+	<!--  リセット
+		position: absolute;
+left: 38.73%;
+right: 52.61%;
+top: 78.82%;
+bottom: 17.21%;
+	 -->
+
+	 <!--  登録
+		position: absolute;
+left: 52.54%;
+right: 38.8%;
+top: 78.82%;
+bottom: 17.21%;
+	 -->
