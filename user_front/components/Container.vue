de8cceb439788011d50f81e0cff015f622b6b944
noaikarashi <s211004@stn.nagaokaut.ac.jp>
2023-01-18 15:16:02 +0900

    wip

---
+++ user_front/components/Container.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <span class="flex m-3 bg-white-100">
+       <button class="px-5 bg-gray-200 rounded-[50%] text-pink-500 text-xl font-bold">マイページに戻る</button>
+       <label class="pl-3 text-xl font-bold">参加団体登録・編集ページ </label>
+      </span>
+    <div class="continar lx bg-gray-200">
+      <ul class="list-reset flex border-b pt-1 px-1">
+        <li class="mr-1">
+          <a class="bg-white inline-block py-2 px-4 rounded-t-xl font-semibold" href="#">Tab</a>
+        </li>
+        <!-- <li class="mr-1">
+          <a class="bg-white inline-block py-2 px-4 text-blue hover:text-blue-darker font-semibold" href="#">Tab</a>
+        </li>
+        <li class="mr-1">
+          <a class="bg-white inline-block py-2 px-4 text-blue hover:text-blue-darker font-semibold" href="#">Tab</a>
+        </li>
+        <li class="mr-1">
+          <a class="bg-white inline-block py-2 px-4 text-grey-light font-semibold" href="#">Tab</a>
+        </li> -->
+      </ul>
+    </div>
+    <div class="bg-gray-100 p-3"></div>
+
+  </div>
+
+</template>
