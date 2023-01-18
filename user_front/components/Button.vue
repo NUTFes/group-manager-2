cbb41a9f9a6a5eed5bdd57a39c23135c6f81c01f
sota1024 <21.s.kotake.nutfes@gmail.com>
2023-01-18 17:09:16 +0900

    [fix]review

--- user_front/components/Button.vue
+++ user_front/components/Button.vue
@@ -8,13 +8,14 @@
 .btn{
 	border-radius: 50%;
   background: linear-gradient(-30deg, #032030, #1f4174);
-  @apply tracking-widest;
-  @apply font-sans;
-  @apply not-italic;
-  @apply font-medium;
-  @apply text-4xl;
-  @apply text-gray-100;
-  @apply w-28;
-  @apply h-28;
+  @apply
+    tracking-widest
+    font-sans
+    not-italic
+    font-medium
+    text-4xl
+    text-gray-100
+    w-28
+    h-28;
 }
 </style>
