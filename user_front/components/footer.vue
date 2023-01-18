3a22260436b15023ce9176e6244f94e0cbdae696
harata-t <22.t.harata.nutfes@gmail.com>
2023-01-18 16:34:35 +0900

    レビュー修正完了

--- user_front/components/footer.vue
+++ user_front/components/footer.vue
@@ -8,9 +8,14 @@
   .footer{
     font-family: Noto Sans JP;
     @apply 
-    flex justify-center items-center 
-    absolute inset-x-0 bottom-0
-    border-t-2 mx-4
+    flex
+    justify-center
+    items-center 
+    absolute
+    inset-x-0
+    bottom-0
+    border-t-2
+    mx-4
     text-xs
     h-[35px]
     font-light
