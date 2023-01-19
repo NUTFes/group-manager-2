ffd9f1507e90270415809a4d41c1a5e03c1ff31e
noaikarashi <s211004@stn.nagaokaut.ac.jp>
2023-01-19 12:21:20 +0900

    [mod]タブに各申請名を表示

--- user_front/components/Container.vue
+++ user_front/components/Container.vue
@@ -1,27 +1,29 @@
 <script setup>
+const orderList = [
+  {title: "副代表申請"},
+  {title: "会場申請"},
+  {title: "物品申請"},
+  {title: "電力申請"},
+  {title: "ステージ申請"},
+  {title: "ステージオプション申請"},
+  {title: "従業員申請"},
+  {title: "販売食品申請"},
+  {title: "購入品申請"},
+]
 </script>
 
 <template>
   <div class="font-sans">
     <div class="flex justify-between m-4 bg-white-100 text-xl font-bold">
       <button class="bg-gray-100 text-pink-500 hover:bg-gray-200 hover:shadow-lg">マイページに戻る</button>
-      <div class="flex-auto ml-8">参加団体登録・編集ページ </div>
+      <div class="flex-auto ml-8">参加団体登録・編集ページ</div>
       <select class="bg-gray-100 cursor-pointer">
         <option value="">PopularMusicClub</option>
       </select>
     </div>
     <ul class="flex">
-      <li>
-        <a class="bg-gray-100 inline-block py-2 px-4 rounded-t-xl font-semibold hover:bg-gray-200 hover:shadow-lg cursor-pointer">会場申請</a>
-      </li>
-      <li>
-        <a class="bg-gray-100 inline-block py-2 px-4 rounded-t-xl font-semibold hover:bg-gray-200 hover:shadow-lg cursor-pointer">Tab</a>
-      </li>
-      <li>
-        <a class="bg-gray-100 inline-block py-2 px-4 rounded-t-xl font-semibold hover:bg-gray-200 hover:shadow-lg cursor-pointer">Tab</a>
-      </li>
-      <li>
-        <a class="bg-gray-100 inline-block py-2 px-4 rounded-t-xl font-semibold hover:bg-gray-200 hover:shadow-lg cursor-pointer">Tab</a>
+      <li v-for="order in orderList" :key="order.title">
+        <a class="bg-gray-100 inline-block py-2 px-4 rounded-t-xl font-semibold hover:bg-gray-200 hover:shadow-lg cursor-pointer">{{ order.title }}</a>
       </li>
     </ul>
       <div class="w-full h-screen bg-gray-200"/>
