0de6c0c05c8369ead52f669c7dd1ec37c8fe7873
ryuseifujisaki <fujiryu1122@gmail.com>
2023-02-04 12:25:22 +0900

    追加修正 App.vueの追加のコンポーネントの呼び出し

--- user_front/layouts/default.vue
+++ user_front/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-  <Header/>
+  <Header />
+    <NuxtPage />
   <Footer />
-  <Group />
   </div>
 </template>
--- user_front/pages/home.vue
+++ user_front/pages/home.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
+
 </script>
 
 <template>
   <div>
-    index
+    <Group />
   </div>
 </template>
