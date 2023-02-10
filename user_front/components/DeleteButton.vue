d09d8425c16cccb31720e754e1959f827d8271b4
aratayo <theancientoceanempire@gmail.com>
2023-02-10 20:29:25 +0900

    ボタンコンポ―ネントの追加(編集、削除、リセット、登録)

---
+++ user_front/components/DeleteButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button class="btn">
+      削除
+      </button>
+  </div>
+</template>
+
+<style>
+.btn{
+  background: #FF6262;
+  border-radius: 4.23358px;
+  @apply
+    tracking-widest
+    font-sans
+    not-italic
+    font-normal
+    text-2xl
+    text-white
+    w-28
+    h-9;
+}
+</style>
