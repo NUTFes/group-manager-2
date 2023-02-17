7b2f5e0888ca8d2d8b56cea722828bb07a510ec2
noaikarashi <s211004@stn.nagaokaut.ac.jp>
2023-02-17 16:12:27 +0900

    [WIP]

--- user_front/components/RegistInfo/delete/index.vue
+++ user_front/components/RegistInfo/delete/index.vue
@@ -1,27 +1,26 @@
 <script lang="ts" setup>
+
 interface Props {
   name: string
-  // to?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  name: '',
-  // to: '/'
+  name: 'aaaa',
 })
 </script>
 
 <template>
     <transition name="fade" appear>
-      <div class="add-modal">
-        <div class="add-modal_box">
-          <div id="btnContainer">
+      <div class="fixed top-0 left-0 p-24 w-full h-full z-10 bg-[#333]/30">
+        <div class="flex mx-[30%] flex-col justify-center rounded-xl bg-[#dadada]">
+          <div class="flex justify-end mr-4">
             <button v-on:click="$emit('closeDeleteItem')">✖</button>
           </div>
-          <p class="text-2xl text-center font-bold">{{ name }}の削除</p>
-          <!-- <span style="display:flex;">
-            <button id="btn" type="button" v-on:click="$emit('closeDeleteItem')">戻る</button>
-            <button id="btn" type="button" @click="deleteItem">✓削除</button>
-          </span> -->
+          <div class="text-2xl text-center font-bold">{{ name }}の削除</div>
+          <span class="flex justify-around mx-16 my-4">
+            <ResetButton />
+            <RegistButton />
+          </span>
         </div>
       </div>
     </transition>
