a0a09c66d7a3276ed6d512aa73a1d813ffd44ca3
noaikarashi <s211004@stn.nagaokaut.ac.jp>
2023-02-19 15:02:50 +0900

    [add]追加、削除モーダルの追加

--- user_front/components/RegistInfo/add/Item.vue
+++ user_front/components/RegistInfo/add/Item.vue
@@ -1,7 +1,18 @@
 <script lang="ts" setup>
+import Modal from '../../Modal.vue';
+import RegistButton from '~~/components/RegistButton.vue';
+import ResetButton from '~~/components/ResetButton.vue';
+
 </script>
 
 <template>
-  <div>
-  </div>
+  <Modal title="貸出物品の追加">
+    <template #form>
+      aaaa
+    </template>
+    <template #method>
+      <RegistButton />
+      <ResetButton />
+    </template>
+  </Modal>
 </template>
