ca4f5dcaa25b26446ea1fec4da4b17e3948f92ff
noaikarashi <s211004@stn.nagaokaut.ac.jp>
2023-02-20 17:48:36 +0900

    [mod]ログインユーザーが持つデータの取得

--- user_front/components/RegistInfo/card/Item.vue
+++ user_front/components/RegistInfo/card/Item.vue
@@ -1,8 +1,61 @@
 <script lang="ts" setup>
+import axios from 'axios'
+
+const config = useRuntimeConfig()
+
+const item = ref("")
+const num = ref('')
+const resultNum = ref(0)
+const itemList = ref([""])
+const setting = ref("")
+const groupId = ref("")
+
+
+
+onMounted(() => {
+  const url = config.baseURL + "/api/v1/current_user/current_regist_info";
+    axios
+      .get(url, {
+        headers: {
+          "Content-Type": "application/json",
+          "access-token": localStorage.getItem("access-token"),
+          client: localStorage.getItem("client"),
+          uid: localStorage.getItem("uid"),
+      },
+        })
+        .then((response) => {
+          itemList.value = response.data.data[0].rental_orders;
+        });
+  console.log(itemList.value)
+  const settingurl = config.APIURL + "/user_page_settings";
+  axios.get(settingurl).then((response) => {
+    setting.value = response.data.data[0];
+  });
+})
+
+// const register = () => {
+//   if (!!item.value && resultNum.value > 0) {
+//     axios.defaults.headers.common["Content-Type"] = "application/json";
+//     let params = new URLSearchParams();
+//     params.append("group_id", groupId.value);
+//     params.append("rental_item_id", item.value);
+//     params.append("num", num.value);
+//     axios
+//       .post(config.app.baseURL + "/rental_orders", params)
+//       .then((response) => {
+//         console.log(response);
+//         // $emit("closeAddItem");
+//         // $emit("reload");
+//       });
+//   }
+// }
 
 </script>
 
 <template>
+  {{ setting }}
+  <div>title</div>
+  {{ itemList }}
   <RegistInfoNarrowCard>
     <template #body>
       <div class="absolute w-full mt-8 text-4xl font-[350] text-center">
