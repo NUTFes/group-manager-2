ec7119a5743de9659e158460b317472a44c27b80
noaikarashi <s211004@stn.nagaokaut.ac.jp>
2023-02-26 15:53:44 +0900

    [mod]タブ切り替え、参加団体名の表示

--- user_front/components/Container.vue
+++ user_front/components/Container.vue
@@ -1,31 +1,32 @@
 <script lang="ts" setup>
-const orderList = [
-  {title: "副代表申請"},
-  {title: "会場申請"},
-  {title: "物品申請"},
-  {title: "電力申請"},
-  {title: "ステージ申請"},
-  {title: "ステージオプション申請"},
-  {title: "従業員申請"},
-  {title: "販売食品申請"},
-  {title: "購入品申請"},
-]
+const router = useRouter()
+
+interface Props {
+  name: string
+}
+
+const group = withDefaults(defineProps<Props>(),{
+  name: ''
+})
+
 </script>
 
 <template>
   <div class="font-sans">
     <div class="flex justify-between m-4 bg-white-100 text-xl font-bold">
-      <button class="bg-gray-100 text-pink-500 hover:bg-gray-200 hover:shadow-lg">マイページに戻る</button>
+      <button @click="router.push('/mypage')" class="bg-gray-100 text-pink-500 hover:bg-gray-200 hover:shadow-lg">
+        マイページに戻る
+      </button>
       <div class="flex-auto ml-8">参加団体登録・編集ページ</div>
-      <select class="bg-gray-100 cursor-pointer">
-        <option value="">PopularMusicClub</option>
-      </select>
+      <div class="bg-gray-100 tracking-wide">{{ group.name }}</div>
     </div>
-    <ul class="flex">
+    <!-- <ul class="flex">
       <li v-for="order in orderList" :key="order.title">
-        <a class="title">{{ order.title }}</a>
+        <div :class="{ clickBtn: tabController === order.tab }" @click="tabController = order.tab" class="title">
+          {{ order.title }}
+        </div>
       </li>
-    </ul>
+    </ul> -->
       <div class="height">
         <slot name="body"/>
       </div>
@@ -47,9 +48,13 @@ const orderList = [
 }
 
 .height {
-  height: 80vh;
   @apply
+    min-h-[100vh]
     w-full
     bg-gray-200
+}
+
+.clickBtn {
+  background-color: darkorchid;
 }
 </style>
--- user_front/pages/regist_info/index.vue
+++ user_front/pages/regist_info/index.vue
@@ -1,15 +1,20 @@
 <script lang="ts" setup>
 import axios from 'axios';
+import { Group } from '~~/types';
 
 const config = useRuntimeConfig()
 
+
 const url = config.APIURL + "/api/v1/current_user/current_regist_info";
+
+const tab = useState("tab", () => 1)
 
 interface RegistInfo {
   sub_rep: SubRep
   place_orders: PlaceOrder
   power_orders: PowerOrder
   rental_orders: RentalOrder
+  group: Group
 }
 
 interface Stage {
@@ -118,54 +123,65 @@ onMounted(() => {
 </script>
 
 <template>
-<Container>
+<Container :name="registInfo?.group.name">
   <template #body>
+    <div class="flex">
+      <RegistButton @click="tab = 1" />
+      <RegistButton @click="tab = 2" />
+      <RegistButton @click="tab = 4" />
+      <RegistButton @click="tab = 5" />
+      <RegistButton @click="tab = 6" />
+      <RegistButton @click="tab = 7" />
+      <RegistButton @click="tab = 8" />
+      <RegistButton @click="tab = 9" />
+    </div>
     <div class="ml-12 pt-4">
-    <!-- ステージ申請 group_category_id === ３ -->
-      <div v-for="s in stageOrder" :key="s">
-        <RegistInfoCardStage
-          :date="s.stage_order.date"
-          :first-stage="s.stage_order.stage_first"
-          :second-stage="s.stage_order.stage_second"
-          :is-sunny="s.stage_order.is_sunny"
+      <!-- 副代表申請  -->
+        <RegistInfoCardSubRep v-show="tab === 1"
+          :name="subRep?.name"
+          :department="subRep?.department"
+          :grade="subRep?.grade"
+          :student_id="subRep?.student_id"
+          :email="subRep?.email"
+          :tel="subRep?.tel"
+          />
+          <!-- 会場申請 group_category_id !== ３ -->
+          <div v-show="tab === 2">
+            <RegistInfoCardPlace :n="1" :place="placeOrder?.first" :remark="placeOrder?.remark" />
+            <RegistInfoCardPlace :n="2" :place="placeOrder?.second" :remark="placeOrder?.remark" />
+            <RegistInfoCardPlace :n="3" :place="placeOrder?.third" :remark="placeOrder?.remark" />
+          </div>
+          <!-- ステージ申請 group_category_id === ３ -->
+          <div v-show="tab === 3" v-for="s in stageOrder" :key="s">
+            <RegistInfoCardStage
+              :date="s.stage_order.date"
+              :first-stage="s.stage_order.stage_first"
+              :second-stage="s.stage_order.stage_second"
+              :is-sunny="s.stage_order.is_sunny"
+            />
+          </div>
+      <!-- ステージオプション申請 group_category_id === ３ -->
+        <RegistInfoCardStageOption v-show="tab === 4"
+          :own-equipment="stageOption?.own_equipment"
+          :bgm="stageOption?.bgm"
+          :camera-permission="stageOption?.camera_permission"
+          :loud-sound="stageOption?.loud_sound"
+          :stage-content="stageOption?.stage_content"
         />
-      </div>
-    <!-- ステージオプション申請 group_category_id === ３ -->
-      <RegistInfoCardStageOption
-        :own-equipment="stageOption?.own_equipment"
-        :bgm="stageOption?.bgm"
-        :camera-permission="stageOption?.camera_permission"
-        :loud-sound="stageOption?.loud_sound"
-        :stage-content="stageOption?.stage_content"
-      />
-    <!-- 副代表申請  -->
-      <RegistInfoCardSubRep
-        :name="subRep?.name"
-        :department="subRep?.department"
-        :grade="subRep?.grade"
-        :student_id="subRep?.student_id"
-        :email="subRep?.email"
-        :tel="subRep?.tel"
-      />
 
-    <!-- 会場申請 group_category_id !== ３ -->
-      <RegistInfoCardPlace :n="1" :place="placeOrder?.first" :remark="placeOrder?.remark" />
-      <RegistInfoCardPlace :n="2" :place="placeOrder?.second" :remark="placeOrder?.remark" />
-      <RegistInfoCardPlace :n="3" :place="placeOrder?.third" :remark="placeOrder?.remark" />
-
-    <!-- 電力申請 -->
-      <div v-for="p in powerOrder" :key="p">
-        <RegistInfoCardPower
-          :item="p.power_order.item"
-          :power="p.power_order.power"
-          :manufacturer="p.power_order.manufacturer"
-          :model="p.power_order.model"
-          :url="p.power_order.item_url"
-        />
-      </div>
+      <!-- 電力申請 -->
+        <div v-show="tab === 5" v-for="p in powerOrder" :key="p">
+          <RegistInfoCardPower
+            :item="p.power_order.item"
+            :power="p.power_order.power"
+            :manufacturer="p.power_order.manufacturer"
+            :model="p.power_order.model"
+            :url="p.power_order.item_url"
+          />
+        </div>
 
     <!-- 物品申請 -->
-      <div class="flex">
+      <div v-show="tab === 6" class="flex">
         <div v-for="item in rentalOrder" :key="item">
           <RegistInfoCardItem
             :name=item.rental_item.name
