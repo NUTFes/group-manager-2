2aa17559b64064f7555e2f652365498b9f73d6d5
ryuseifujisaki <fujiryu1122@gmail.com>
2023-03-06 00:16:44 +0900

    food情報の登録機能の作成

--- user_front/pages/regist/food/index.vue
+++ user_front/pages/regist/food/index.vue
@@ -1,4 +1,37 @@
 <script lang="ts" setup>
+
+const config = useRuntimeConfig();
+const router = useRouter();
+
+const registerParams = reactive({
+  dishName: "",
+  isCooking: false,
+  numFirstDay: 0,
+  numSecondDay: 0,
+  groupId: 0,
+});
+
+onMounted(async () =>{
+  registerParams.groupId = Number(localStorage.getItem("group_id"));
+})
+
+const registerFood = async () => {
+  await $fetch(config.APIURL + "/food_products", {
+    method: "POST",
+    params: {
+      group_id: registerParams.groupId,
+      name: registerParams.dishName,
+      is_cooking: registerParams.isCooking,
+      first_day_num: registerParams.numFirstDay,
+      second_day_num: registerParams.numSecondDay,
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  router.push("/mypage");
+};
+
 </script>
 
 <template>
@@ -7,31 +40,35 @@
       <Card>
         <h1 class="text-3xl">Registration of foodstuffs</h1>
         <Card border="none" align="end" gap="20px">
+
           <div class="flex">
             <p class="label">name</p>
-            <input class="form" />
+            <input class="form" v-model="registerParams.dishName">
           </div>
+
           <div class="flex">
             <p class="label">Do you cook?</p>
-            <select style="width:180px;">
+            <select style="width:180px;" v-model="registerParams.isCooking">
               <option value="" selected disabled></option>
               <option value='true'>Yes</option>
               <option value='false'>No</option>
             </select>
           </div>
+
           <div class="flex">
             <p class="label">number of products on the first day</p>
-            <input class="form" />
+            <input class="form" v-model="registerParams.numFirstDay">
           </div>
+
           <div class="flex">
             <p class="label">number of products on the second day</p>
-            <input class="form" />
+            <input class="form" v-model="registerParams.numSecondDay">
           </div>
+
         </Card>
         <Row>
-          <RegistButton />
-          <ResetButton />
-          <AddButton />
+          <RegistPageButton text="reset"></RegistPageButton>
+          <RegistPageButton @click="registerFood" text="登録"></RegistPageButton>
         </Row>
       </Card>
     </div>
