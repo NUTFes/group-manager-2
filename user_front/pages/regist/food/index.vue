7e7a2b84b7a4bc78cf74d9544301d16eab198192
ryuseifujisaki <fujiryu1122@gmail.com>
2023-03-06 16:49:51 +0900

    食品の複数登録作成完了

--- user_front/pages/regist/food/index.vue
+++ user_front/pages/regist/food/index.vue
@@ -2,34 +2,55 @@
 
 const config = useRuntimeConfig();
 const router = useRouter();
+const formCount = ref(1);
 
-const registerParams = reactive({
-  dishName: "",
-  isCooking: false,
-  numFirstDay: 0,
-  numSecondDay: 0,
+const state = reactive({
   groupId: 0,
 });
 
 onMounted(async () =>{
-  registerParams.groupId = Number(localStorage.getItem("group_id"));
+  state.groupId = Number(localStorage.getItem("group_id"));
 })
 
+const registerParams = [reactive({
+  dishName: "",
+  isCooking: false,
+  numFirstDay: 0,
+  numSecondDay: 0,
+})];
+
 const registerFood = async () => {
-  await $fetch(config.APIURL + "/food_products", {
+  for (let i =0; i < formCount.value; i++){
+    await $fetch(config.APIURL + "/food_products", {
     method: "POST",
     params: {
-      group_id: registerParams.groupId,
-      name: registerParams.dishName,
-      is_cooking: registerParams.isCooking,
-      first_day_num: registerParams.numFirstDay,
-      second_day_num: registerParams.numSecondDay,
+      group_id: state.groupId,
+      name: registerParams[i].dishName,
+      is_cooking: registerParams[i].isCooking,
+      first_day_num: registerParams[i].numFirstDay,
+      second_day_num: registerParams[i].numSecondDay,
     },
     headers: {
       "Content-Type": "application/json",
     },
   });
   router.push("/mypage");
+  }
+};
+
+const increment = () => {
+  formCount.value++;
+  registerParams.push(reactive({
+    dishName: "",
+    isCooking: false,
+    numFirstDay: 0,
+    numSecondDay: 0,
+  }));
+};
+
+const decrement = () => {
+  formCount.value--;
+  registerParams.pop();
 };
 
 </script>
@@ -41,14 +62,15 @@ const registerFood = async () => {
         <h1 class="text-3xl">Registration of foodstuffs</h1>
         <Card border="none" align="end" gap="20px">
 
+        <div v-for="count , i in formCount">
           <div class="flex">
             <p class="label">name</p>
-            <input class="form" v-model="registerParams.dishName">
+            <input class="form" v-model="registerParams[i].dishName">
           </div>
 
           <div class="flex">
             <p class="label">Do you cook?</p>
-            <select style="width:180px;" v-model="registerParams.isCooking">
+            <select style="width:180px;" v-model="registerParams[i].isCooking">
               <option value="" selected disabled></option>
               <option value='true'>Yes</option>
               <option value='false'>No</option>
@@ -57,17 +79,22 @@ const registerFood = async () => {
 
           <div class="flex">
             <p class="label">number of products on the first day</p>
-            <input class="form" v-model="registerParams.numFirstDay">
+            <input class="form" v-model="registerParams[i].numFirstDay">
           </div>
 
           <div class="flex">
             <p class="label">number of products on the second day</p>
-            <input class="form" v-model="registerParams.numSecondDay">
+            <input class="form" v-model="registerParams[i].numSecondDay">
           </div>
+          <div v-if="i != 0">
+            <RegistPageButton @click="decrement" text="削除"></RegistPageButton>
+          </div>
+        </div>
 
         </Card>
         <Row>
           <RegistPageButton text="reset"></RegistPageButton>
+          <RegistPageButton text="Add form" @click="increment"></RegistPageButton>
           <RegistPageButton @click="registerFood" text="登録"></RegistPageButton>
         </Row>
       </Card>
