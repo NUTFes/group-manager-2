2c92dd4cfb349f240bfb31dce9fcee8761d5fc08
noaikarashi <s211004@stn.nagaokaut.ac.jp>
2023-04-29 17:24:04 +0900

    [add]多言語対応追加

--- user_front/pages/index.vue
+++ user_front/pages/index.vue
@@ -1,30 +1,12 @@
-<script lang="ts" setup>
-  import { useField, useForm } from 'vee-validate';
-  import { object, string, number } from 'yup';
-
-  const schema = object({
-    studentId: string().matches(/^[0-9]{8}$/, "半角数字のみ8桁").required("必須項目"),
-    password: string().matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, "半角英字と半角数字を1文字以上使用し8桁以上").required("必須項目")
-  });
-
-  const { errors } = useForm({
-    validationSchema: schema,
-  });
-
-  const { value: studentId } = useField('studentId');
-  const { value: password, meta } = useField('password');
-
-</script>
-
 <template>
-  <h1>Vee-Validateでフォームバリデーション</h1>
-  <div><label for="studentId">学籍番号</label></div>
-  <input type="text" v-model="studentId" id="studentId"/>
-  <p>{{ errors.studentId }}</p>
   <div>
-    <label for="password" >パスワード(半角数字のみ8桁)</label>
-    <Button />
+    <h1>{{ $t('hello', { name: 'vue-i18n' }) }}</h1>
+    <form>
+      <label for="locale-select">{{ $t('language') }}: </label>
+      <select id="locale-select" v-model="$i18n.locale">
+        <option value="ja">ja</option>
+        <option value="en">en</option>
+      </select>
+    </form>
   </div>
-  <input type="text" v-model="password" id="password"/>
-  <p>{{ errors.password }}</p>
 </template>
