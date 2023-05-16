268d855e106f8e7a6218660431d2e2efeca6ad31
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-16 18:16:42 +0900

    [fix] 不要なインポートを削除

--- user_front/components/RegistInfo/add/Item.vue
+++ user_front/components/RegistInfo/add/Item.vue
@@ -2,7 +2,6 @@
 import { Item, ItemList } from "@/types/regist/item"
 import { useField, useForm } from 'vee-validate'
 import { editItemSchema } from '~/utils/validate'
-import { RentalOrders } from "~~/types/mypage/registAlarm"
 const config = useRuntimeConfig()
 
 const { meta, isSubmitting } = useForm({
