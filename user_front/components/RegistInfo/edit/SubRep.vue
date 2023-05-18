c0fb6d37c4339689fe256dfd0ecf5a1b6e8dddd3
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-19 01:18:18 +0900

    [fix] 副代表編集の学科を学年によって動的に変化させる

--- user_front/components/RegistInfo/edit/SubRep.vue
+++ user_front/components/RegistInfo/edit/SubRep.vue
@@ -1,30 +1,29 @@
 <script lang="ts" setup>
-import { departmentList } from "~/utils/list";
-import { gradeList } from "~/utils/list";
+import { gradeList, GradeWithDepartmentList } from "~/utils/list";
 import { useField, useForm } from "vee-validate";
 import { subRepSchema } from "~/utils/validate";
 const config = useRuntimeConfig();
 
 interface Props {
-  id: number | null
-  groupId: number | null
-  name: string
-  department_id: number | null
-  grade_id: number | null
-  tel: string
-  email: string
-  student_id: number | null
+  id: number | null;
+  groupId: number | null;
+  name: string;
+  department_id: number | null;
+  grade_id: number | null;
+  tel: string;
+  email: string;
+  student_id: number | null;
 }
 const props = withDefaults(defineProps<Props>(), {
   id: null,
   groupId: null,
-  name: '',
+  name: "",
   department_id: null,
   grade_id: null,
-  tel: '',
-  email: '',
+  tel: "",
+  email: "",
   student_id: null,
-})
+});
 
 const { meta, isSubmitting } = useForm({
   validationSchema: subRepSchema,
@@ -35,33 +34,36 @@ const { meta, isSubmitting } = useForm({
     email: props.email,
     department: props.department_id,
     grade: props.grade_id,
-  }
+  },
 });
 const { handleChange: handleName, errorMessage: nameError } = useField("name");
-const { handleChange: handleStudentId, errorMessage: studentIdError } = useField("studentId");
+const { handleChange: handleStudentId, errorMessage: studentIdError } =
+  useField("studentId");
 const { handleChange: handleTel, errorMessage: telError } = useField("tel");
 const { handleChange: handleMail, errorMessage: mailError } = useField("email");
-const { handleChange: handleDepartmentId, errorMessage: departmentIdError } = useField("department");
-const { handleChange: handleGradeId, errorMessage: gradeIdError } = useField("grade");
+const { handleChange: handleDepartmentId, errorMessage: departmentIdError } =
+  useField("department");
+const { handleChange: handleGradeId, errorMessage: gradeIdError } =
+  useField("grade");
 
 interface Emits {
-  (e: 'update:editSubRep', isEditSubRep: boolean): void
-  (e: 'reloadSubRep', v: null): void
+  (e: "update:editSubRep", isEditSubRep: boolean): void;
+  (e: "reloadSubRep", v: null): void;
 }
-const emits = defineEmits<Emits>()
+const emits = defineEmits<Emits>();
 const closeEditSubRep = () => {
-  emits('update:editSubRep', false)
-}
+  emits("update:editSubRep", false);
+};
 const reloadSubRep = () => {
-  emits('reloadSubRep', null)
-}
+  emits("reloadSubRep", null);
+};
 
-const newName = ref<Props['name']>(props.name)
-const newDepartment = ref<Props['department_id']>(props.department_id)
-const newGrade = ref<Props['grade_id']>(props.grade_id)
-const newTel = ref<Props['tel']>(props.tel)
-const newEmail = ref<Props['email']>(props.email)
-const newStudentId = ref<Props['student_id']>(props.student_id)
+const newName = ref<Props["name"]>(props.name);
+const newDepartment = ref<Props["department_id"]>(props.department_id);
+const newGrade = ref<Props["grade_id"]>(props.grade_id);
+const newTel = ref<Props["tel"]>(props.tel);
+const newEmail = ref<Props["email"]>(props.email);
+const newStudentId = ref<Props["student_id"]>(props.student_id);
 // const newSetting = ref<boolean>()
 
 const editSubRep = async () => {
@@ -77,8 +79,8 @@ const editSubRep = async () => {
         email: newEmail.value,
         student_id: newStudentId.value,
       },
-    })
-  }else {
+    });
+  } else {
     await useFetch(config.APIURL + "/sub_reps/" + props.id, {
       method: "PUT",
       params: {
@@ -90,59 +92,119 @@ const editSubRep = async () => {
         email: newEmail.value,
         student_id: newStudentId.value,
       },
-    })
+    });
   }
-  reloadSubRep()
-  closeEditSubRep()
+  reloadSubRep();
+  closeEditSubRep();
 };
 
 const reset = () => {
-  newName.value = ''
-  newDepartment.value = null
-  newGrade.value = null
-  newTel.value = ''
-  newEmail.value = ''
-  newStudentId.value = null
-}
+  newName.value = "";
+  newDepartment.value = null;
+  newGrade.value = null;
+  newTel.value = "";
+  newEmail.value = "";
+  newStudentId.value = null;
+};
+
+const currentDepartmentList = ref<{ id: number; name: string }[]>([]);
+const createCurrentDepartmentList = (e: any) => {
+  currentDepartmentList.value = GradeWithDepartmentList.filter((grade) => {
+    return grade.id === Number(e.target.value);
+  })[0].departmentList;
+};
 </script>
 
 <template>
   <Modal :title="$t('Subrep.editSubrepresentative')">
     <template #close>
       <div class="flex justify-end">
-        <button @click="closeEditSubRep()" class="hover:text-black hover:opacity-75">✖</button>
+        <button
+          @click="closeEditSubRep()"
+          class="hover:text-black hover:opacity-75"
+        >
+          ✖
+        </button>
       </div>
     </template>
     <template #form>
-      <div class="text">{{ $t('Subrep.name') }}</div>
-      <input class="entry" v-model="newName" @change="handleName" :class="{'error_border': nameError}" />
+      <div class="text">{{ $t("Subrep.name") }}</div>
+      <input
+        class="entry"
+        v-model="newName"
+        @change="handleName"
+        :class="{ error_border: nameError }"
+      />
       <div class="error_msg">{{ nameError }}</div>
-      <div class="text">{{ $t('Subrep.studentId') }}</div>
-      <input class="entry" v-model="newStudentId" maxlength="8" @change="handleStudentId" :class="{'error_border': studentIdError }" />
+      <div class="text">{{ $t("Subrep.studentId") }}</div>
+      <input
+        class="entry"
+        v-model="newStudentId"
+        maxlength="8"
+        @change="handleStudentId"
+        :class="{ error_border: studentIdError }"
+      />
       <div class="error_msg">{{ studentIdError }}</div>
-      <div class="text">{{ $t('Subrep.department') }}</div>
-      <select class="entry" v-model="newDepartment" @change="handleDepartmentId" :class="{ 'error_border': departmentIdError }">
-        <option v-for="department in departmentList" :value="department.id" :key="department.id">
-          {{ department.name }}
-        </option>
-      </select>
       <div class="error_msg">{{ departmentIdError }}</div>
-      <div class="text">{{ $t('Subrep.grade') }}</div>
-      <select class="entry" v-model="newGrade" @change="handleGradeId" :class="{ 'error_border': gradeIdError }">
+      <div class="text">{{ $t("Subrep.grade") }}</div>
+      <select
+        class="entry"
+        v-model="newGrade"
+        @change="
+          (e) => {
+            handleGradeId(e);
+            createCurrentDepartmentList(e);
+          }
+        "
+        :class="{ error_border: gradeIdError }"
+      >
         <option v-for="grade in gradeList" :value="grade.id" :key="grade.id">
           {{ grade.name }}
         </option>
       </select>
       <div class="error_msg">{{ gradeIdError }}</div>
-      <div class="text">{{ $t('Subrep.mail') }}</div>
-      <input class="entry" v-model="newEmail" @change="handleMail" :class="{ 'error_border': mailError }" />
+      <div class="text">{{ $t("Subrep.department") }}</div>
+      <select
+        class="entry"
+        v-model="newDepartment"
+        @change="handleDepartmentId"
+        :class="{ error_border: departmentIdError }"
+      >
+        <option
+          v-for="department in currentDepartmentList"
+          :value="department.id"
+          :key="department.id"
+        >
+          {{ department.name }}
+        </option>
+      </select>
+      <div class="text">{{ $t("Subrep.mail") }}</div>
+      <input
+        class="entry"
+        v-model="newEmail"
+        @change="handleMail"
+        :class="{ error_border: mailError }"
+      />
       <div class="error_msg">{{ mailError }}</div>
-      <div class="text">{{ $t('Subrep.tel') }}</div>
-      <input class="entry" v-model="newTel" @change="handleTel" maxlength="11" :class="{ 'error_border': telError }" />
+      <div class="text">{{ $t("Subrep.tel") }}</div>
+      <input
+        class="entry"
+        v-model="newTel"
+        @change="handleTel"
+        maxlength="11"
+        :class="{ error_border: telError }"
+      />
       <div class="error_msg">{{ telError }}</div>
       <div class="flex justify-between mt-8 mx-8">
-        <RegistPageButton :text="$t('Button.reset')" @click="reset()"></RegistPageButton>
-        <RegistPageButton :disabled="!meta.valid || isSubmitting" :text="$t('Button.edit')" @click="editSubRep()"></RegistPageButton>
+        <RegistPageButton
+          :text="$t('Button.reset')"
+          @click="reset()"
+        ></RegistPageButton>
+        <RegistPageButton
+          :disabled="!meta.valid || isSubmitting"
+          :text="$t('Button.edit')"
+          @click="editSubRep()"
+        ></RegistPageButton>
       </div>
     </template>
   </Modal>
@@ -150,10 +212,10 @@ const reset = () => {
 
 <style scoped>
 .error_msg {
-  @apply mx-[10%] text-rose-600
+  @apply mx-[10%] text-rose-600;
 }
 .error_border {
-  @apply border-2 border-rose-600
+  @apply border-2 border-rose-600;
 }
 .text {
   margin: 3% 10% 0%;
