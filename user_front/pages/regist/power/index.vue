7c8c920f3dc1f79889f7e95168f61c17977b3b19
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-23 02:30:07 +0900

    [fix] 電力申請のデザイン修正

--- user_front/pages/regist/power/index.vue
+++ user_front/pages/regist/power/index.vue
@@ -6,33 +6,37 @@ import { loginCheck } from "@/utils/methods";
 const initialData = {
   powers: [
     {
-      productName: '',
+      productName: "",
       maxPower: 0,
-      manufacturer: '',
-      model: '',
-      url: '',
+      manufacturer: "",
+      model: "",
+      url: "",
     },
   ],
 };
 
 const reset = (idx: number) => {
-  registerParams[idx].productName = "",
-  registerParams[idx].maxPower = 0,
-  registerParams[idx].manufacturer = "",
-  registerParams[idx].model = "",
-  registerParams[idx].url = ""
-}
+  (registerParams[idx].productName = ""),
+    (registerParams[idx].maxPower = 0),
+    (registerParams[idx].manufacturer = ""),
+    (registerParams[idx].model = ""),
+    (registerParams[idx].url = "");
+};
 
 const { meta, isSubmitting } = useForm({
   validationSchema: powerSchema,
   initialValues: initialData,
 });
 
-const { fields: powerValidate, push: addValidate, remove: removeValidate } = useFieldArray('powers')
+const {
+  fields: powerValidate,
+  push: addValidate,
+  remove: removeValidate,
+} = useFieldArray("powers");
 
 const config = useRuntimeConfig();
 const router = useRouter();
-const formCount = ref(1)
+const formCount = ref(1);
 
 let registerParams = [
   reactive({
@@ -41,8 +45,8 @@ let registerParams = [
     manufacturer: "",
     model: "",
     url: "",
-  },)
-]
+  }),
+];
 const state = reactive({
   groupId: 0,
 });
@@ -54,7 +58,7 @@ onMounted(async () => {
 });
 
 const increment = () => {
-  formCount.value++
+  formCount.value++;
   registerParams.push(
     reactive({
       productName: "",
@@ -63,14 +67,20 @@ const increment = () => {
       model: "",
       url: "",
     })
-  )
-  addValidate({ productName: '', maxPower: 0, manufacturer: '', model: '', url: '' })
-}
+  );
+  addValidate({
+    productName: "",
+    maxPower: 0,
+    manufacturer: "",
+    model: "",
+    url: "",
+  });
+};
 
 const decrement = (idx: number) => {
-  formCount.value--
-  removeValidate(idx)
-}
+  formCount.value--;
+  removeValidate(idx);
+};
 
 const registerPower = async () => {
   for (let i = 0; i < formCount.value; i++) {
@@ -92,92 +102,136 @@ const registerPower = async () => {
   router.push("/regist/employees");
 };
 
-const skip = () =>{
+const skip = () => {
   router.push("/regist/employees");
 };
 
-const back = () =>{
+const back = () => {
   router.push("/regist/item");
-}
-
+};
 </script>
 
 <template>
   <div class="mx-[20%] my-[5%]">
     <Card>
-      <h1 class="text-3xl">{{ $t('Power.registPower') }}</h1>
-      <Card border="none" align="end" gap="20px">
-        <div v-for="(field, idx) in powerValidate" :key="field.key">
-          <div class="flex">
-            <p class="label">{{ $t('Power.name') }}</p>
-            <Field
-              :id="`productName${idx}`"
-              :name="`powers[${idx}].productName`"
-              type="text"
-              class="form"
-              v-model="registerParams[idx].productName"
-            />
+      <h1 class="text-3xl">{{ $t("Power.registPower") }}</h1>
+      <Card border="none" align="center" gap="20px">
+        <div
+          v-for="(field, idx) in powerValidate"
+          :key="field.key"
+          class="border rounded-md p-4 flex flex-col items-center gap-4"
+        >
+          <div class="grid grid-cols-2 gap-y-2">
+            <p class="label">{{ $t("Power.name") }}</p>
+            <div class="flex flex-col gap-2">
+              <Field
+                :id="`productName${idx}`"
+                :name="`powers[${idx}].productName`"
+                type="text"
+                class="form"
+                v-model="registerParams[idx].productName"
+              />
+              <ErrorMessage
+                class="text-rose-600 text-sm"
+                :name="`powers[${idx}].productName`"
+              />
+            </div>
+            <p class="label">{{ $t("Power.maximum") }}</p>
+            <div class="flex flex-col">
+              <div class="flex gap-2">
+                <Field
+                  :id="`maxPower${idx}`"
+                  :name="`powers[${idx}].maxPower`"
+                  type="number"
+                  class="form"
+                  v-model="registerParams[idx].maxPower"
+                />
+                <p>[W]</p>
+              </div>
+              <ErrorMessage
+                class="text-rose-600 text-sm"
+                :name="`powers[${idx}].maxPower`"
+              />
+            </div>
+            <p class="label">{{ $t("Power.maker") }}</p>
+            <div class="flex flex-col">
+              <Field
+                :id="`manufacturer${idx}`"
+                :name="`powers[${idx}].manufacturer`"
+                type="text"
+                class="form"
+                v-model="registerParams[idx].manufacturer"
+              />
+              <ErrorMessage
+                class="text-rose-600 text-sm"
+                :name="`powers[${idx}].manufacturer`"
+              />
+            </div>
+            <p class="label">{{ $t("Power.model") }}</p>
+            <div class="flex flex-col">
+              <Field
+                :id="`model${idx}`"
+                :name="`powers[${idx}].model`"
+                type="text"
+                class="form"
+                v-model="registerParams[idx].model"
+              />
+              <ErrorMessage
+                class="text-rose-600 text-sm"
+                :name="`powers[${idx}].model`"
+              />
+            </div>
+            <p class="label">{{ $t("Power.URL") }}</p>
+            <div class="flex flex-col">
+              <Field
+                :id="`url${idx}`"
+                :name="`powers[${idx}].url`"
+                type="url"
+                class="form"
+                v-model="registerParams[idx].url"
+              />
+              <ErrorMessage
+                class="text-rose-600 text-sm"
+                :name="`powers[${idx}].url`"
+              />
+            </div>
           </div>
-          <ErrorMessage class="text-rose-600" :name="`powers[${idx}].productName`" />
-          <div class="flex">
-            <p class="label">{{ $t('Power.maximum') }}</p>
-            <Field
-              :id="`maxPower${idx}`"
-              :name="`powers[${idx}].maxPower`"
-              type="number"
-              class="form"
-              v-model="registerParams[idx].maxPower"
-            />
-            <p>[W]</p>
-          </div>
-          <ErrorMessage class="text-rose-600" :name="`powers[${idx}].maxPower`" />
-          <div class="flex">
-            <p class="label">{{ $t('Power.maker') }}</p>
-            <Field
-              :id="`manufacturer${idx}`"
-              :name="`powers[${idx}].manufacturer`"
-              type="text"
-              class="form"
-              v-model="registerParams[idx].manufacturer"
-            />
-          </div>
-          <ErrorMessage class="text-rose-600" :name="`powers[${idx}].manufacturer`" />
-          <div class="flex">
-            <p class="label">{{ $t('Power.model') }}</p>
-            <Field
-              :id="`model${idx}`"
-              :name="`powers[${idx}].model`"
-              type="text"
-              class="form"
-              v-model="registerParams[idx].model"
-            />
-          </div>
-          <ErrorMessage class="text-rose-600" :name="`powers[${idx}].model`" />
-          <div class="flex">
-            <p class="label">{{ $t('Power.URL') }}</p>
-            <Field
-              :id="`url${idx}`"
-              :name="`powers[${idx}].url`"
-              type="url"
-              class="form"
-              v-model="registerParams[idx].url"
-            />
-          </div>
-          <ErrorMessage class="text-rose-600" :name="`powers[${idx}].url`" />
           <div v-if="idx == 0">
-            <RegistPageButton :text="$t('Button.reset')" @click="reset(idx)"></RegistPageButton>
+            <RegistPageButton
+              :text="$t('Button.reset')"
+              @click="reset(idx)"
+            ></RegistPageButton>
           </div>
           <div v-if="idx != 0" class="flex gap-3">
-            <RegistPageButton :text="$t('Button.reset')" @click="reset(idx)"></RegistPageButton>
-            <RegistPageButton :text="$t('Button.delete')" @click="decrement(idx)"></RegistPageButton>
+            <RegistPageButton
+              :text="$t('Button.reset')"
+              @click="reset(idx)"
+            ></RegistPageButton>
+            <RegistPageButton
+              :text="$t('Button.delete')"
+              @click="decrement(idx)"
+            ></RegistPageButton>
           </div>
         </div>
       </Card>
       <Row>
-        <RegistPageButton :text="$t('Button.back')" @click="back"></RegistPageButton>
-        <RegistPageButton :text="$t('Button.add')" @click="increment"></RegistPageButton>
-        <RegistPageButton :disabled="!meta.valid || isSubmitting" :text="$t('Button.register')" @click="registerPower"></RegistPageButton>
-        <RegistPageButton :text="$t('Button.skip')" @click="skip"></RegistPageButton>
+        <RegistPageButton
+          :text="$t('Button.back')"
+          @click="back"
+        ></RegistPageButton>
+        <RegistPageButton
+          :text="$t('Button.add')"
+          @click="increment"
+        ></RegistPageButton>
+        <RegistPageButton
+          :disabled="!meta.valid || isSubmitting"
+          :text="$t('Button.register')"
+          @click="registerPower"
+        ></RegistPageButton>
+        <RegistPageButton
+          :text="$t('Button.skip')"
+          @click="skip"
+        ></RegistPageButton>
       </Row>
     </Card>
   </div>
@@ -185,19 +239,16 @@ const back = () =>{
 
 <style scoped>
 .error-border {
-  @apply
-    border-rose-600
+  @apply border-rose-600;
 }
 .label {
-  @apply
-    flex-none
+  @apply flex-none
     text-xl
-    pr-5
+    pr-5;
 }
 .form {
-  @apply
-    flex-none
+  @apply flex-none
     border-solid
-    border-2
+    border-2;
 }
 </style>
