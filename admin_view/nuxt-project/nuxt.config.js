098e6fb1fa7190415a523f0e48a46994a3546e67
大浦 翼 <s21957@ca-20016661.tailnet-7e8b.ts.net>
2023-05-25 14:57:29 +0900

    mod api:3000 -> process.env.VUE_APP_URL

--- admin_view/nuxt-project/nuxt.config.js
+++ admin_view/nuxt-project/nuxt.config.js
@@ -58,7 +58,7 @@ export default {
 
   axios: {
     // baseURL: 'http://localhost:3000'
-    baseURL: "http://api:3000",
+    baseURL: process.env.VUE_APP_URL,
     browserBaseURL: process.env.VUE_APP_URL,
   },
 
