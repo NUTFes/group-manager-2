5e258d030e3ed5e27f839c81fc1e6bf23ab497de
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-26 17:04:34 +0900

    [feat] アナウンス文の空文字処理を追加

--- user_front/pages/mypage/edit_announcement/index.vue
+++ user_front/pages/mypage/edit_announcement/index.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
-
 const config = useRuntimeConfig();
-const groupId = ref<number>(0)
-const announcement = ref<string>("")
+const groupId = ref<number>(0);
+const announcement = ref<string>("");
 
 const postAnnouncement = () => {
+  if (announcement.value.length === 0) {
+    alert("会場アナウンス文を入力してください");
+    return;
+  }
+  
   useFetch(config.APIURL + "/announcements", {
     method: "POST",
     params: {
@@ -14,20 +18,27 @@ const postAnnouncement = () => {
     headers: {
       "Content-Type": "application/json",
     },
-  })
-}
+  });
+};
 </script>
 
 <template>
-  <NuxtLink to="/mypage" class="ml-4 text-left text-pink-500 text-2xl hover:font-bold">{{ $t('RegistInfo.return') }}</NuxtLink>
+  <NuxtLink
+    to="/mypage"
+    class="ml-4 text-left text-pink-500 text-2xl hover:font-bold"
+    >{{ $t("RegistInfo.return") }}</NuxtLink
+  >
   <div class="mx-[10%] my-[5%]">
-    <h1 class="text-4xl ">{{ $t('Announcement.editAnnouncement') }}</h1>
+    <h1 class="text-4xl">{{ $t("Announcement.editAnnouncement") }}</h1>
     <Card>
       <div class="text-left">
-        <span class="text-3xl mr-4">{{ $t('Announcement.text') }}</span>
+        <span class="text-3xl mr-4">{{ $t("Announcement.text") }}</span>
       </div>
       <textarea class="border-2 w-[60%]" v-model="announcement"></textarea>
-      <RegistPageButton :text="$t('Announcement.edit')" @click="postAnnouncement"></RegistPageButton>
+      <RegistPageButton
+        :text="$t('Announcement.edit')"
+        @click="postAnnouncement"
+      ></RegistPageButton>
     </Card>
   </div>
 </template>
