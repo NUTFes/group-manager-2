3363c3bba201a2a16d5837cc328d35fba265aca8
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-26 22:15:09 +0900

    [fix] PRの編集ページでPR文だけ登録できるように/画像を表示する/新規時に全て入力するように

--- user_front/pages/mypage/publicRelations/index.vue
+++ user_front/pages/mypage/publicRelations/index.vue
@@ -1,82 +1,152 @@
 <script lang="ts" setup>
-import { getDownloadURL, getStorage, ref as fireRef, uploadBytes } from "firebase/storage";
+import {
+  getDownloadURL,
+  getStorage,
+  ref as fireRef,
+  uploadBytes,
+} from "firebase/storage";
 import { loginCheck } from "~~/utils/methods";
+import axios from "axios";
 
-const router = useRouter()
+interface PublicRelation {
+  id: number;
+  group_id: number;
+  picture_name: string;
+  picture_path: string;
+  blurb: string;
+  created_at: string;
+  updated_at: string;
+}
+
+const router = useRouter();
 const config = useRuntimeConfig();
 const state = reactive({
   groupId: 0,
 });
 
-const selectedFile = ref<File|null>(null)
-const fileName = ref<string>('選択してください')
-const pictureName = ref<string>("")
-const blurb = ref<string>("")
+const currentPublicRelation = ref<PublicRelation | null>(null);
+const selectedFile = ref<File | null>(null);
+const selectedFileUrl = ref<string>("");
+const fileName = ref<string>("選択してください");
+const pictureName = ref<string>("");
+const blurb = ref<string>("");
+const isSubmitting = ref<boolean>(false);
 
 const isBlurbOver = computed(() => {
-  return blurb.value.length > 40
-})
+  return blurb.value.length > 40;
+});
 
 onMounted(async () => {
   // ログインしていない場合は/welcomeに遷移させる
-  loginCheck()
+  loginCheck();
   state.groupId = Number(localStorage.getItem("group_id"));
-})
+
+  const getPublicRelationUrl = "/public_relations";
+  axios
+    .get(config.APIURL + getPublicRelationUrl)
+    .then((response) => {
+      const publicRelations: PublicRelation[] = response.data.data;
+      const publicRelation = publicRelations.find(
+        (pr) => pr.group_id === state.groupId
+      );
+      currentPublicRelation.value = publicRelation || null;
+      blurb.value = publicRelation?.blurb || "";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 const fileUpload = (e: Event) => {
-  const target = e.target as HTMLInputElement
-  const files = target.files
-  const file = files![0]
-  selectedFile.value = file
-  fileName.value = file.name
-}
+  const target = e.target as HTMLInputElement;
+  const files = target.files;
+  const file = files![0];
+  selectedFile.value = file;
+  selectedFileUrl.value = URL.createObjectURL(file);
+  fileName.value = file.name;
+};
 
 const storage = getStorage();
 const storageRef = fireRef(storage, fileName.value);
 
 const editImageURL = () => {
   if (blurb.value.length === 0) {
-    alert('PR文を入力してください')
-    return
+    alert("PR文を入力してください");
+    return;
   }
 
-  selectedFile.value &&
-  uploadBytes(storageRef, selectedFile.value).then((snapshot) => {
-    pictureName.value = snapshot.ref.name
-    getDownloadURL(fireRef(storage, pictureName.value))
-    .then((url) => {
-        const postUrl =
-        "/public_relations?group_id=" +
-        state.groupId;
+  // currentPublicRelationがなくて、画像が未選択の場合はエラー
+  if (!selectedFile.value && !currentPublicRelation.value) {
+    alert("画像を選択してください");
+    return;
+  }
 
-        useFetch(config.APIURL + postUrl,{
-          method: "POST",
-          params: {
-            picture_name: fileName.value,
-            picture_path: url,
-            blurb: blurb.value,
-          },
-          headers: {
-            "Content-Type": "application/json",
-          },
+  isSubmitting.value = true;
+  // currentPublicRelationがない場合は新規登録する
+  selectedFile.value &&
+    uploadBytes(storageRef, selectedFile.value).then((snapshot) => {
+      pictureName.value = snapshot.ref.name;
+      getDownloadURL(fireRef(storage, pictureName.value))
+        .then((url) => {
+          const postUrl = "/public_relations?group_id=" + state.groupId;
+
+          useFetch(config.APIURL + postUrl, {
+            method: "POST",
+            params: {
+              picture_name: fileName.value,
+              picture_path: url,
+              blurb: blurb.value,
+            },
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
         })
-    })
-    .then(
-    (response) =>{
-      alert('登録できました')
-      router.push("/mypage");
-    },
-    (error) => {
-      alert('登録できませんでした')
-    })
-  })
-}
+        .then(
+          (response) => {
+            alert("登録しました（画像の反映には数分かかります）");
+            router.push("/mypage");
+          },
+          (error) => {
+            alert("登録できませんでした");
+          }
+        );
+    });
+
+  // 画像は選択してないが、currentPublicRelationがある場合はPR文のみ更新する
+  if (!selectedFile.value && currentPublicRelation.value) {
+    const putUrl = "/public_relations/" + currentPublicRelation.value.id;
+    useFetch(config.APIURL + putUrl, {
+      method: "PUT",
+      params: {
+        blurb: blurb.value,
+        picture_name: currentPublicRelation.value.picture_name,
+        picture_path: currentPublicRelation.value.picture_path,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(
+      (response) => {
+        alert("PR文を更新しました");
+        router.push("/mypage");
+      },
+      (error) => {
+        alert("PR文を更新きませんでした");
+      }
+    );
+  }
+};
 </script>
 
 <template>
-  <NuxtLink to="/mypage" class="ml-4 text-left text-xl text-pink-500 hover:font-bold">{{ $t("RegistInfo.return") }}</NuxtLink>
+  <NuxtLink
+    to="/mypage"
+    class="ml-4 text-left text-xl text-pink-500 hover:font-bold"
+    >{{ $t("RegistInfo.return") }}</NuxtLink
+  >
   <div class="mx-[10%] my-[5%]">
-    <h1 class="text-4xl ">{{ $t("PR.PR") }}</h1>
+    <h1 class="text-4xl">{{ $t("PR.PR") }}</h1>
     <Card>
       <div class="left text-3xl">
         {{ $t("PR.text") }}
@@ -86,10 +156,27 @@ const editImageURL = () => {
       <div class="my-4 items-center">
         <label>
           <span class="text-3xl mr-4">{{ $t("PR.illustration") }}</span>
-          <input type="file" @change="fileUpload">
+          <input type="file" @change="fileUpload" />
         </label>
       </div>
-      <RegistPageButton :text="$t('Button.register')" @click="editImageURL" :disabled="isBlurbOver"></RegistPageButton>
+      <div class="flex flex-col gap-2 my-4">
+        <div
+          v-if="currentPublicRelation"
+          class="flex flex-col items-center gap-2"
+        >
+          <p>現在登録済みの画像</p>
+          <img :src="currentPublicRelation.picture_path" class="w-1/3" />
+        </div>
+        <div v-if="selectedFile" class="flex flex-col items-center gap-2">
+          <p>選択中の画像</p>
+          <img :src="selectedFileUrl" class="w-1/3" />
+        </div>
+      </div>
+      <RegistPageButton
+        :text="$t('Button.register')"
+        @click="editImageURL"
+        :disabled="isBlurbOver || isSubmitting"
+      ></RegistPageButton>
     </Card>
   </div>
 </template>
