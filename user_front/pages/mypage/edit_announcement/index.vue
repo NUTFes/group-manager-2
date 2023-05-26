36cda509ec513a3d10abb500a7ded45975be617c
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-26 23:31:01 +0900

    [fix] 会場アナウンス文の編集ができるようにする

--- user_front/pages/mypage/edit_announcement/index.vue
+++ user_front/pages/mypage/edit_announcement/index.vue
@@ -1,24 +1,74 @@
 <script lang="ts" setup>
+import axios from "axios";
+
+interface Announcement {
+  id: number;
+  group_id: number;
+  message: string;
+  created_at: string;
+  updated_at: string;
+}
+
+const router = useRouter();
+
+const currentAnnouncement = ref<Announcement>();
 const config = useRuntimeConfig();
 const groupId = ref<number>(0);
-const announcement = ref<string>("");
+const message = ref<string>("");
+
+onMounted(async () => {
+  // ログインしていない場合は/welcomeに遷移させる
+  loginCheck();
+  groupId.value = Number(localStorage.getItem("group_id"));
+
+  const getAnnouncementUrl = "/announcements";
+  axios.get(config.APIURL + getAnnouncementUrl).then((res) => {
+    const announcements = res.data.data;
+    // 同じgroup_idのannouncementを取得
+    currentAnnouncement.value = announcements.find(
+      (announcement: Announcement) => announcement.group_id === groupId.value
+    );
+
+    if (currentAnnouncement.value) {
+      message.value = currentAnnouncement.value.message;
+    }
+  });
+});
 
 const postAnnouncement = () => {
-  if (announcement.value.length === 0) {
+  if (message.value.length === 0) {
     alert("会場アナウンス文を入力してください");
     return;
   }
-  
-  useFetch(config.APIURL + "/announcements", {
-    method: "POST",
-    params: {
-      group_id: groupId.value,
-      message: announcement.value,
-    },
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+
+  if (currentAnnouncement.value) {
+    const putURl = "/announcements/" + currentAnnouncement.value?.id;
+    axios
+      .put(config.APIURL + putURl, {
+        group_id: groupId.value,
+        message: message.value,
+      })
+      .then((res) => {
+        alert("会場アナウンスを更新しました");
+        router.push("/mypage");
+      })
+      .catch((err) => {
+        alert("会場アナウンスの更新に失敗しました");
+      });
+  } else {
+    const postUrl = "/announcements?group_id=" + groupId.value;
+    axios
+      .post(config.APIURL + postUrl, {
+        message: message.value,
+      })
+      .then((res) => {
+        alert("会場アナウンスを登録しました");
+        router.push("/mypage");
+      })
+      .catch((err) => {
+        alert("会場アナウンスの登録に失敗しました");
+      });
+  }
 };
 </script>
 
@@ -34,7 +84,7 @@ const postAnnouncement = () => {
       <div class="text-left">
         <span class="text-3xl mr-4">{{ $t("Announcement.text") }}</span>
       </div>
-      <textarea class="border-2 w-[60%]" v-model="announcement"></textarea>
+      <textarea class="border-2 w-[60%]" v-model="message"></textarea>
       <RegistPageButton
         :text="$t('Announcement.edit')"
         @click="postAnnouncement"
