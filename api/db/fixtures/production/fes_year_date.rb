32d2355c1faefbd21ea1c0241ce5599264013009
ryuseifujisaki <fujiryu1122@gmail.com>
2023-05-28 00:11:18 +0900

    開催日数変更

--- api/db/fixtures/production/fes_year_date.rb
+++ api/db/fixtures/production/fes_year_date.rb
@@ -3,7 +3,7 @@ FesYear.seed( :id,
 )
 
 FesDate.seed( :id,
-    { id: 1, days_num: 1, date: '9月9日', day: '土曜日', fes_year_id: 1 },
-    { id: 2, days_num: 2, date: '9月10日', day: '日曜日', fes_year_id: 1 },
+    { id: 1, days_num: 1, date: '9月16日', day: '土曜日', fes_year_id: 1 },
+    { id: 2, days_num: 2, date: '9月17日', day: '日曜日', fes_year_id: 1 },
 )
 
