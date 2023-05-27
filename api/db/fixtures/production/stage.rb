82ccd876f533bdcf97a5deeadbec813057aa8d9e
ryuseifujisaki <fujiryu1122@gmail.com>
2023-05-27 17:44:31 +0900

    本番環境でのseedsデータ変更

--- api/db/fixtures/production/stage.rb
+++ api/db/fixtures/production/stage.rb
@@ -1,8 +1,5 @@
 Stage.seed( :id,
-    { id: 1, name: '希望なし'               , enable_sunny: true, enable_rainy: true  },
-    { id: 2, name: 'メインステージ'         , enable_sunny: true, enable_rainy: false },
-    { id: 3, name: 'サブステージ'           , enable_sunny: true, enable_rainy: false },
-    { id: 4, name: '体育館'                 , enable_sunny: true, enable_rainy: true  },
-    { id: 5, name: 'マルチメディアセンター' , enable_sunny: true, enable_rainy: true  },
-    { id: 6, name: '武道館'                 , enable_sunny: true, enable_rainy: true  },
+    { id: 1, name: 'メインステージ'         , enable_sunny: true, enable_rainy: false },
+    { id: 2, name: '体育館'                 , enable_sunny: true, enable_rainy: true  },
+    { id: 3, name: '武道館'                 , enable_sunny: true, enable_rainy: true  },
 )
