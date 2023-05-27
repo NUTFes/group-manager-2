b4a37657f944d3d4a1d92055fa8c4f39deddd9fc
ryuseifujisaki <fujiryu1122@gmail.com>
2023-05-27 18:29:59 +0900

    本番環境データ変更

--- api/db/fixtures/production/rental_item.rb
+++ api/db/fixtures/production/rental_item.rb
@@ -1,6 +1,6 @@
 RentalItem.seed( :id,
     { id: 1 ,   name: '机' , is_inside_shop_rentable: true, is_outside_shop_rentable: true, is_stage_rentable: true },
-    { id: 2 ,   name: '長机' , is_inside_shop_rentable: false, is_outside_shop_rentable: true, is_stage_rentable: false},
+    { id: 2 ,   name: '長机' , is_inside_shop_rentable: false, is_outside_shop_rentable: true, is_stage_rentable: true},
     { id: 3 ,   name: '椅子' , is_inside_shop_rentable: true, is_outside_shop_rentable: true, is_stage_rentable: true },
     { id: 4 ,   name: 'パーテーション・パーテション足' , is_inside_shop_rentable: true, is_outside_shop_rentable: true, is_stage_rentable: false },
     { id: 5 ,   name: '掲示板' , is_inside_shop_rentable: true, is_outside_shop_rentable: true, is_stage_rentable: false },
