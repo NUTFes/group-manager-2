b0aa7656270604c62de860ed11519ce75f19cfe6
ryuseifujisaki <fujiryu1122@gmail.com>
2023-05-27 12:16:14 +0900

    全お知らせ表示完了

--- user_front/components/Mypage/News.vue
+++ user_front/components/Mypage/News.vue
@@ -3,15 +3,20 @@ import { News } from "@/types";
 
 const config = useRuntimeConfig();
 const state = reactive({
-  title: "",
-  body: "",
-  updateDate: "",
+  // title: "",
+  // body: "",
+  news : [] as News[],
+  updateDate: [] as string[],
 });
+
 onMounted(async () => {
   const news = await $fetch<News[]>(config.APIURL + "/news");
-  state.title = news[0].title;
-  state.body = news[0].body;
-  state.updateDate = new Date(news[0].updated_at).toLocaleDateString("ja-JP");
+  state.news = news;
+
+  news.forEach((newsItem)=>{
+    const date = new Date(newsItem.updated_at);
+    state.updateDate.push(date.toLocaleDateString("ja-JP"));
+  })
 });
 </script>
 
@@ -20,14 +25,19 @@ onMounted(async () => {
     <div class="text-slate-900 text-2xl font-bold p-2 bg-slate-200">
       <p>{{ $t("Mypage.notice") }}</p>
     </div>
-    <div class="text-slate-800 flex flex-col gap-1 py-2 px-4">
-      <p class="text-xl">{{ state.title }}</p>
-      <div class="bg-slate-200 p-1">
-        <p class="text-lg">{{ state.body }}</p>
+    <div class="text-slate-800 flex flex-col gap-1 py-2 px-4" style="max-height: 200px; overflow-y: auto;">
+      <div v-for="newsItem, i in state.news">
+        <div class="border-b-2">
+          <p class="text-xl p-2">{{ newsItem.title }}</p>
+          <p class="text-xs p-2">created:{{ state.updateDate[i] }}</p>
+            <div class="bg-slate-400 mb-8">
+              <p class="text-lg">{{ newsItem.body }}</p>
+            </div>
+          </div>
+        </div>
       </div>
       <p class="text-md ml-auto w-fit">
-        {{ $t("Mypage.updateDate") }}:{{ state.updateDate }}
+          {{ $t("Mypage.updateDate") }}:{{ state.updateDate[0] }}
       </p>
-    </div>
   </div>
 </template>
