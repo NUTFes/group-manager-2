1f0e46a93ce7a800ab0ba91d6ed46f81a25fa8b3
imaimai17468 <tosiki_801_life@outlook.jp>
2023-05-27 16:07:10 +0900

    [fix] newsのデザインを修正

--- user_front/components/Mypage/News.vue
+++ user_front/components/Mypage/News.vue
@@ -5,7 +5,7 @@ const config = useRuntimeConfig();
 const state = reactive({
   // title: "",
   // body: "",
-  news : [] as News[],
+  news: [] as News[],
   updateDate: [] as string[],
 });
 
@@ -13,10 +13,10 @@ onMounted(async () => {
   const news = await $fetch<News[]>(config.APIURL + "/news");
   state.news = news;
 
-  news.forEach((newsItem)=>{
+  news.forEach((newsItem) => {
     const date = new Date(newsItem.updated_at);
     state.updateDate.push(date.toLocaleDateString("ja-JP"));
-  })
+  });
 });
 </script>
 
@@ -25,19 +25,19 @@ onMounted(async () => {
     <div class="text-slate-900 text-2xl font-bold p-2 bg-slate-200">
       <p>{{ $t("Mypage.notice") }}</p>
     </div>
-    <div class="text-slate-800 flex flex-col gap-1 py-2 px-4" style="max-height: 200px; overflow-y: auto;">
-      <div v-for="newsItem, i in state.news">
-        <div class="border-b-2">
-          <p class="text-xl p-2">{{ newsItem.title }}</p>
-          <p class="text-xs p-2">created:{{ state.updateDate[i] }}</p>
-            <div class="bg-slate-400 mb-8">
-              <p class="text-lg">{{ newsItem.body }}</p>
-            </div>
-          </div>
+    <div
+      class="text-slate-800 flex flex-col gap-4 p-4"
+      style="max-height: 300px; overflow-y: auto"
+    >
+      <div v-for="(newsItem, i) in state.news" class="flex flex-col gap-2">
+        <div class="flex flex-row items-center gap-2 border-b">
+          <p class="text-xl">{{ newsItem.title }}</p>
+          <p class="text-xs">{{ state.updateDate[i] }}</p>
+        </div>
+        <div class="bg-slate-200 p-2 rounded-md">
+          <p class="text-lg">{{ newsItem.body }}</p>
         </div>
       </div>
-      <p class="text-md ml-auto w-fit">
-          {{ $t("Mypage.updateDate") }}:{{ state.updateDate[0] }}
-      </p>
+    </div>
   </div>
 </template>
