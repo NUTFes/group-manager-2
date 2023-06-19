f43f4a36895d5fd0fc80ed17bab2f8b0787d4be8
RyuseiFujisaki <50622120+ryuseifujisaki@users.noreply.github.com>
2023-06-19 14:01:00 +0900

    Revert "group_idがない時にログインしようとしたら，団体登録に飛ばされるようにした"

--- user_front/pages/mypage/index.vue
+++ user_front/pages/mypage/index.vue
@@ -12,6 +12,7 @@ const registGroupCategoryId = ref<number>(0);
 const config = useRuntimeConfig();
 
 onMounted(async () => {
+  loginCheck();
   await $fetch<RegistInfo>(
     config.APIURL + "/api/v1/current_user/current_regist_info",
     {
@@ -23,17 +24,14 @@ onMounted(async () => {
       },
     }
   ).then((response) => {
-      registGroupId.value = response.data[0].group.id;
-      registGroupCategoryId.value = response.data[0].group.group_category_id;
-      localStorage.setItem("group_id", registGroupId.value.toString());
-      localStorage.setItem(
-        "group_category_id",
-        registGroupCategoryId.value.toString()
-      );
-    })
-    .catch(() => {
-      localStorage.setItem("group_id", "");
-    });
+    registGroupId.value = response.data[0].group.id;
+    registGroupCategoryId.value = response.data[0].group.group_category_id;
+    localStorage.setItem("group_id", registGroupId.value.toString());
+    localStorage.setItem(
+      "group_category_id",
+      registGroupCategoryId.value.toString()
+    );
+  });
   await loginCheck();
   const currentUser = await getCurrentUser();
   state.currentUserName = currentUser?.data.user.name || "";
@@ -72,9 +70,7 @@ const links: { to: string; text: string }[] = [
           </div>
           <div class="flex flex-col gap-4 items-center">
             <MypageButton :text="$t('Mypage.check')" link="/regist_info" />
-            <div
-              class="grid md:grid-cols-2 justify-items-center my-2 gap-2 text-pink-400"
-            >
+            <div class="grid md:grid-cols-2 justify-items-center my-2 gap-2 text-pink-400">
               <ui v-for="link in links" :key="link.text">
                 <nuxt-link :to="link.to" class="text-lg">{{
                   $t(link.text)
