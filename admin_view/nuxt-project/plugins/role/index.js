a9fd9f10e3a664765cff9fbdf0b5d383b190af71
Yosuke Iida <stuiida2002@gmail.com>
2023-11-01 02:09:55 +0900

    [feat] add viewer role

--- admin_view/nuxt-project/plugins/role/index.js
+++ admin_view/nuxt-project/plugins/role/index.js
@@ -2,10 +2,11 @@ import Vue from "vue";
 import { userRole } from './user.js'
 import { managerRole } from './manager.js'
 import { developerRole } from './developer.js'
+import { viewerRole } from "./viewer.js";
 
 export default ({ app }, inject) => {
   inject('role', (roleID) => {
-    const roleList = [developerRole, managerRole, userRole]
+    const roleList = [developerRole, managerRole, userRole, viewerRole]
     return roleList[roleID - 1]
   })
 }
