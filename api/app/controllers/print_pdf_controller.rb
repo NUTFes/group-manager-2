b29220786ebec5cfad27252d279b4b7e4df83a4c
sota1024 <21.s.kotake.nutfes@gmail.com>
2024-01-28 19:59:00 +0900

    [fix]output_all_groups_rental_items_pdf

--- api/app/controllers/print_pdf_controller.rb
+++ api/app/controllers/print_pdf_controller.rb
@@ -8,7 +8,7 @@ class PrintPdfController < ApplicationController
   
   # 物品貸し出し書類をまとめて出力
   def output_all_groups_rental_items_pdf
-    @groups = Group.where(fes_year_id: params[:fes_year_id])
+    @groups = Group.where(fes_year_id: params[:fes_year_id]).order(:group_category_id)
     print_pdf("output_all_groups_rental_items", "output_rental_items_pdf", "物品貸し出し書類_物品持ち出し表(各団体向け)", "Not Landscape")
   end
 
