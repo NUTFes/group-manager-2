20b0be7f35fd9a12ad5201aa5bea16427dfc0d80
harata-t <22.t.harata.nutfes@gmail.com>
2024-03-08 14:56:42 +0900

    [feat] 物品申請数調整ページの表示 (#1496)

--- admin_view/nuxt-project/components/Menu.vue
+++ admin_view/nuxt-project/components/Menu.vue
@@ -168,6 +168,7 @@ export default {
           icon: "assignment_return",
           click: "/assign_items",
         },
+        { title: "物品申請数調整", icon: "stadium", click: "/adjustment_order_items" },
         { title: "識別番号", icon: "format_list_numbered", click: "/group_identify" },
         { title: "会場割り当て", icon: "event_seat", click: "/assign_places" },
         { title: "ステージ割り当て", icon: "stadium", click: "/assign_stages" },
---
+++ admin_view/nuxt-project/pages/adjustment_order_items/index.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="main-content">
+    <SubHeader pageTitle="物品申請数調整">
+      <CommonButton v-if="this.$role(roleID).stocker_places.create" iconName="add_circle" :on_click="openAddModal">
+        追加
+      </CommonButton>
+    </SubHeader>
+
+    <SubSubHeader>
+      <template v-slot:refinement>
+        <SearchDropDown
+          :nameList="yearList"
+          :on_click="refinementGroups"
+          value="year_num"
+        >
+          {{ refYears }}
+        </SearchDropDown>
+        <SearchDropDown
+          :nameList="groupCategories"
+          :on_click="refinementGroups"
+          value="name"
+        >
+          {{ refGroupCategories }}
+        </SearchDropDown>
+      </template>
+    </SubSubHeader>
+
+    <div style="display: flex; justify-content: space-between;">
+      <Card width="45%">
+        <Table>
+          <template v-slot:table-header>
+            <th v-for="(header, index) in itemHeaders" v-bind:key="index">
+              {{ header }}
+            </th>
+          </template>
+          <template v-slot:table-body>
+            <tr v-for="(stocker_place, index) in stockerPlaces">
+              <td>{{ stocker_place.id }}</td>
+              <td>{{  }}</td>
+              <td>{{  }}</td>
+              <td>{{  }}</td>
+              <td>{{  }}</td>
+            </tr>
+          </template>
+        </Table>
+      </Card>
+      <Card width="45%">
+        <Table>
+          <template v-slot:table-header>
+            <th v-for="(header, index) in groupHeaders" v-bind:key="index">
+              {{ header }}
+            </th>
+          </template>
+          <template v-slot:table-body>
+            <tr v-for="(stocker_place, index) in stockerPlaces">
+              <td>{{ stocker_place.id }}</td>
+              <td>{{  }}</td>
+              <td>{{  }}</td>
+              <td>{{  }}</td>
+              <td>{{  }}</td>
+            </tr>
+          </template>
+        </Table>
+      </Card>
+    </div>
+
+    <AddModal
+      @close="closeAddModal"
+      v-if="isOpenAddModal"
+      title="在庫場所の追加"
+    >
+      <template v-slot:form>
+        <div>
+          <h3>部屋名</h3>
+          <input v-model="roomName" placeholder="入力してください" />
+        </div>
+      </template>
+      <template v-slot:method>
+        <CommonButton iconName="add_circle" :on_click="submit"
+          >登録</CommonButton
+        >
+      </template>
+    </AddModal>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  watchQuery: ["page"],
+  data() {
+      return {
+          itemHeaders: [
+              "ID",
+              "物品",
+              "申請数",
+              "在庫数",
+              "差分",
+          ],
+          groupHeaders: [
+              "ID",
+              "団体名",
+              "物品",
+              "個数",
+              "",
+          ],
+          isOpenAddModal: false,
+          stocker_place: [],
+          roomName: [],
+          roomNameList: [],
+          stockItemStatus: [],
+          assignItemStatus: [],
+      };
+  },
+
+	async asyncData({ $axios }) {
+		const stockerPlacesUrl = "/stocker_places";
+		const stockerPlacesRes = await $axios.$get(stockerPlacesUrl);
+		return {
+			stockerPlaces: stockerPlacesRes.data
+		}
+	},
+  computed: {
+    ...mapState({
+      roleID: (state) => state.users.role,
+    }),
+  },
+  methods: {
+    openAddModal() {
+      this.isOpenAddModal = false;
+      this.isOpenAddModal = true;
+    },
+    closeAddModal() {
+      this.isOpenAddModal = false;
+    },
+    reload(id) {
+      const url = "/stocker_places/" + id;
+      this.$axios.$get(url).then((response) => {
+        this.stocker_place.data.push(response.data);
+        console.log(response)
+      })
+      .catch(error =>
+      {
+        console.log(error)
+      })
+      ;
+      console.log(this.stocker_place)
+    },
+    async submit() {
+      const url =
+        "/stocker_places/" +
+        "?name=" +
+        this.roomName +
+        "&stock_item_status=1&assign_item_status=1";
+
+      this.$axios.$post(url).then((response) => {
+        this.roomName = "";
+        this.stockItemStatus = "";
+        this.assignItemStatus = "";
+        this.reload(response.data.id);
+        this.closeAddModal();
+      });
+    },
+  },
+};
+</script>
