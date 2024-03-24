8f221f7db8b0e1006803b0ee8f8c23b61cf24373
takasuka_takumi <s233319@stn.nagaokaut.ac.jp>
2024-03-24 20:29:07 +0900

    [feat]Create-page-for-adjusting-the-number-of-goods(#1503)

--- admin_view/nuxt-project/pages/adjustment_order_items/index.vue
+++ admin_view/nuxt-project/pages/adjustment_order_items/index.vue
@@ -1,33 +1,10 @@
 <template>
   <div class="main-content">
-    <SubHeader pageTitle="物品申請数調整">
-      <CommonButton v-if="this.$role(roleID).stocker_places.create" iconName="add_circle" :on_click="openAddModal">
-        追加
-      </CommonButton>
-    </SubHeader>
-
-    <SubSubHeader>
-      <template v-slot:refinement>
-        <SearchDropDown
-          :nameList="yearList"
-          :on_click="refinementGroups"
-          value="year_num"
-        >
-          {{ refYears }}
-        </SearchDropDown>
-        <SearchDropDown
-          :nameList="groupCategories"
-          :on_click="refinementGroups"
-          value="name"
-        >
-          {{ refGroupCategories }}
-        </SearchDropDown>
-      </template>
-    </SubSubHeader>
-
+    <SubHeader pageTitle="物品申請数調整" />
     <Row wrap="nowrap" align="start">
-      <Column width="50%" height="800px">
+      <Column width="40%" height="800px">
         <Card width="100%" style="overflow: scroll">
+          <SubHeader pageTitle="物品総数" />
           <Table>
             <template v-slot:table-header>
               <th v-for="(header, index) in itemHeaders" v-bind:key="index">
@@ -35,18 +12,41 @@
               </th>
             </template>
             <template v-slot:table-body>
-              <tr v-for="(stocker_place, index) in stockerPlaces">
-                <td>{{ stocker_place.id }}</td>
-                <td>{{  }}</td>
-                <td>{{  }}</td>
-                <td>{{  }}</td>
+              <tr
+                v-for="(stocker_place, index) in stockerPlaces"
+                :key="index"
+              >
+                <td>{{ stocker_place.itemName }}</td>
+                <td>{{ stocker_place.requestedAmount }}</td>
+                <td>{{ stocker_place.stockAmount }}</td>
+                <td :class="{ 'warning': calculateDifference(stocker_place) < 0 }">
+                  {{ calculateDifference(stocker_place) }}
+                </td>
               </tr>
             </template>
           </Table>
         </Card>
       </Column>
-      <Column width="50%" height="800px">
+      <Column width="60%" height="800px">
         <Card width="100%" style="overflow: scroll">
+          <SubHeader pageTitle="物品申請一覧" />
+          <div class="search-dropdowns">
+            <SearchDropDown
+              :nameList="yearList"
+              :on_click="refinementGroups"
+              value="year_num"
+            >
+              団体名
+            </SearchDropDown>
+            <SearchDropDown
+              :nameList="groupCategories"
+              :on_click="refinementGroups"
+              value="name"
+            >
+              物品名
+            </SearchDropDown>
+          </div>
+
           <Table>
             <template v-slot:table-header>
               <th v-for="(header, index) in groupHeaders" v-bind:key="index">
@@ -54,12 +54,22 @@
               </th>
             </template>
             <template v-slot:table-body>
-              <tr v-for="(stocker_place, index) in stockerPlaces">
-                <td>{{ stocker_place.id }}</td>
-                <td>{{  }}</td>
-                <td>{{  }}</td>
-                <td>{{  }}</td>
-                <td>{{  }}</td>
+              <tr
+                v-for="(stocker_place, index) in stockerPlaces"
+                :key="index"
+                @click="openModal(stocker_place)"
+              >
+                <td>{{ stocker_place.groupName }}</td>
+                <td>{{ stocker_place.itemName }}</td>
+                <td>{{ stocker_place.requestedAmount }}</td>
+                <td>
+                  <Button
+                    iconName="delete"
+                    @click.stop="deleteItem(stocker_place.id)"
+                  >
+                    削除
+                  </Button>
+                </td>
               </tr>
             </template>
           </Table>
@@ -67,19 +77,27 @@
       </Column>
     </Row>
 
-    <AddModal
-      @close="closeAddModal"
-      v-if="isOpenAddModal"
-      title="在庫場所の追加"
-    >
+    <AddModal @close="closeModal" v-if="isModalOpen" title="物品申請数調整">
       <template v-slot:form>
         <div>
-          <h3>部屋名</h3>
-          <input v-model="roomName" placeholder="入力してください" />
+          <h3>団体名</h3>
+          <input v-model="groupName" placeholder="入力してください" />
+        </div>
+        <div>
+          <h3>物品名</h3>
+          <input v-model="itemName" placeholder="入力してください" />
+        </div>
+        <div>
+          <h3>申請数</h3>
+          <input
+            v-model="requestedAmount"
+            type="number"
+            placeholder="入力してください"
+          />
         </div>
       </template>
       <template v-slot:method>
-        <CommonButton iconName="add_circle" :on_click="submit"
+        <CommonButton iconName="add_circle" :on_click="submitItem"
           >登録</CommonButton
         >
       </template>
@@ -92,78 +110,119 @@ import { mapState } from "vuex";
 export default {
   watchQuery: ["page"],
   data() {
-      return {
-          itemHeaders: [
-              "ID",
-              "物品",
-              "申請数",
-              "在庫数",
-              "差分",
-          ],
-          groupHeaders: [
-              "ID",
-              "団体名",
-              "物品",
-              "個数",
-              "",
-          ],
-          isOpenAddModal: false,
-          stocker_place: [],
-          roomName: [],
-          roomNameList: [],
-          stockItemStatus: [],
-          assignItemStatus: [],
-      };
+    return {
+      stockerPlaces: [
+        {
+          id: 1,
+          groupName: "団体A",
+          itemName: "テント",
+          requestedAmount: 10,
+          stockAmount: 5,
+        },
+        {
+          id: 2,
+          groupName: "団体B",
+          itemName: "テーブル",
+          requestedAmount: 5,
+          stockAmount: 10,
+        },
+      ],
+      itemHeaders: ["物品名", "申請数", "在庫総数", "差分"],
+      groupHeaders: ["団体名", "物品名", "申請数", ""],
+      stocker_place: [],
+      roomName: [],
+      roomNameList: [],
+      stockItemStatus: [],
+      assignItemStatus: [],
+      isModalOpen: false,
+      groupName: "",
+      itemName: "",
+      requestedAmount: null,
+      warningStatus: {},
+    };
   },
 
-	async asyncData({ $axios }) {
-		const stockerPlacesUrl = "/stocker_places";
-		const stockerPlacesRes = await $axios.$get(stockerPlacesUrl);
-		return {
-			stockerPlaces: stockerPlacesRes.data
-		}
-	},
-  computed: {
-    ...mapState({
-      roleID: (state) => state.users.role,
-    }),
-  },
+  // async asyncData({ $axios }) {
+  //   const stockerPlacesUrl = "/stocker_places";
+  //   const stockerPlacesRes = await $axios.$get(stockerPlacesUrl);
+  //   return {
+  //     stockerPlaces: stockerPlacesRes.data,
+  //   };
+  // },
+  // computed: {
+  //   ...mapState({
+  //     roleID: (state) => state.users.role,
+  //   }),
+  // },
   methods: {
-    openAddModal() {
-      this.isOpenAddModal = false;
-      this.isOpenAddModal = true;
+    calculateDifference(stocker_place) {
+      return stocker_place.requestedAmount - stocker_place.stockAmount;
     },
-    closeAddModal() {
-      this.isOpenAddModal = false;
+    setWarningStatus(index) {
+      const difference = this.calculateDifference(this.stockerPlaces[index]);
+      this.$set(this.warningStatus, index, difference < 0);
+    },
+    openModal(stocker_place) {
+      this.groupName = stocker_place.groupName || "";
+      this.itemName = stocker_place.itemName || "";
+      this.requestedAmount = stocker_place.requestedAmount || null;
+      this.isModalOpen = true;
+    },
+    closeModal() {
+      this.isModalOpen = false;
+    },
+    submitItem() {
+      const url = "/path/to/your/api";
+      const payload = {
+        groupName: this.groupName,
+        itemName: this.itemName,
+        requestedAmount: this.requestedAmount,
+      };
+      this.$axios
+        .$post(url, payload)
+        .then(() => {
+          this.closeModal();
+        })
+        .catch((error) => {
+          console.error("エラー発生:", error);
+        });
     },
     reload(id) {
       const url = "/stocker_places/" + id;
-      this.$axios.$get(url).then((response) => {
-        this.stocker_place.data.push(response.data);
-        console.log(response)
-      })
-      .catch(error =>
-      {
-        console.log(error)
-      })
-      ;
-      console.log(this.stocker_place)
-    },
-    async submit() {
-      const url =
-        "/stocker_places/" +
-        "?name=" +
-        this.roomName +
-        "&stock_item_status=1&assign_item_status=1";
-
-      this.$axios.$post(url).then((response) => {
-        this.roomName = "";
-        this.stockItemStatus = "";
-        this.assignItemStatus = "";
-        this.reload(response.data.id);
-        this.closeAddModal();
-      });
+      this.$axios
+        .$get(url)
+        .then((response) => {
+          this.stocker_place.data.push(response.data);
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      console.log(this.stocker_place);
     },
   },
 };
 </script>
+
+<style>
+.search-dropdowns {
+  display: flex;
+  justify-content: flex-start;
+  gap: 20px;
+  width: 100%;
+  align-items: start;
+  padding-bottom: 20px;
+}
+.warning {
+  text-align: center;
+  vertical-align: middle;
+  padding: 25px;
+  background-color: #ffac5d; 
+}
+.normal-table td.warning:hover {
+  background-color: #ffac5d !important;
+  background: none;
+  -webkit-background-clip: initial !important;
+  -webkit-text-fill-color: black !important;
+}
+</style>
