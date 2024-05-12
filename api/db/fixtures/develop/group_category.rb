74147e08484fa6c7f9b0ecfc43bdc6a77092da3a
nose221834 <iatyy01151821@gmail.com>
2024-05-12 17:38:56 +0900

    group_categoryのseedsを修正

--- api/db/fixtures/develop/group_category.rb
+++ api/db/fixtures/develop/group_category.rb
@@ -2,4 +2,8 @@ GroupCategory.seed( :id,
   { id: 1, name: '食品販売' },
   { id: 2, name: '物品販売' },
   { id: 3, name: 'ステージ' },
+  { id: 4, name: '展示・体験' },
+  { id: 5, name: '研究室' },
+  { id: 6, name: '実行委員' },
+  { id: 7, name: 'その他' }
 )
