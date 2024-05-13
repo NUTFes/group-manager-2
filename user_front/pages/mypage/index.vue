3828b9f35eaa934f33639336c05cdea33f9a6737
nose221834 <iatyy01151821@gmail.com>
2024-05-13 11:50:10 +0900

    食品販売の場合のみ調理工程へのリンクを表示する

--- user_front/pages/mypage/index.vue
+++ user_front/pages/mypage/index.vue
@@ -11,8 +11,11 @@ const registGroupId = ref<number>(0);
 const registGroupCategoryId = ref<number>(0);
 const config = useRuntimeConfig();
 const is_external = ref<boolean>(false);
+const isLoading = ref(true); // ローディング状態を管理するフラグ
+
 
 onMounted(async () => {
+  isLoading.value = true; // データ取得開始時にローディングを true に設定
   loginCheck();
   await $fetch<RegistInfo>(
     config.APIURL + "/api/v1/current_user/current_regist_info",
@@ -34,11 +37,13 @@ onMounted(async () => {
       registGroupCategoryId.value.toString()
     );
   });
+  isLoading.value = false; // データ取得終了時にローディングを false に設定
   await loginCheck();
   const currentUser = await getCurrentUser();
   state.currentUserName = currentUser?.data.user.name || "";
 });
 
+// 食品販売を行う団体のみが、mypage/edit_cooking_processにいけるようにする
 const links: { to: string; text: string }[] = [
   { to: "/mypage/edit_group", text: "Mypage.editGroup" },
   { to: "/mypage/publicRelations", text: "Mypage.editPR" },
@@ -47,12 +52,20 @@ const links: { to: string; text: string }[] = [
   { to: "/mypage/password_reset", text: "Mypage.editPassword" },
   { to: "/mypage/edit_announcement", text: "Mypage.editAnnouncemant" },
   { to: "/mypage/edit_contact_person", text: "Mypage.editContactPerson" },
-  { to: "/mypage/edit_cooking_process", text: "Mypage.editCookingProcess"},
 ];
+
+// 食品販売用のリストを作る
+const linksGroupCategoryId_1 = [...links,  { to: "/mypage/edit_cooking_process", text: "Mypage.editCookingProcess"}]
+
+
+console.log("registGroupCategoryId",registGroupCategoryId)
 
 const shouldDisplayLink = (link: { to: string; text: string }) => {
   return link.text !== "Mypage.editContactPerson" || is_external.value;
 }
+
+
+
 </script>
 
 <template>
@@ -76,9 +89,19 @@ const shouldDisplayLink = (link: { to: string; text: string }) => {
               {{ $t("Mypage.operations") }}
             </p>
           </div>
-          <div class="flex flex-col gap-4 items-center">
+          <!-- データを取り出してからリンクを表示する -->
+          <div v-if="isLoading===false" class="flex flex-col gap-4 items-center">
             <MypageButton :text="$t('Mypage.check')" link="/regist_info" />
-            <div class="grid md:grid-cols-2 justify-items-center my-2 gap-2 text-pink-400">
+              <!-- 食品販売団体の場合 -->
+            <div v-if="registGroupCategoryId === 1" class="grid md:grid-cols-2 justify-items-center my-2 gap-2 text-pink-400">
+              <ui v-for="linkCook in linksGroupCategoryId_1" :key="linkCook.text">
+                <nuxt-link v-if="shouldDisplayLink(linkCook)" :to="linkCook.to" class="text-lg">{{
+                  $t(linkCook.text)
+                }}</nuxt-link>
+              </ui>
+            </div>
+            <!-- それ以外の場合 -->
+            <div v-else class="grid md:grid-cols-2 justify-items-center my-2 gap-2 text-pink-400">
               <ui v-for="link in links" :key="link.text">
                 <nuxt-link v-if="shouldDisplayLink(link)" :to="link.to" class="text-lg">{{
                   $t(link.text)
