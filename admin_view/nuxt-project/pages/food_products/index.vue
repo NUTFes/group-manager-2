c87f7e3e96589687d0c610a5164373e33ada8351
kageya34 <24.t.kageyama.nutfes@gmail.com>
2024-11-22 00:24:30 +0900

    [fix]管理者画面で絞り込みをAllから個別名に変える(#1708)

--- admin_view/nuxt-project/pages/food_products/index.vue
+++ admin_view/nuxt-project/pages/food_products/index.vue
@@ -279,7 +279,7 @@ export default {
         this.refYearID = item_id;
         // ALLの時
         if (item_id == 0) {
-          this.refYears = "ALL";
+          this.refYears = "ALL Yeras";
         } else {
           this.refYears = name_list[item_id - 1].year_num;
         }
@@ -290,7 +290,7 @@ export default {
         this.refIsCookingID = item_id;
         // ALLの時
         if (item_id === 0) {
-          this.refIsCooking = "ALL";
+          this.refIsCooking = "ALL Cooking states";
         } else {
           this.refIsCooking = name_list[item_id - 1].text;
         }
@@ -300,7 +300,7 @@ export default {
         this.refCategoryID = item_id;
         // ALLの時
         if (item_id === 0) {
-          this.refCategory = "ALL";
+          this.refCategory = "ALL Categorys";
         } else {
           this.refCategory = name_list[item_id - 1].text;
         }
