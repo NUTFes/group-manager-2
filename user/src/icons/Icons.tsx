076be0d8f2c21600c0cd7abe8ace38a465a6f4e5
hikahana <22.h.hanada.nutfes@gmail.com>
2025-01-20 09:11:59 +0900

    [feat] icon格納場所を追加

---
+++ user/src/icons/Icons.tsx
@@ -0,0 +1,13 @@
+import { SlPencil } from "react-icons/sl";
+import { RxCross2 } from "react-icons/rx";
+import { FiPlus } from "react-icons/fi";
+import { LiaLessThanSolid } from "react-icons/lia";
+
+const Icons: { [key: string]: JSX.Element } = {
+  pencil: <SlPencil />,
+  cross: <RxCross2 />,
+  plus: <FiPlus />,
+  lessThan: <LiaLessThanSolid />,
+};
+
+export default Icons;
