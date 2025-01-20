a81c1fba850004022d07ed920c3b166342f3176f
hikahana <22.h.hanada.nutfes@gmail.com>
2025-01-20 09:11:42 +0900

    [fix] docs導入

--- user/.storybook/preview.ts
+++ user/.storybook/preview.ts
@@ -1,12 +1,17 @@
 import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
 
 const preview: Preview = {
+  tags: ["autodocs"],
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    docs: {
+      theme: themes.dark,
     },
   },
 };
