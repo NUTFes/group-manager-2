2d6b1158490a0d4da3ec2b23782933b6a35424a2
K-wakaT <hasukiiq@gmail.com>
2025-02-02 01:20:44 +0900

    TextBoxコンポーンネントの作成

---
+++ user/src/components/Form/TextBox/TextBox.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from '@storybook/react';
+import TextBox from './TextBox';
+import "../../styles/globals.css";
+
+export default {
+title: 'Components/TextBox',
+tags: ["autodocs"],
+component: TextBox,
+parameters: {
+    docs: {
+      source: {
+        type: "auto",
+      },
+    },
+  },
+} as Meta<typeof TextBox>;
+
+type Story = StoryObj<typeof TextBox>;
+
+export const Default: Story = {
+  args: {
+
+  },
+};
---
+++ user/src/components/Form/TextBox/TextBox.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+
+type TextBoxProps = {
+    label: string;
+    value: string;
+    onChange: (value: string) => void;
+    required?: boolean;
+    note?: string;
+    error?: string;
+};
+
+const TextBox: FC<TextBoxProps> = ({label, value, onChange, required, note, error}) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        onChange(event.target.value);
+    };
+
+    return (
+        <>
+            <label>
+                <div className='flex gap-6 items-center mb-[5px]'>
+                    <p className='text-base'>{label}</p>
+                    {required && <p className='text-xs text-alert'>※必須</p>}
+                </div>
+                <input 
+                    type="text" 
+                    value={value} 
+                    onChange={handleChange} 
+                    className={`w-96 h-12 border-2 rounded-[10px] ${error ? 'border-alert' : 'border-main'}`}
+                />
+                <p className='text-xs text-sub'>{note}</p>
+                <p className='text-xs text-alert'>{error}</p>
+            </label>
+        </>
+    );
+};
+
+export default TextBox;
