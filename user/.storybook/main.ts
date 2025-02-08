b9103323404df85ca3932defb0e1b1eb60af309b
hikahana <22.h.hanada.nutfes@gmail.com>
2025-02-09 01:02:11 +0900

    [feat] svgr設定追記

--- user/.storybook/main.ts
+++ user/.storybook/main.ts
@@ -14,5 +14,26 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+
+    // This modifies the existing image rule to exclude .svg files
+    // since you want to handle those files with @svgr/webpack
+    const imageRule = config.module.rules.find((rule) =>
+      rule?.["test"]?.test(".svg")
+    );
+    if (imageRule) {
+      imageRule["exclude"] = /\.svg$/;
+    }
+
+    // Configure .svg files to be loaded with @svgr/webpack
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 export default config;
