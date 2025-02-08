c8cfe3126c596397e1ae1a9735ef258136082dfd
hikahana <22.h.hanada.nutfes@gmail.com>
2025-02-08 19:11:38 +0900

    [fix] loading追加

--- user/src/icons/Icons.tsx
+++ user/src/icons/Icons.tsx
@@ -2,6 +2,17 @@ import { SlPencil } from "react-icons/sl";
 import { RxCross2 } from "react-icons/rx";
 import { FiPlus } from "react-icons/fi";
 import { LiaLessThanSolid } from "react-icons/lia";
+import { FC } from "react";
+
+export const Loading: FC<{ colorClass: string }> = ({ colorClass }) => (
+  <div className="flex justify-center" aria-label="読み込み中">
+    <div className={`animate-ping h-2 w-2 ${colorClass} rounded-full`}></div>
+    <div
+      className={`animate-ping h-2 w-2 ${colorClass} rounded-full mx-4`}
+    ></div>
+    <div className={`animate-ping h-2 w-2 ${colorClass} rounded-full`}></div>
+  </div>
+);
 
 const Icons: { [key: string]: JSX.Element } = {
   pencil: <SlPencil />,
