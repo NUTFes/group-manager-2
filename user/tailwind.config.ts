0f0d8e4469d89418988e82015c8388b4d5ce3205
hikahana <22.h.hanada.nutfes@gmail.com>
2025-02-08 19:11:15 +0900

    [fix] tailwind適応パスを追加

--- user/tailwind.config.ts
+++ user/tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/icons/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
