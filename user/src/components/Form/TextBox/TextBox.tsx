594f062d8d66723e0595a44ffc41ad2d4f5fe274
izuizu0424 <24.i.yasui.nutfes@gmail.com>
2025-02-09 12:13:12 +0900

    [fix]ラベルと補足メッセージの表示位置の調整

--- user/src/components/Form/TextBox/TextBox.tsx
+++ user/src/components/Form/TextBox/TextBox.tsx
@@ -17,7 +17,7 @@ const TextBox: FC<TextBoxProps> = ({label, value, onChange, required, note, erro
     return (
         <>
             <label>
-                <div className='flex gap-6 items-center mb-[5px]'>
+                <div className='flex gap-6 items-center mb-[4px]'>
                     <p className='text-base'>{label}</p>
                     {required && <p className='text-xs text-alert'>※必須</p>}
                 </div>
@@ -27,7 +27,7 @@ const TextBox: FC<TextBoxProps> = ({label, value, onChange, required, note, erro
                     onChange={handleChange} 
                     className={`w-[400px] text-font h-12 border-2 rounded-[10px] ${error ? 'border-alert' : 'border-main'}`}
                 />
-                <p className='text-xs text-sub'>{note}</p>
+                <p className='text-xs text-sub max-w-[400px] break-words mt-[4px]'>{note}</p>
                 <p className='text-xs text-alert'>{error}</p>
             </label>
         </>
