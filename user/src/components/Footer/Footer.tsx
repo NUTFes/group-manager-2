e76cebc3ea6021c3660f650c8a3dc472550225ba
i2_yosuke <95607264+YosukeIida@users.noreply.github.com.>
2025-02-28 20:02:20 +0900

    [wip] pages/index.tsxでLayoutを表示した時に，headerが表示できない

--- user/src/components/Footer/Footer.tsx
+++ user/src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="fixed bottom-0 left-0 w-full h-20 bg-green-600 flex justify-center items-center px-8 text-xs font-light text-white">
+    <footer className="w-full h-20 bg-green-600 flex justify-center items-center px-8 text-xs font-light text-white">
       Copyright © {currentYear} NUTMEG. All Rights Reserved.
     </footer>
   );
