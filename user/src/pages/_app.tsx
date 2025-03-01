c699b8e56a584591efe5d55e77a036d5f59cad2d
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2025-03-01 14:08:00 +0900

    layout表示

--- user/src/pages/_app.tsx
+++ user/src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="max-h-svh">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
 }
