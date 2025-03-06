56c59c2b8991e2c07f21f5e0673e530b624b1f53
izuizu0424 <24.i.yasui.nutfes@gmail.com>
2025-03-06 13:41:55 +0900

    [fix]モバイル版のパディング調整

--- user/src/components/FormContainer/FormContainer.tsx
+++ user/src/components/FormContainer/FormContainer.tsx
@@ -6,7 +6,7 @@ type FormContainerProps = {
 
 const FormContainer: FC<FormContainerProps> = ({ children }) => {
   return (
-    <div className="p-5 md:p-20 bg-baseColor rounded-[20px] shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)] border border-[#b2b2b2] flex-col justify-start items-start gap-10 inline-flex overflow-hidden">
+    <div className="p-6 md:p-20 bg-baseColor rounded-[20px] shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)] border border-[#b2b2b2] flex-col justify-start items-start gap-10 inline-flex overflow-hidden">
       {children}
     </div>
   );
