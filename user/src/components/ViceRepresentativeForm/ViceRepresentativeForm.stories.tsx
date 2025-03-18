466ef284ffc86770e3fc6c5c1a4182ecbffa5c09
kureshou <kure92350@gmail.com>
2025-03-19 05:57:53 +0900

    フォルダの作成

---
+++ user/src/components/ViceRepresentativeForm/ViceRepresentativeForm.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ViceRepresentativeForm from './ViceRepresentativeForm';
+import "@globals";
+
+export default {
+title: 'Components/ViceRepresentativeForm',
+tags: ["autodocs"],
+component: ViceRepresentativeForm,
+parameters: {
+    docs: {
+      source: {
+        type: "auto",
+      },
+    },
+  },
+} as Meta<typeof ViceRepresentativeForm>;
+
+type Story = StoryObj<typeof ViceRepresentativeForm>;
+
+export const Default: Story = {
+  args: {
+
+  },
+};
