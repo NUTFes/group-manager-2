a2327accb4d01f5f1a22c0450c91b4db1b90126e
kureshou <kure92350@gmail.com>
2025-03-23 14:38:54 +0900

    フォルダ構造をStageOptionに合わせた

---
+++ user/src/components/ViceRepresentative/ViceRepresentative.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+type ViceRepresentativeProps = {};
+
+const ViceRepresentative: FC<ViceRepresentativeProps> = () => {
+return <div>ViceRepresentative Component</div>;
+};
+
+export default ViceRepresentative;
---
+++ user/src/components/ViceRepresentative/ViceRepresentativeForm/ViceRepresentativeForm.tsx
@@ -0,0 +1,85 @@
+import { FC } from 'react';
+import FormContainer from '../../FormContainer';
+import Radio from '../../Form/Radio';
+import TextBox from '../../Form/TextBox';
+import { PiSquareSplitVerticalDuotone } from 'react-icons/pi';
+import { getRandomValues } from 'crypto';
+import {useForm} from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import snakecaseKeys from 'snakecase-keys';
+import { mutate } from 'swr';
+import useSWR from 'swr';
+import api from '@/lib/api';
+import { stageOptionSchema } from './schema';
+
+type ViceRepresentativeFormProps = {};
+
+const option2 = [
+    {id: 1, name:'はい'},
+    {id: 0, name:'いいえ'},
+];
+
+type FormData ={
+    isGroup: number;
+};
+
+const ViceRepresentativeForm: FC<ViceRepresentativeFormProps> = () => {
+
+    const {
+        handleSubmit,
+        setValue,
+        getValues,
+        formState: { errors },
+        reset,
+        watch,
+      } = useForm<FormData>({
+        resolver: zodResolver(stageOptionSchema),
+        mode: 'onChange',
+        defaultValues: {
+          groupId: 1,
+        },
+      });
+    
+    const values = watch();
+
+    const onSubmit = async (data: FormData) => {
+        try {
+          await api.post('/stage_common_options/', data);
+          mutate('/stage_common_options/');
+          alert('送信しました');
+          reset();
+        } catch {
+          alert('送信に失敗しました。');
+        }
+    };
+
+    const radioValue1 = values.isGroup?.toString() || '';
+
+
+return (
+    <FormContainer>
+        <form>
+            <div>
+                <Radio
+                    label="一人での参加ですか？"
+                    note='選んでください'
+                    onChange={(value) => setValue('isGroup',Number(value))}
+                    options={option2}
+                    required
+                    value={radioValue1}
+                    error=''
+                />
+                {radioValue1 === "1" &&(
+                    <div>
+                        <TextBox
+                        
+                        />
+                    </div>
+                )}
+            </div>
+        </form>
+    </FormContainer>
+)
+};
+
+export default ViceRepresentativeForm;
