11878badc29013c4c3139dc0226aa914e2a223ac
kureshou <kure92350@gmail.com>
2025-03-23 13:38:01 +0900

    前回のブランチから引継ぎ

---
+++ user/src/components/ViceRepresentativeForm/ViceRepresentativeForm.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+type ViceRepresentativeFormProps = {};
+
+const ViceRepresentativeForm: FC<ViceRepresentativeFormProps> = () => {
+return <div>ViceRepresentativeForm Component</div>;
+};
+
+export default ViceRepresentativeForm;
