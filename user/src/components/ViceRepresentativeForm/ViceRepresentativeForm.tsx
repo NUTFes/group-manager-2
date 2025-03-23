75517d43cf563ffc2c0bdc6880abbcb1b20584f9
kureshou <kure92350@gmail.com>
2025-03-23 14:38:54 +0900

    フォルダ構造をStageOptionに合わせた

--- user/src/components/ViceRepresentativeForm/ViceRepresentativeForm.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-type ViceRepresentativeFormProps = {};
-
-const ViceRepresentativeForm: FC<ViceRepresentativeFormProps> = () => {
-return <div>ViceRepresentativeForm Component</div>;
-};
-
-export default ViceRepresentativeForm;
