2818a37c5f19bf33587a4f4f5077f6fce2f137ca
hikahana <22.h.hanada.nutfes@gmail.com>
2025-03-24 23:25:18 +0900

    [feat] api繋ぎ込み

--- user/src/components/NotifyList/NotifyList.tsx
+++ user/src/components/NotifyList/NotifyList.tsx
@@ -1,23 +1,26 @@
 import { FC } from 'react';
-import { Announcement } from '@/types/announcement';
+import { useGetAnnouncements } from '@/api/announcementsAPI';
 import { format } from 'date-fns';
 import FormContainer from '@/components/FormContainer';
 
-type NotifyListProps = {
-  announcements: Announcement[];
-};
+type NotifyListProps = {};
 
-const NotifyList: FC<NotifyListProps> = ({ announcements }) => {
-  const formattedDates = announcements.map((item) =>
+const NotifyList: FC<NotifyListProps> = () => {
+  // FIX: ログイン認証実装で修正する。
+  const testGroupID = 1;
+
+  const { announcements, error, isLoading } = useGetAnnouncements(testGroupID);
+
+  const formattedDates = (announcements || []).map((item) =>
     format(new Date(item.created_at), 'yyyy/MM/dd')
   );
 
-  const announcementList = announcements.map((announcement, index) => {
+  const announcementList = announcements?.map((announcement, index) => {
+    const date = formattedDates[index] ?? 'お知らせはありません。';
+
     return (
-      <div className="flex flex-col gap-2">
-        <span className="w-24 text-base font-medium text-font">
-          {formattedDates[index]}
-        </span>
+      <div key={announcement.id} className="flex flex-col gap-2">
+        <span className="w-24 text-base font-medium text-font">{date}</span>
         <span className="w-56 text-base font-medium text-font">
           {announcement.message}
         </span>
@@ -31,7 +34,15 @@ const NotifyList: FC<NotifyListProps> = ({ announcements }) => {
         <div className="w-[497px] mb-10">
           <div className="text-4xl font-bold text-main">お知らせ</div>
         </div>
-        <div className="flex flex-col gap-4 pl-10">{announcementList}</div>
+        <div className="flex flex-col gap-4 pl-10">
+          {isLoading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div>Error occurred</div>
+          ) : (
+            announcementList
+          )}
+        </div>
       </FormContainer>
     </div>
   );
