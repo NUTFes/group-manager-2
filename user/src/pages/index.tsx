ce94ac1e0a88598afdf74487728a897806cda33e
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2025-03-26 22:32:40 +0900

    不要なコンポーネント削除

--- user/src/pages/index.tsx
+++ user/src/pages/index.tsx
@@ -62,7 +62,6 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <VenueApplication />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
