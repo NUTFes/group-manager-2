bac0393b09aeb40727acab3f348a5c5dec84f2c5
hikahana <22.h.hanada.nutfes@gmail.com>
2025-04-06 18:49:20 +0900

    lint修正

--- user/src/components/StageOptions/StageOptionForm/StageOptionForm.tsx
+++ user/src/components/StageOptions/StageOptionForm/StageOptionForm.tsx
@@ -10,6 +10,7 @@ import Radio from '@/components/Form/Radio';
 import FormContainer from '@/components/FormContainer';
 import { stageOptionSchema } from './schema';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 type StageOptionFormProps = {};
 
 const options1 = [
