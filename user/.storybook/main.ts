e0eb6a84f169875cd90d746a266a758627bcf9da
hikahana <22.h.hanada.nutfes@gmail.com>
2025-04-06 17:52:53 +0900

    lint修正

--- user/.storybook/main.ts
+++ user/.storybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
-    '@storybook/addon-docs',
   ],
   framework: {
     name: '@storybook/nextjs',
