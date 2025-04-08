f4a52dc9dd63e8eca4d3530d1fd9d8863df15a88
hikahana <22.h.hanada.nutfes@gmail.com>
2025-04-09 03:59:51 +0900

    fix: update呼び出し

--- user/src/components/StageOptions/StageOptionForm/hooks.ts
+++ user/src/components/StageOptions/StageOptionForm/hooks.ts
@@ -1,13 +1,12 @@
 import {
-  StageOption,
+  useCreateStageOptions,
   useGetStageOptions,
-  useMutateStageOptions,
+  useUpdateStageOptions,
 } from '@/api/stageOptionApi';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { mutate } from 'swr';
-import { stageOptionSchema } from './schema';
-import { StageOptionForm } from './schema';
+import { StageOptionForm, stageOptionSchema } from './schema';
 
 export const useStageOptionHooks = () => {
   const {
@@ -20,39 +19,63 @@ export const useStageOptionHooks = () => {
     resolver: zodResolver(stageOptionSchema),
     mode: 'onChange',
     defaultValues: {
-      groupId: 3,
+      groupId: 4,
     },
   });
 
   const values = watch();
 
-  const { trigger, error, isMutating } = useMutateStageOptions();
+  const {
+    trigger: create,
+    error: createError,
+    isMutating: createIsMutating,
+  } = useCreateStageOptions();
+
+  const {
+    trigger: update,
+    error: updateError,
+    isMutating: updateIsMutating,
+  } = useUpdateStageOptions();
 
   const { stageOptions, isLoading, hasError } = useGetStageOptions(
     values.groupId
   );
 
   // alert以外で通知したい。
-  const onSubmit = async (formData: StageOption) => {
-    try {
-      await trigger({ query: formData });
-      mutate(`/stage_common_options/${formData.groupId}`);
-      reset();
-      alert('送信しました');
-    } catch {
-      alert('送信に失敗しました。');
+  const onSubmit = async (formData: StageOptionForm) => {
+    if (stageOptions) {
+      try {
+        await update({ query: formData });
+        mutate(`/stage_common_options/group/${formData.groupId}`);
+        reset();
+        alert('送信しました');
+      } catch {
+        alert('送信に失敗しました。');
+      }
+    } else {
+      try {
+        await create({ query: formData });
+        mutate(`/stage_common_options/group/${formData.groupId}`);
+        reset();
+        alert('送信しました');
+      } catch {
+        alert('送信に失敗しました。');
+      }
     }
   };
 
   return {
     handleSubmit,
     errors,
-    isMutating,
     stageOptions,
     isLoading,
     hasError,
     onSubmit,
     setValue,
     values,
+    createError,
+    createIsMutating,
+    updateError,
+    updateIsMutating,
   };
 };
