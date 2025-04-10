edf09ea571d05b4b0d256e936011950c523d454f
GitHub Action <action@github.com>
2025-04-10 07:39:31 +0000

    chore: auto fix lint issues

--- user/src/components/FormList/FormList.tsx
+++ user/src/components/FormList/FormList.tsx
@@ -13,20 +13,20 @@ const FormList: FC<FormListProps> = ({ items, onEdit, isEdit }) => {
   return (
     <FormContainer>
       {items.map((item, index) => (
-        <div key={index} className="w-auto md:w-[400px] flex flex-col">
+        <div key={index} className="flex w-auto flex-col md:w-[400px]">
           <div className="flex">
             <div className="text-xs text-font">{item.label}</div>
             {item.isEditable && (
-              <span className="text-red-500 ml-6 text-xs">※変更できません</span>
+              <span className="ml-6 text-xs text-red-500">※変更できません</span>
             )}
           </div>
-          <div className="h-6 md:h-10 mt-2 text-base text-font font-medium">
+          <div className="mt-2 h-6 text-base font-medium text-font md:h-10">
             {item.content}
           </div>
         </div>
       ))}
       {isEdit && (
-        <div className="w-full flex justify-center items-center">
+        <div className="flex w-full items-center justify-center">
           <Button
             size="pc"
             color="main"
