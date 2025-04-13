3bf558856266da25dd4b791c651624977be081fe
kureshou <kure92350@gmail.com>
2025-04-14 05:49:20 +0900

    コンフリクトの解決

--- user/src/components/Form/Selector/Selector.tsx
+++ user/src/components/Form/Selector/Selector.tsx
@@ -44,12 +44,7 @@ const Selector: FC<SelectorProps> = ({
           className={`h-12 w-[400px] rounded-[10px] border-2 text-font ${error ? 'border-alert' : 'border-main'} mb-[4px]`}
         >
           {options.map((option) => (
-            <option
-              key={option.id}
-              value={option.id}
-              disabled={option.disabled}
-              hidden={option.disabled}
-            >
+            <option key={option.id} value={option.id} disabled={option.disabled} hidden={option.disabled}>
               {option.name}
             </option>
           ))}
