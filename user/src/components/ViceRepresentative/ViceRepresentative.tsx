98cf4b1176decfd5523ddb4273b6c7714f198d52
kureshou <kure92350@gmail.com>
2025-04-14 07:23:22 +0900

    AccordionMenuに合わせてViceRepresentativeを修正

--- user/src/components/ViceRepresentative/ViceRepresentative.tsx
+++ user/src/components/ViceRepresentative/ViceRepresentative.tsx
@@ -1,18 +1,12 @@
 import { FC } from 'react';
-import { useAtom, useAtomValue } from 'jotai';
 import AccordionMenu from '@/components/AccordionMenu';
 import ViceRepresentativeForm from '@/components/ViceRepresentative/ViceRepresentativeForm';
-import {
-  getIsOpenAccordionMenuAtom,
-  setIsOpenAccordionMenuAtom,
-} from './store';
+
 
 // TODO: pageからのデータはここのpropsでバケツリレーし、isEdit,isExistに入れる。
 type ViceRepresentativeFormProps = {};
 
 const ViceRepresentative: FC<ViceRepresentativeFormProps> = () => {
-  const isOpen = useAtomValue(getIsOpenAccordionMenuAtom);
-  const [, toggleIsOpen] = useAtom(setIsOpenAccordionMenuAtom);
 
   return (
     <AccordionMenu
@@ -20,9 +14,6 @@ const ViceRepresentative: FC<ViceRepresentativeFormProps> = () => {
       isEdit={false}
       isExist={false}
       required
-      isOpen={isOpen}
-      onToggle={toggleIsOpen}
-      onSubmit={() => {}}
     >
       <ViceRepresentativeForm />
     </AccordionMenu>
