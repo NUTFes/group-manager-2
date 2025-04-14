17eaa0faa58ead4db31a3cf6949dc04571c5cd88
hikahana <22.h.hanada.nutfes@gmail.com>
2025-04-15 02:16:01 +0900

    feat: react-toastifyを追加し、_app.tsxにToastContainerを組み込み

--- user/src/pages/_app.tsx
+++ user/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="max-h-svh">
       <Layout>
+        <ToastContainer />
         <Component {...pageProps} />
       </Layout>
     </div>
