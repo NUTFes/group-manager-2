8ce3893fbc496620c69abdfddd33b20c948c4c4e
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2025-04-18 01:33:19 +0900

    enumの修正

--- api/app/models/un_registered_group.rb
+++ api/app/models/un_registered_group.rb
@@ -2,7 +2,7 @@ class UnRegisteredGroup < ApplicationRecord
   belongs_to :group
 
   enum order_type: {
-    place_order: 0,
+    rental_item_order: 0,
     power_order: 1,
     sub_rep: 2,
     public_relation: 3,
