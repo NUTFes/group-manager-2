a0675d6a6b79179e8248efd0da89aa0e04c1362a
kureshou <kure92350@gmail.com>
2025-04-19 17:33:37 +0900

    prettierで成型

--- user/src/api/viceRepresentativeApi.ts
+++ user/src/api/viceRepresentativeApi.ts
@@ -1,12 +1,9 @@
-
-
-
-export type FormData ={
-    groupId: number;
-    isGroup: number;
-    name:string;
-    number:string;
-    grade:number;
-    field:number;
-    address:string;
+export type FormData = {
+  groupId: number;
+  isGroup: number;
+  name: string;
+  number: string;
+  grade: number;
+  field: number;
+  address: string;
 };
--- user/src/components/Applications/ViceRepresentative/ViceRepresentativeForm/ViceRepresentativeForm.tsx
+++ user/src/components/Applications/ViceRepresentative/ViceRepresentativeForm/ViceRepresentativeForm.tsx
@@ -1,99 +1,98 @@
 import { FC } from 'react';
-import FormContainer from '../../../FormContainer';
+import Button from '@/components/Button';
+import Selector from '@/components/Form/Selector';
 import Radio from '../../../Form/Radio';
 import TextBox from '../../../Form/TextBox';
-import Selector from '@/components/Form/Selector';
-import Button from '@/components/Button';
+import FormContainer from '../../../FormContainer';
 import { useViceRepresentativeHook } from './hook';
-import { FormData } from '@/api/viceRepresentativeApi';
 
 type ViceRepresentativeFormProps = {};
 
 const ViceRepresentativeForm: FC<ViceRepresentativeFormProps> = () => {
-    const {
-        handleSubmit,
-        setValue,
-        getValues,
-        errors ,
-        reset,
-        watch,
-        onSubmit,
-        option2,
-        optiongrade,
-        optionfield,
-        radioValue1,
-        textName,
-        textNumber,
-        valuegrade,
-        valuefield,
-        textAddress,
-    }=useViceRepresentativeHook();
+  const {
+    handleSubmit,
+    setValue,
+    getValues,
+    errors,
+    reset,
+    watch,
+    onSubmit,
+    option2,
+    optiongrade,
+    optionfield,
+    radioValue1,
+    textName,
+    textNumber,
+    valuegrade,
+    valuefield,
+    textAddress,
+  } = useViceRepresentativeHook();
 
-return (
+  return (
     <FormContainer>
-        <form onSubmit={handleSubmit(onSubmit)} className='w-full'>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+        <div>
+          <Radio
+            label="一人での参加ですか？"
+            onChange={(value) => setValue('isGroup', Number(value))}
+            options={option2}
+            required
+            value={radioValue1}
+            error={errors.isGroup?.message}
+          />
+          {radioValue1 === '1' && (
             <div>
-                <Radio
-                    label="一人での参加ですか？"
-                    onChange={(value) => setValue('isGroup',Number(value))}
-                    options={option2}
-                    required
-                    value={radioValue1}
-                    error={errors.isGroup?.message}
-                />
-                {radioValue1 === "1" &&(
-                    <div>
-                        <TextBox
-                            label='名前'
-                            value={textName}
-                            onChange={(value)=> setValue('name',value)}
-                            note='例：長岡　太郎'
-                            required = {true}
-                            error={errors.name?.message}
-                        />
-                        <TextBox
-                            label='学籍番号'
-                            value={textNumber}
-                            onChange={(value)=> setValue('number',value)}
-                            note='例：123456'
-                            required = {true}
-                            error={errors.number?.message}
-                        />
-                        <Selector
-                            label='課程・学年'
-                            value={valuegrade}
-                            onChange={(value)=> setValue('grade',Number(value))}
-                            required = {true}
-                            options={optiongrade}
-                            error={errors.grade?.message}
-                        />
-                        <Selector
-                            label='学科・専攻'
-                            value={valuefield}
-                            onChange={(value)=> setValue('field',Number(value))}
-                            required = {true}
-                            options={optionfield}
-                            error={errors.field?.message}
-                        />
-                        <TextBox
-                            label='メールアドレス'
-                            value={textAddress}
-                            onChange={(value)=> setValue('address',value)}
-                            note='例：123456@stn.nagaokaut.ac.jp'
-                            required = {true}
-                            error={errors.address?.message}
-                        />
-                    </div>
-                )}
+              <TextBox
+                label="名前"
+                value={textName}
+                onChange={(value) => setValue('name', value)}
+                note="例：長岡　太郎"
+                required={true}
+                error={errors.name?.message}
+              />
+              <TextBox
+                label="学籍番号"
+                value={textNumber}
+                onChange={(value) => setValue('number', value)}
+                note="例：123456"
+                required={true}
+                error={errors.number?.message}
+              />
+              <Selector
+                label="課程・学年"
+                value={valuegrade}
+                onChange={(value) => setValue('grade', Number(value))}
+                required={true}
+                options={optiongrade}
+                error={errors.grade?.message}
+              />
+              <Selector
+                label="学科・専攻"
+                value={valuefield}
+                onChange={(value) => setValue('field', Number(value))}
+                required={true}
+                options={optionfield}
+                error={errors.field?.message}
+              />
+              <TextBox
+                label="メールアドレス"
+                value={textAddress}
+                onChange={(value) => setValue('address', value)}
+                note="例：123456@stn.nagaokaut.ac.jp"
+                required={true}
+                error={errors.address?.message}
+              />
             </div>
-            <div className="w-full flex justify-center items-center mt-10">
-                <Button size="pc" color="main" type="submit" >
-                    登録
-                </Button>
-            </div>
-        </form>
+          )}
+        </div>
+        <div className="w-full flex justify-center items-center mt-10">
+          <Button size="pc" color="main" type="submit">
+            登録
+          </Button>
+        </div>
+      </form>
     </FormContainer>
-)
+  );
 };
 
 export default ViceRepresentativeForm;
