0ea5616de43517542ec371a773e41621d0cdc729
hikahana <22.h.hanada.nutfes@gmail.com>
2025-04-19 23:26:37 +0900

    feat: ログインと登録のモーダルを追加し、ホームページのレイアウトを更新

--- user/src/pages/index.tsx
+++ user/src/pages/index.tsx
@@ -1,18 +1,44 @@
+import { useState } from 'react';
+import LoginModal from '@/components/LoginModal';
 import NewsList from '@/components/NewsList';
+import RegisterCarousel from '@/components/RegisterCarousel';
+import WelcomeBox from '@/components/WelcomeBox';
 
 export default function Home() {
+  const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
+  const handleRegisterClick = () => {
+    return setIsRegisterOpen(true);
+  };
+
+  const handleLoginClick = () => {
+    return setIsLoginOpen(true);
+  };
+
+  const onCloseLogin = () => {
+    return setIsLoginOpen(false);
+  };
+
+  const onCloseRegister = () => {
+    return setIsRegisterOpen(false);
+  };
+
   return (
-    <div className="m-4 flex flex-col gap-10 lg:m-10 lg:flex-row">
-      <NewsList isLoginPage={true} />
-      <LoginForm />
-    </div>
+    <>
+      <RegisterCarousel isOpen={isRegisterOpen} onClose={onCloseRegister} />
+      <LoginModal isOpen={isLoginOpen} onClose={onCloseLogin} />
+      <div className="m-4 flex flex-col gap-10 lg:m-10 lg:flex-row">
+        <NewsList isLoginPage={true} />
+        <WelcomeBox
+          handleLoginClick={() => {
+            handleLoginClick();
+          }}
+          handleRegisterClick={() => {
+            handleRegisterClick();
+          }}
+        />
+      </div>
+    </>
   );
 }
-
-const LoginForm = () => {
-  return (
-    <div className="mx-auto size-[400px] bg-slate-500">
-      ログインコンポーネント
-    </div>
-  );
-};
