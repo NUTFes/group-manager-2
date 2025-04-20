5b2fa097762df0a0126efc87d22fb5076449621d
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2025-04-21 01:17:40 +0900

    modal作成

--- user/src/components/UserModal/UserModal.tsx
+++ user/src/components/UserModal/UserModal.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import CancelButton from '../CancelButton';
 import EditButton from '../EditButton';
 import LogoutButton from '../LogoutButton';
-import Modal from '../Modal';
 
 type UserModalProps = {
   isOpen: boolean;
@@ -18,21 +17,25 @@ export type UserInfo = {
 };
 
 const UserModal: FC<UserModalProps> = ({ isOpen, onClose, user }) => {
+  if (!isOpen) return null;
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="flex flex-col items-center gap-12 rounded-[20px] bg-white p-6 shadow-2xl">
-        <div className="absolute left-6 top-6">
+    <div className="fixed inset-0 z-50 flex justify-end">
+      <div className="absolute inset-0 bg-black/50" onClick={onClose} />
+      <div className="z-10 mx-[30px] my-[100px] flex h-[300px] max-w-[880px]">
+        <div className="absolute m-6">
           <CancelButton onClick={onClose} />
         </div>
-        <div className="flex w-[420px] flex-col items-center justify-center gap-6 py-4">
-          <p className="text-sm font-bold text-font">{user.role}</p>
-          <p className="text-3xl font-bold text-font">{user.name}</p>
-          <p className="text-base font-medium text-font">{user.email}</p>
-          <EditButton OnClick={onClose} />
-          <LogoutButton onClick={onClose} />
+        <div className="flex flex-col items-center gap-12 rounded-[20px] bg-white p-6 shadow-2xl">
+          <div className="flex w-[420px] flex-col items-center justify-center gap-6 py-4">
+            <p className="text-sm font-bold text-font">{user.role}</p>
+            <p className="text-3xl font-bold text-font">{user.name}</p>
+            <p className="text-base font-medium text-font">{user.email}</p>
+            <EditButton OnClick={onClose} />
+            <LogoutButton onClick={onClose} />
+          </div>
         </div>
       </div>
-    </Modal>
+    </div>
   );
 };
 
