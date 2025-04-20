00c9735d38cc0902794af54030402c1436e88d80
nose221834 <iatyy01151821@gmail.com>
2025-04-20 18:01:01 +0900

    test

--- user/src/pages/index.tsx
+++ user/src/pages/index.tsx
@@ -2,7 +2,7 @@ import NewsList from '@/components/NewsList';
 
 export default function Home() {
   return (
-    <div className="m-4 flex flex-col gap-10 lg:m-10 lg:flex-row">
+    <div className="flex m-4 flex-col gap-10 lg:m-10 lg:flex-row">
       <NewsList isLoginPage={true} />
       <LoginForm />
     </div>
