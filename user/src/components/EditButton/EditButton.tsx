01b9397e6f20847ba0e6b99e5f8cbb1a09d11979
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2025-04-21 00:44:47 +0900

    編集ボタン追加

---
+++ user/src/components/EditButton/EditButton.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import { MdModeEdit } from 'react-icons/md';
+
+type EditButtonProps = {
+  OnClick: () => void;
+};
+
+const EditButton: FC<EditButtonProps> = ({ OnClick }) => {
+  return (
+    <button className="flex gap-3" onClick={OnClick}>
+      <MdModeEdit color="#000000" size="24" />
+      <p className="text-base font-medium text-font">編集</p>
+    </button>
+  );
+};
+
+export default EditButton;
