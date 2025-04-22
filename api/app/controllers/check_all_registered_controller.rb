cb166ca93dd52b17fc43a785b75986542e333542
hikahana <22.h.hanada.nutfes@gmail.com>
2025-04-22 21:22:53 +0900

    check_statusメソッドを使用して、groupのステータスチェックを簡素化

--- api/app/controllers/check_all_registered_controller.rb
+++ api/app/controllers/check_all_registered_controller.rb
@@ -25,20 +25,19 @@ class CheckAllRegisteredController < ApplicationController
                            .to_set
 
     statuses = {
-      group:           present_or_unregistered?(group, unregistered_types, :group_application),
-      sub_rep:         present_or_unregistered?(group.sub_rep, unregistered_types, :sub_rep_application),
-      rental_item:     present_or_unregistered?(group.rental_orders.any?, unregistered_types, :rental_item_application),
-      place_order:     present_or_unregistered?(group.place_order, unregistered_types, :place_order_application),
-      stage_order:     present_or_unregistered?(group.stage_orders.any?, unregistered_types, :stage_order_application),
-      stage_option:    present_or_unregistered?(group.stage_common_option, unregistered_types, :stage_option_application),
-      power_order:     present_or_unregistered?(group.power_orders.any?, unregistered_types, :power_order_application),
-      public_relation: present_or_unregistered?(group.public_relation, unregistered_types, :public_relation_application),
-      employee:        present_or_unregistered?(group.employees.any?, unregistered_types, :employee_application),
-      venue_map:       present_or_unregistered?(group.venue_map, unregistered_types, :venue_map_application),
-      food_product:    present_or_unregistered?(group.food_products.any?, unregistered_types, :food_product_application),
-      purchase_list:   present_or_unregistered?(group.food_products.any? { |fp| fp.purchase_lists.any? }, unregistered_types, :purchase_list_application),
-      cooking_process: present_or_unregistered?(group.cooking_process_order, unregistered_types, :cooking_process_application),
-      fire_usage:      present_or_unregistered?(false, unregistered_types, :fire_usage_application)
+      group:                  group.present?,
+      sub_rep:                check_status(group.sub_rep, unregistered_types, :sub_rep_application),
+      rental_item:            check_status(group.rental_orders.any?, unregistered_types, :rental_item_application),
+      place_order:            group.place_order.present?,
+      stage_order:            group.stage_orders.any?,
+      stage_option:           group.stage_common_option.present?,
+      power_order:            check_status(group.power_orders.any?, unregistered_types, :power_order_application),
+      employee:               check_status(group.employees.any?, unregistered_types, :employee_application),
+      venue_map:              group.venue_map.present?,
+      food_product:           group.food_products.any?,
+      purchase_list:          group.food_products.any? { |fp| fp.purchase_lists.any? },
+      cooking_process_order:  group.cooking_process_order.present?,
+      fire_equipment_order:   check_status(group.fire_equipment_orders.any?, unregistered_types, :fire_equipment_order_application)
     }
 
     render json: fmt(:ok, statuses)
@@ -49,11 +48,11 @@ class CheckAllRegisteredController < ApplicationController
   private
 
   ORDER_TYPE_MAPPING = {
-    sub_rep_application:           'sub_rep',
-    rental_item_application:       'rental_item_order',
-    power_order_application:       'power_order',
-    public_relation_application:   'public_relation',
-    employee_application:          'employee'
+    sub_rep_application:              'sub_rep',
+    rental_item_application:          'rental_item_order',
+    power_order_application:          'power_order',
+    fire_equipment_order_application: 'fire_equipment_order',
+    employee_application:             'employee'
   }.with_indifferent_access.freeze
 
 
@@ -61,5 +60,13 @@ class CheckAllRegisteredController < ApplicationController
     is_present = value_or_condition.respond_to?(:present?) ? value_or_condition.present? : !!value_or_condition
     mapped_order_type = ORDER_TYPE_MAPPING[status_key]
     is_present || (mapped_order_type && unregistered_types.include?(mapped_order_type)) || false
+  end
+
+  def check_status(value_or_condition, unregistered_types, status_key)
+    if ORDER_TYPE_MAPPING.key?(status_key)
+      present_or_unregistered?(value_or_condition, unregistered_types, status_key)
+    else
+      value_or_condition.respond_to?(:present?) ? value_or_condition.present? : !!value_or_condition
+    end
   end
 end
