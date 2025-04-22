051a3811ab1d6acc644539733e6f98596caf5141
nose221834 <iatyy01151821@gmail.com>
2025-04-22 20:20:07 +0900

    feat: 公式の通りに修正

--- api/app/controllers/users/registrations_controller.rb
+++ api/app/controllers/users/registrations_controller.rb
@@ -1,3 +1,4 @@
+# app/controllers/users/registrations_controller.rb
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
@@ -5,9 +6,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def respond_with(resource, _opts = {})
     if resource.persisted?
-      render json: { status: 'success', data: resource }, status: :created
+      render json: {
+        status: {
+          code: 200,
+          message: 'Signed up successfully.'
+        },
+        data: resource.as_json(only: [:id, :email, :created_at])
+      }, status: :created
     else
-      render json: { status: 'error', errors: resource.errors.full_messages }, status: :unprocessable_entity
+      render json: {
+        status: {
+          message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"
+        }
+      }, status: :unprocessable_entity
     end
   end
 end
--- api/app/controllers/users/sessions_controller.rb
+++ api/app/controllers/users/sessions_controller.rb
@@ -1,14 +1,30 @@
+# app/controllers/users/sessions_controller.rb
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
   private
 
   def respond_with(resource, _opts = {})
-    token = request.env['warden-jwt_auth.token']
-    render json: { id: resource.id, email: resource.email, token: token }, status: :ok
+    render json: {
+      status: {
+        code: 200,
+        message: 'Logged in successfully.'
+      },
+      data: resource.as_json(only: [:id, :email, :created_at])
+    }, status: :ok
   end
 
   def respond_to_on_destroy
-    head :no_content
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Logged out successfully.'
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
   end
 end
