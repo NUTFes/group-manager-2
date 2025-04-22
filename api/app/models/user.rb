fb09606f5373abdbadb3e76ca99eb777d3492d20
nose221834 <iatyy01151821@gmail.com>
2025-04-22 14:28:45 +0900

    feat: userにjwtを追加

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -4,7 +4,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
+  # include DeviseTokenAuth::Concerns::User など残す場合は、その下に配置
   include DeviseTokenAuth::Concerns::User
   belongs_to :role
   has_one :user_detail, dependent: :destroy
