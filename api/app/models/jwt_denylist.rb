29f486acb7f9475ff6b0aca78437a8fbe938f808
nose221834 <iatyy01151821@gmail.com>
2025-04-22 15:51:02 +0900

    feat: sign_out時にjwtをdenylistsに追加

--- api/app/models/jwt_denylist.rb
+++ api/app/models/jwt_denylist.rb
@@ -1,2 +1,8 @@
+# app/models/jwt_denylist.rb
 class JwtDenylist < ApplicationRecord
+  # ここで Denylist 戦略を include する
+  include Devise::JWT::RevocationStrategies::Denylist
+
+  # マイグレーションで作成したテーブル名が 'jwt_denylists' の場合は次を追記
+  self.table_name = 'jwt_denylists'
 end
