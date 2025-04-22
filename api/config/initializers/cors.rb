a5b49d8e57a10cb1e883d990cd76f6f8c6de36c7
nose221834 <iatyy01151821@gmail.com>
2025-04-22 14:37:58 +0900

    feat: Authorizationを追加

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -13,6 +13,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['access-token', 'client', 'uid']
+      expose: ['access-token', 'client', 'uid','Authorization']
   end
 end
