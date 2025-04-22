aaed29f7eba6e57c0b5d05f79c476a185f488528
nose221834 <iatyy01151821@gmail.com>
2025-04-22 14:23:08 +0900

    feat: credentialsから情報を取得

--- api/config/initializers/devise.rb
+++ api/config/initializers/devise.rb
@@ -26,6 +26,10 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+  end
+
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
