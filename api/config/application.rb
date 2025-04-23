6cc33cefca8c217e245360411df82f13adda6ae2
nose221834 <iatyy01151821@gmail.com>
2025-04-24 05:06:30 +0900

    fix: CORS設定を無効化するためのコードをコメントアウト

--- api/config/application.rb
+++ api/config/application.rb
@@ -33,11 +33,13 @@ module Myapp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Credentials' => 'true',
-      # 'Access-Control-Allow-Origin' => 'http://localhost:8080',
-      'Access-Control-Request-Method' => '*'
-    }
-    config.middleware.delete ActionDispatch::HostAuthorization
+
+    # CORSが効かなくなるため無効化
+    # config.action_dispatch.default_headers = {
+    #   'Access-Control-Allow-Credentials' => 'true',
+    #   # 'Access-Control-Allow-Origin' => 'http://localhost:8080',
+    #   'Access-Control-Request-Method' => '*'
+    # }
+    # config.middleware.delete ActionDispatch::HostAuthorization
   end
 end
