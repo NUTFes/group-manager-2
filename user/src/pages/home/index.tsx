af4ef478e370b0c2fc1c663b0bab51056c978b80
hikahana <22.h.hanada.nutfes@gmail.com>
2025-04-25 19:29:57 +0900

    [refactor] HomePageコンポーネントの構造を改善し、ApplicationFormに名前プロパティを追加

--- user/src/pages/home/index.tsx
+++ user/src/pages/home/index.tsx
@@ -1,27 +1,46 @@
+import { useGetCheckAllRegisteredGroups } from '@/api/checkAllRegisteredApi';
+import { useGetUserPageSettings } from '@/api/userPageSettingAPI';
+import Group from '@/components/Applications/Group';
+import Power from '@/components/Applications/Power';
+import PublicRelations from '@/components/Applications/PublicRelations';
+import Stage from '@/components/Applications/Stage';
+import StageOptions from '@/components/Applications/StageOptions';
 import NewsList from '@/components/NewsList';
 
 export default function HomePage() {
+  const { userPageSettings } = useGetUserPageSettings();
+  const { checkAllRegisteredGroups } = useGetCheckAllRegisteredGroups(1);
+
   return (
-    <div className="m-4 flex flex-col-reverse justify-center gap-4 lg:mx-10 lg:my-16 lg:flex-row">
-      <ApplicationForm />
-      <div className="flex justify-center">
+    <div className="m-4 flex flex-col gap-10 lg:mx-10 lg:my-16 lg:flex-row">
+      <div className="flex flex-1 flex-col">
+        <Group
+          isDeadline={userPageSettings?.isRegistGroup}
+          isRegistered={checkAllRegisteredGroups?.group}
+        />
+        <ApplicationForm name="副代表申請" />
+        <ApplicationForm name="会場申請" />
+        <ApplicationForm name="物品申請" />
+        <Stage isDeadline={userPageSettings?.isEditStageOrder} />
+        <StageOptions isDeadline={userPageSettings?.isEditStageCommonOption} />
+        <Power isDeadline={userPageSettings?.isEditStageOrder} />
+        <PublicRelations
+          groupId={1}
+          isDeadline={userPageSettings?.isEditPublicRelation}
+        />
+      </div>
+      <div className="flex flex-1 justify-center">
         <NewsList isLoginPage={false} />
       </div>
     </div>
   );
 }
 
-const ApplicationForm = () => {
+const ApplicationForm = ({ name }: { name: string }) => {
   return (
-    <div className="flex w-full flex-col gap-20">
-      <div className="text-4xl font-bold text-main">申請登録</div>
-      <div className="flex flex-col gap-10">
-        <div className="mx-auto flex h-20 w-[450px] items-center justify-center bg-slate-500 md:w-[560px]">
-          団体申請
-        </div>
-        <div className="mx-auto flex h-20 w-[450px] items-center justify-center bg-slate-500 md:w-[560px]">
-          団体申請
-        </div>
+    <div className="flex flex-col gap-10">
+      <div className="mx-auto flex h-20 w-[450px] items-center justify-center bg-slate-500 md:w-[560px]">
+        {name}
       </div>
     </div>
   );
