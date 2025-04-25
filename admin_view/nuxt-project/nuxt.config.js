cb6e53b2dfc747307edd6e68a9e5aa52041fc2a8
nose221834 <iatyy01151821@gmail.com>
2025-04-25 15:31:20 +0900

    feat: 認証付きでダウンロードを行う関数を切り出し

--- admin_view/nuxt-project/nuxt.config.js
+++ admin_view/nuxt-project/nuxt.config.js
@@ -43,6 +43,7 @@ export default {
     { src: "~/plugins/fileter.js", ssr: false },
     { src: "~/plugins/firebase.js" },
     { src: "~/plugins/role" },
+    { src: '~/plugins/auth-axios.js', mode: 'client' },
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
