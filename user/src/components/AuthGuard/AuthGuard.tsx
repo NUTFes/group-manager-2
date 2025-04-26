9dae08cd49a283a432b3365e049ac7fd56ea7062
nose221834 <iatyy01151821@gmail.com>
2025-04-27 01:23:41 +0900

    feat: 認証のガードをするコンポーネントを作成

---
+++ user/src/components/AuthGuard/AuthGuard.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+
+function AuthGuard({ children }: { children: React.ReactNode }) {
+  // 認証状態を取得
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  // トップページのURLを取得
+  const isAuthPage = router.pathname === '/';
+
+  // 認証されていない場合、トップページ以外にいる場合はリダイレクト
+  if (status === 'loading') {
+    return null; // 読み込み中は何も表示しない
+  }
+
+  // 認証されていない場合、トップページ以外にいる場合はリダイレクト
+  if (!session && !isAuthPage) {
+    // 未認証かつ、トップページ以外にいる場合はリダイレクト
+    router.replace('/');
+    return null;
+  }
+
+  // 認証されている場合、子コンポーネントを表示
+  return <>{children}</>;
+}
+
+export default AuthGuard;
