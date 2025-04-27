c23d37df6c96fb188673d9917cd684917b173a24
nose221834 <iatyy01151821@gmail.com>
2025-04-27 13:00:50 +0900

    feat: sessionの状態遷移をログ出力

--- user/src/components/AuthGuard/AuthGuard.tsx
+++ user/src/components/AuthGuard/AuthGuard.tsx
@@ -11,11 +11,13 @@ function AuthGuard({ children }: { children: React.ReactNode }) {
 
   // 認証されていない場合、トップページ以外にいる場合はリダイレクト
   if (status === 'loading') {
+    console.log('Loading...'); // 認証状態が読み込み中
     return null; // 読み込み中は何も表示しない
   }
 
   // 認証されていない場合、トップページ以外にいる場合はリダイレクト
   if (status === 'unauthenticated' && !isAuthPage) {
+    console.log('Redirecting to login page...'); // 認証されていない場合
     // 未認証かつ、トップページ以外にいる場合はリダイレクト
     router.replace('/');
     return null;
