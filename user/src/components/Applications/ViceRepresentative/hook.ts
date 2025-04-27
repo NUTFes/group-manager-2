bddec74540462bbee6b4f6ce923f9541c0d1c85e
kureshou <kure92350@gmail.com>
2025-04-28 06:38:50 +0900

    unRegi申請すると専用のformlistを表示するように変更

--- user/src/components/Applications/ViceRepresentative/hook.ts
+++ user/src/components/Applications/ViceRepresentative/hook.ts
@@ -1,43 +1,70 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { useGetUnregisteredGroup } from '@/api/unRegisteredGroupApi';
 import { useGetViceRepresentatives } from '@/api/viceRepresentativesApi';
 import { FormItem } from '@/components/FormList/type';
+import { ORDER_TYPES } from '../Power';
 import { viceRepresentativeLabels } from '../label';
 
 export const useViceRepresentativeHook = () => {
+  const groupId = 3; // TODO: groupIdを取得する方法を考える
   const { viceRepresentative, isLoading, hasError } =
-    useGetViceRepresentatives(3);
+    useGetViceRepresentatives(groupId);
+  const { unregisteredData } = useGetUnregisteredGroup(
+    groupId,
+    ORDER_TYPES.SUB_REP
+  );
+  const isUnregistered = !!unregisteredData;
 
-  const formItem: FormItem[] = [
-    {
-      label: viceRepresentativeLabels[1],
-      content: viceRepresentative?.name ?? '',
-    },
-    {
-      label: viceRepresentativeLabels[2],
-      content: viceRepresentative?.studentId ?? '',
-    },
-    {
-      label: viceRepresentativeLabels[3],
-      content: viceRepresentative?.gradeId ?? '',
-    },
-    {
-      label: viceRepresentativeLabels[4],
-      content: viceRepresentative?.departmentId ?? '',
-    },
-    {
-      label: viceRepresentativeLabels[5],
-      content: viceRepresentative?.email ?? '',
-    },
-    {
-      label: viceRepresentativeLabels[6],
-      content: viceRepresentative?.tel ?? '',
-    },
-  ];
+  const formItem: FormItem[] = useMemo(() => {
+    if (isUnregistered) {
+      return [
+        {
+          label: '副代表登録は不要（申請済み）',
+          content: 'あなたは１人での参加です',
+        },
+      ];
+    }
+    if (!viceRepresentative) {
+      return [];
+    }
+    return [
+      {
+        label: viceRepresentativeLabels[1],
+        content: viceRepresentative?.name ?? '',
+      },
+      {
+        label: viceRepresentativeLabels[2],
+        content: viceRepresentative?.studentId ?? '',
+      },
+      {
+        label: viceRepresentativeLabels[3],
+        content: viceRepresentative?.gradeId ?? '',
+      },
+      {
+        label: viceRepresentativeLabels[4],
+        content: viceRepresentative?.departmentId ?? '',
+      },
+      {
+        label: viceRepresentativeLabels[5],
+        content: viceRepresentative?.email ?? '',
+      },
+      {
+        label: viceRepresentativeLabels[6],
+        content: viceRepresentative?.tel ?? '',
+      },
+    ];
+  }, [isUnregistered, viceRepresentative]);
+
   const [isEditing, setIsEditing] = useState(true);
 
   const toEdit = () => {
     setIsEditing(!isEditing);
   };
+  useEffect(() => {
+    if (isUnregistered) {
+      setIsEditing(false);
+    }
+  }, [isUnregistered]);
 
   useEffect(() => {
     if (viceRepresentative) {
