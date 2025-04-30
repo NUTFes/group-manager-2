58b74e77e81a04585216f632d5efec15afd3e9b7
harata-t <22.t.harata.nutfes@gmail.com>
2025-05-01 03:16:17 +0900

    user_page_settingsのbooleanを反転する

--- user/src/components/Applications/PublicRelations/PublicRelations.tsx
+++ user/src/components/Applications/PublicRelations/PublicRelations.tsx
@@ -73,7 +73,7 @@ const PublicRelations: FC<PublicRelationsProps> = ({
   return (
     <AccordionMenu
       title="PR文申請"
-      isEdit={isDeadline}
+      isEdit={!isDeadline}
       isExist={isRegistered}
       required
     >
