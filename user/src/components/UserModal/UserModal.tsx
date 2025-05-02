b44d1733575128d381c46133e749f43ee8a77625
nose221834 <iatyy01151821@gmail.com>
2025-05-03 02:14:37 +0900

    fix: componentsにsession情報を持たせないよう修正

--- user/src/components/UserModal/UserModal.tsx
+++ user/src/components/UserModal/UserModal.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { signOut, useSession } from 'next-auth/react';
+import { FC, useEffect, useState } from 'react';
+import { signOut } from 'next-auth/react';
 import { toast } from 'react-toastify';
 import CancelButton from '../CancelButton';
 import EditButton from '../EditButton';
@@ -15,8 +15,25 @@ export type UserInfo = {
   email: string;
 };
 
+const getUserInfo = async (): Promise<UserInfo> => {
+  const res = await fetch('/api/getUser');
+  const data = await res.json();
+  return data;
+};
+
 const UserModal: FC<UserModalProps> = ({ isOpen, onClose }) => {
-  const { data: session } = useSession();
+  const [user, setUser] = useState<UserInfo>({
+    name: '',
+    email: '',
+  });
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userInfo = await getUserInfo();
+      setUser(userInfo);
+    };
+    fetchUser();
+  }, []);
 
   const handleLogout = async () => {
     try {
@@ -40,10 +57,10 @@ const UserModal: FC<UserModalProps> = ({ isOpen, onClose }) => {
         <div className="flex flex-col items-center gap-12 rounded-[20px] bg-white p-6 shadow-2xl">
           <div className="flex w-[420px] flex-col items-center justify-center gap-6 py-4">
             <p className="text-3xl font-bold text-font">
-              {session?.user?.name || 'ゲスト'}
+              {user.name || 'ゲスト'}
             </p>
             <p className="text-base font-medium text-font">
-              {session?.user?.email || 'メールアドレスなし'}
+              {user.email || 'メールアドレスなし'}
             </p>
             <EditButton OnClick={onClose} />
             <LogoutButton onClick={handleLogout} />
