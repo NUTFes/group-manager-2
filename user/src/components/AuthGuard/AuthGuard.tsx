abf37b59e52651affac7642205a8d9993cb95271
ichinose takumi <115196617+nose221834@users.noreply.github.com>
2025-05-06 18:43:24 +0900

    Update user/src/components/AuthGuard/AuthGuard.tsx
    Co-authored-by: harata-t <107479066+harata-t@users.noreply.github.com>

--- user/src/components/AuthGuard/AuthGuard.tsx
+++ user/src/components/AuthGuard/AuthGuard.tsx
@@ -9,21 +9,18 @@ function AuthGuard({ children }: { children: React.ReactNode }) {
   // トップページのURLを取得
   const isAuthPage = router.pathname === '/';
 
-  // 認証されていない場合、トップページ以外にいる場合はリダイレクト
+  // 読み込み中は何も表示しない
   if (status === 'loading') {
-    console.log('Loading...'); // 認証状態が読み込み中
-    return null; // 読み込み中は何も表示しない
+    return null;
   }
 
-  // 認証されていない場合、トップページ以外にいる場合はリダイレクト
+  // 未認証かつ、トップページ以外にいる場合はリダイレクト
   if (status === 'unauthenticated' && !isAuthPage) {
-    console.log('Redirecting to login page...'); // 認証されていない場合
-    // 未認証かつ、トップページ以外にいる場合はリダイレクト
     router.replace('/');
     return null;
   }
 
-  // 認証されている場合、子コンポーネントを表示
+  // 認証済みの場合、子コンポーネントを表示
   return <>{children}</>;
 }
 
