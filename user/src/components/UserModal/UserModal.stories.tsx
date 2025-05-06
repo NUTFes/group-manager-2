47c4811094f4bc3050bc5e2401c137a72c655e48
nose221834 <iatyy01151821@gmail.com>
2025-05-06 21:52:52 +0900

    fix: type Error

--- user/src/components/UserModal/UserModal.stories.tsx
+++ user/src/components/UserModal/UserModal.stories.tsx
@@ -23,13 +23,5 @@ export const Default: Story = {
     onClose: () => {
       console.log('Modal closed');
     },
-    user: {
-      id: 1,
-      email: 's223301@stn.nagaokaut.ac.jp',
-      name: 'John Doe',
-      role_id: 1,
-      created_at: '2023-10-01T00:00:00Z',
-      updated_at: '2023-10-01T00:00:00Z',
-    },
   },
 };
