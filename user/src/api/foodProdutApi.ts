2cadda155a78d0dba5d7af1d068807240ad68fac
hikahana <22.h.hanada.nutfes@gmail.com>
2025-05-28 22:05:11 +0900

    fix: hookにもbulk追加

--- user/src/api/foodProdutApi.ts
+++ user/src/api/foodProdutApi.ts
@@ -50,6 +50,27 @@ export const useGetFoodProducts = () => {
   };
 };
 
+/**
+ * 単一作成用hook
+ */
+export const useCreateFoodProduct = () => {
+  const endpoint = `${API_ENDPOINTS.FOOD_PRODUCTS}`;
+  const { trigger, isMutating, error } = useAuthenticatedPost(endpoint);
+
+  const createFoodProduct = async (foodProduct: FoodProduct) => {
+    return trigger({ body: foodProduct });
+  };
+
+  return {
+    createFoodProduct,
+    isMutating,
+    error,
+  };
+};
+
+/**
+ * 複数作成用hook
+ */
 export const useCreateFoodProducts = () => {
   const endpoint = `${API_ENDPOINTS.FOOD_PRODUCTS}`;
   const { trigger, isMutating, error } = useAuthenticatedPost(endpoint);
@@ -68,6 +89,29 @@ export const useCreateFoodProducts = () => {
   };
 };
 
+/**
+ * 単一更新用hook
+ */
+export const useUpdateFoodProduct = () => {
+  const endpoint = `${API_ENDPOINTS.FOOD_PRODUCTS}`;
+  const { trigger, isMutating, error } = useAuthenticatedPatch(endpoint);
+
+  const updateFoodProduct = async (
+    foodProduct: FoodProduct & { id: number }
+  ) => {
+    return trigger({ body: foodProduct });
+  };
+
+  return {
+    updateFoodProduct,
+    isMutating,
+    error,
+  };
+};
+
+/**
+ * 複数更新用hook
+ */
 export const useUpdateFoodProducts = () => {
   const endpoint = `${API_ENDPOINTS.FOOD_PRODUCTS}`;
   const { trigger, isMutating, error } = useAuthenticatedPatch(endpoint);
