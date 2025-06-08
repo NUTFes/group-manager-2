c3ed96b7611b0b591200f6cb14a0875cd322ee95
hikahana <22.h.hanada.nutfes@gmail.com>
2025-06-08 21:25:27 +0900

    fix: rswagの変なのがあったから修正

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_06_095258) do
+ActiveRecord::Schema.define(version: 2025_06_06_104949) do
 
   create_table "announcements", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "group_id"
