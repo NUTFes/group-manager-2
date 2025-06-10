a6044818637124bc8b8f42303a098bcebdecdcfa
harata-t <22.t.harata.nutfes@gmail.com>
2025-06-11 08:58:08 +0900

    groupCategoryによる表示の切り替え

--- user/src/pages/home/index.tsx
+++ user/src/pages/home/index.tsx
@@ -13,6 +13,7 @@ import PurchaseLists from '@/components/Applications/PurchaseLists/PurchaseLists
 import Stage from '@/components/Applications/Stage';
 import StageOptions from '@/components/Applications/StageOptions';
 import VenueApplications from '@/components/Applications/VenueApplication';
+import VenueMap from '@/components/Applications/VenueMap';
 import ViceRepresentative from '@/components/Applications/ViceRepresentative';
 import NewsList from '@/components/NewsList';
 import { useUser } from '@/hooks/useUser';
@@ -30,74 +31,7 @@ export default function HomePage() {
 
   const GroupCategoryContent = () => {
     if (groupCategoryId === GROUP_CATEGORY.FOOD_SALES) {
-      return (
-        <>
-          <Employees
-            isDeadline={!userPageSettings?.isEditEmployee}
-            isRegistered={checkAllRegisteredGroups?.employee}
-            mutateCheckAllRegisteredGroups={mutateCheckAllRegisteredGroups}
-            groupId={groupId}
-          />
-          <FoodProduct
-            groupId={groupId}
-            isDeadline={!userPageSettings?.isEditFoodProduct}
-            isRegistered={checkAllRegisteredGroups?.foodProduct}
-          />
-          <PurchaseLists
-            isDeadline={!userPageSettings?.isEditPurchaseList}
-            isRegistered={checkAllRegisteredGroups?.purchaseList}
-            groupId={groupId}
-          />
-        </>
-      );
-    } else if (groupCategoryId === GROUP_CATEGORY.COMMITTEE) {
-      return (
-        <>
-          <RentItems
-            isDeadline={!userPageSettings?.isEditRentalOrder}
-            isRegistered={checkAllRegisteredGroups?.rentalItem}
-            groupId={groupId}
-            groupCategoryId={groupCategoryId}
-          />
-          <Power
-            isDeadline={!userPageSettings?.isEditPowerOrder}
-            isRegistered={checkAllRegisteredGroups?.powerOrder}
-            groupId={groupId}
-          />
-        </>
-      );
-    } else if (groupCategoryId === GROUP_CATEGORY.STAGE) {
-      return (
-        <>
-          <Stage
-            isDeadline={!userPageSettings?.isEditStageOrder}
-            isRegistered={checkAllRegisteredGroups?.stageOrder}
-            groupId={groupId}
-          />
-          <StageOptions
-            isDeadline={!userPageSettings?.isEditStageCommonOption}
-            isRegistered={checkAllRegisteredGroups?.stageOption}
-            groupId={groupId}
-          />
-          <RentItems
-            isDeadline={!userPageSettings?.isEditRentalOrder}
-            isRegistered={checkAllRegisteredGroups?.rentalItem}
-            groupId={groupId}
-            groupCategoryId={groupCategoryId}
-          />
-          <Power
-            isDeadline={!userPageSettings?.isEditPowerOrder}
-            isRegistered={checkAllRegisteredGroups?.powerOrder}
-            groupId={groupId}
-          />
-          <PublicRelations
-            isDeadline={!userPageSettings?.isEditPublicRelation}
-            isRegistered={checkAllRegisteredGroups?.publicRelation}
-            groupId={groupId}
-          />
-        </>
-      );
-    } else {
+      // 🍙 食品販売: 会場申請、物品申請、電力申請、PR文申請、従業員申請、模擬店平面図申請、販売品申請、購入品申請、調理工程申請、火器使用申請
       return (
         <>
           <VenueApplications
@@ -121,14 +55,194 @@ export default function HomePage() {
             isRegistered={checkAllRegisteredGroups?.publicRelation}
             groupId={groupId}
           />
+          <Employees
+            isDeadline={!userPageSettings?.isEditEmployee}
+            isRegistered={checkAllRegisteredGroups?.employee}
+            mutateCheckAllRegisteredGroups={mutateCheckAllRegisteredGroups}
+            groupId={groupId}
+          />
+          <VenueMap
+            isDeadline={!userPageSettings?.isEditVenueMap}
+            isRegistered={checkAllRegisteredGroups?.venueMap}
+            groupId={groupId}
+          />
           <FoodProduct
             groupId={groupId}
             isDeadline={!userPageSettings?.isEditFoodProduct}
             isRegistered={checkAllRegisteredGroups?.foodProduct}
           />
+          <PurchaseLists
+            isDeadline={!userPageSettings?.isEditPurchaseList}
+            isRegistered={checkAllRegisteredGroups?.purchaseList}
+            groupId={groupId}
+          />
           <CookingProcessOrder
             isDeadline={!userPageSettings?.isEditCookingProcess}
             isRegistered={checkAllRegisteredGroups?.cookingProcessOrder}
+            groupId={groupId}
+          />
+          {/* TODO: 火器使用申請コンポーネントを追加予定 */}
+        </>
+      );
+    } else if (groupCategoryId === GROUP_CATEGORY.GOODS_SALES) {
+      // 📦 物品販売: 会場申請、物品申請、電力申請、PR文申請
+      return (
+        <>
+          <VenueApplications
+            isDeadline={!userPageSettings?.isEditPlace}
+            isRegistered={checkAllRegisteredGroups?.placeOrder}
+            groupId={groupId}
+          />
+          <RentItems
+            isDeadline={!userPageSettings?.isEditRentalOrder}
+            isRegistered={checkAllRegisteredGroups?.rentalItem}
+            groupId={groupId}
+            groupCategoryId={groupCategoryId}
+          />
+          <Power
+            isDeadline={!userPageSettings?.isEditPowerOrder}
+            isRegistered={checkAllRegisteredGroups?.powerOrder}
+            groupId={groupId}
+          />
+          <PublicRelations
+            isDeadline={!userPageSettings?.isEditPublicRelation}
+            isRegistered={checkAllRegisteredGroups?.publicRelation}
+            groupId={groupId}
+          />
+        </>
+      );
+    } else if (groupCategoryId === GROUP_CATEGORY.STAGE) {
+      // 🎤 ステージ団体: ステージ申請、ステージオプション申請、電力申請、PR文申請
+      return (
+        <>
+          <Stage
+            isDeadline={!userPageSettings?.isEditStageOrder}
+            isRegistered={checkAllRegisteredGroups?.stageOrder}
+            groupId={groupId}
+          />
+          <StageOptions
+            isDeadline={!userPageSettings?.isEditStageCommonOption}
+            isRegistered={checkAllRegisteredGroups?.stageOption}
+            groupId={groupId}
+          />
+          <Power
+            isDeadline={!userPageSettings?.isEditPowerOrder}
+            isRegistered={checkAllRegisteredGroups?.powerOrder}
+            groupId={groupId}
+          />
+          <PublicRelations
+            isDeadline={!userPageSettings?.isEditPublicRelation}
+            isRegistered={checkAllRegisteredGroups?.publicRelation}
+            groupId={groupId}
+          />
+        </>
+      );
+    } else if (groupCategoryId === GROUP_CATEGORY.RESEARCH_LAB) {
+      // 🔬 研究室公開: 会場申請、物品申請、電力申請、PR文申請、火器使用申請
+      return (
+        <>
+          <VenueApplications
+            isDeadline={!userPageSettings?.isEditPlace}
+            isRegistered={checkAllRegisteredGroups?.placeOrder}
+            groupId={groupId}
+          />
+          <RentItems
+            isDeadline={!userPageSettings?.isEditRentalOrder}
+            isRegistered={checkAllRegisteredGroups?.rentalItem}
+            groupId={groupId}
+            groupCategoryId={groupCategoryId}
+          />
+          <Power
+            isDeadline={!userPageSettings?.isEditPowerOrder}
+            isRegistered={checkAllRegisteredGroups?.powerOrder}
+            groupId={groupId}
+          />
+          <PublicRelations
+            isDeadline={!userPageSettings?.isEditPublicRelation}
+            isRegistered={checkAllRegisteredGroups?.publicRelation}
+            groupId={groupId}
+          />
+          {/* TODO: 火器使用申請コンポーネントを追加予定 */}
+        </>
+      );
+    } else if (groupCategoryId === GROUP_CATEGORY.EXHIBITION) {
+      // 🎨 展示・体験: 会場申請、物品申請、電力申請、PR文申請、火器使用申請
+      return (
+        <>
+          <VenueApplications
+            isDeadline={!userPageSettings?.isEditPlace}
+            isRegistered={checkAllRegisteredGroups?.placeOrder}
+            groupId={groupId}
+          />
+          <RentItems
+            isDeadline={!userPageSettings?.isEditRentalOrder}
+            isRegistered={checkAllRegisteredGroups?.rentalItem}
+            groupId={groupId}
+            groupCategoryId={groupCategoryId}
+          />
+          <Power
+            isDeadline={!userPageSettings?.isEditPowerOrder}
+            isRegistered={checkAllRegisteredGroups?.powerOrder}
+            groupId={groupId}
+          />
+          <PublicRelations
+            isDeadline={!userPageSettings?.isEditPublicRelation}
+            isRegistered={checkAllRegisteredGroups?.publicRelation}
+            groupId={groupId}
+          />
+          {/* TODO: 火器使用申請コンポーネントを追加予定 */}
+        </>
+      );
+    } else if (groupCategoryId === GROUP_CATEGORY.COMMITTEE) {
+      // 📋 実行委員会: 会場申請、物品申請、電力申請、PR文申請
+      return (
+        <>
+          <VenueApplications
+            isDeadline={!userPageSettings?.isEditPlace}
+            isRegistered={checkAllRegisteredGroups?.placeOrder}
+            groupId={groupId}
+          />
+          <RentItems
+            isDeadline={!userPageSettings?.isEditRentalOrder}
+            isRegistered={checkAllRegisteredGroups?.rentalItem}
+            groupId={groupId}
+            groupCategoryId={groupCategoryId}
+          />
+          <Power
+            isDeadline={!userPageSettings?.isEditPowerOrder}
+            isRegistered={checkAllRegisteredGroups?.powerOrder}
+            groupId={groupId}
+          />
+          <PublicRelations
+            isDeadline={!userPageSettings?.isEditPublicRelation}
+            isRegistered={checkAllRegisteredGroups?.publicRelation}
+            groupId={groupId}
+          />
+        </>
+      );
+    } else {
+      // その他のカテゴリー: デフォルトは基本的な申請のみ
+      return (
+        <>
+          <VenueApplications
+            isDeadline={!userPageSettings?.isEditPlace}
+            isRegistered={checkAllRegisteredGroups?.placeOrder}
+            groupId={groupId}
+          />
+          <RentItems
+            isDeadline={!userPageSettings?.isEditRentalOrder}
+            isRegistered={checkAllRegisteredGroups?.rentalItem}
+            groupId={groupId}
+            groupCategoryId={groupCategoryId}
+          />
+          <Power
+            isDeadline={!userPageSettings?.isEditPowerOrder}
+            isRegistered={checkAllRegisteredGroups?.powerOrder}
+            groupId={groupId}
+          />
+          <PublicRelations
+            isDeadline={!userPageSettings?.isEditPublicRelation}
+            isRegistered={checkAllRegisteredGroups?.publicRelation}
             groupId={groupId}
           />
         </>
