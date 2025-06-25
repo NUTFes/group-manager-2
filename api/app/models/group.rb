320c5c5736fb8e56693b3bcbf3ca18801f81457e
hikahana <106811268+hikahana@users.noreply.github.com>
2025-06-26 04:28:45 +0900

    Ensure groups delete associated unregistered records

--- api/app/models/group.rb
+++ api/app/models/group.rb
@@ -16,6 +16,7 @@ class Group < ApplicationRecord
     has_one :venue_map
     has_one :announcement
     has_one :cooking_process_order
+    has_many :un_registered_groups, dependent: :destroy
     has_many :fire_equipment_orders
 
     ### group_category (参加団体カテゴリ)
