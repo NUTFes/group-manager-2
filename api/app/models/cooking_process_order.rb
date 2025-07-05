ecb3ba9a578a1304c470d4fe3c331da736b09afc
i2 <95607264+YosukeIida@users.noreply.github.com>
2025-07-05 21:51:29 +0900

    feat: food_productとの関連を追加し、関連情報を取得するメソッドを実装

--- api/app/models/cooking_process_order.rb
+++ api/app/models/cooking_process_order.rb
@@ -1,6 +1,7 @@
 class CookingProcessOrder < ApplicationRecord
   # group_id という名前の外部キーを持つ group への belongs_to 関連
   belongs_to :group
+  belongs_to :food_product
 
   # 全ての CookingProcessOrder レコードとそれらの group をプリロードしてハッシュで返すクラスメソッド
   def self.with_groups
@@ -19,6 +20,17 @@ class CookingProcessOrder < ApplicationRecord
     {
       "cooking_process_order": cooking_process_order,
       "group": cooking_process_order.group
+    }
+  end
+
+  # food_product_id を受け取り、関連する調理工程、団体、販売品情報をハッシュで返す
+  def self.with_group_by_food_product_id(food_product_id)
+    food_product = FoodProduct.find(food_product_id)
+    cooking_process_order = food_product.cooking_process_order
+    {
+      "cooking_process_order": cooking_process_order,
+      "group": food_product.group,
+      "food_product": food_product
     }
   end
 
