168c70ceceaa19b732ee27cc60300705126f88c1
hikahana <22.h.hanada.nutfes@gmail.com>
2025-12-30 03:45:14 +0900

    [feat] Footerコンポーネントにi18nextを使用した翻訳機能を追加し、著作権表示をローカライズ。Headerコンポーネントに言語切り替え機能を追加。

--- user/src/components/Footer/Footer.tsx
+++ user/src/components/Footer/Footer.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
+import { useTranslation } from 'next-i18next';
 
 const Footer: FC = () => {
   const currentYear = new Date().getFullYear();
+  const { t } = useTranslation('common');
 
   return (
     <footer className="flex h-20 w-full items-center justify-center bg-green-600 px-8 text-xs font-light text-white">
-      Copyright © {currentYear} NUTMEG. All Rights Reserved.
+      {t('footer.copyright', { year: currentYear })}
     </footer>
   );
 };
