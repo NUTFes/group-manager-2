3c4d1afbb4f5d72f2d100a3ded5e50e7c03ce078
hikahana <22.h.hanada.nutfes@gmail.com>
2026-01-01 12:59:05 +0900

    [feat] LogoutButtonコンポーネントにi18nextを使用した翻訳機能を追加し、ボタンのテキストをローカライズ

--- user/src/components/LogoutButton/LogoutButton.tsx
+++ user/src/components/LogoutButton/LogoutButton.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { useTranslation } from 'next-i18next';
 import { MdOutlineLogout } from 'react-icons/md';
 
 type LogoutButtonProps = {
@@ -6,10 +7,12 @@ type LogoutButtonProps = {
 };
 
 const LogoutButton: FC<LogoutButtonProps> = ({ onClick }) => {
+  const { t } = useTranslation('common');
+
   return (
     <button className="flex w-32 gap-3" onClick={onClick}>
       <MdOutlineLogout color="#000000" size="24" />
-      <p className="text-base font-medium text-font">ログアウト</p>
+      <p className="text-base font-medium text-font">{t('auth.logout')}</p>
     </button>
   );
 };
