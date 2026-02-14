f0e6f873839f5a6ddccd5b89700ed1aa0cdaa6cb
hikahana <22.h.hanada.nutfes@gmail.com>
2026-02-14 19:09:28 +0900

    feat: implement translation support for food products and cooking process orders

--- api/app/controllers/api/v1/cooking_process_orders_api_controller.rb
+++ api/app/controllers/api/v1/cooking_process_orders_api_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::CookingProcessOrdersApiController < ApplicationController
     if @food_products.empty?
       render json: fmt(not_found, [], 'Not found food products')
     else
-      render json: fmt(ok, @food_products.as_json(include: %i[group cooking_process_order]))
+      render json: fmt(ok, build_food_products_response_with_translations(@food_products))
     end
   end
 
@@ -33,13 +33,47 @@ class Api::V1::CookingProcessOrdersApiController < ApplicationController
     if @food_products.empty?
       render json: fmt(not_found, [], 'Not found food products')
     else
-      render json: fmt(ok, @food_products.as_json(include: %i[group cooking_process_order]))
+      render json: fmt(ok, build_food_products_response_with_translations(@food_products))
     end
   end
 
   # GET /api/v1/get_cooking_process_order_by_food_product_id/:food_product_id - 販売品IDに紐づく調理工程申請を取得
   def get_cooking_process_order_by_food_product_id
     @cooking_process_order = CookingProcessOrder.with_group_by_food_product_id(params[:food_product_id])
-    render json: fmt(ok, @cooking_process_order)
+    render json: fmt(ok, build_cooking_process_order_response_with_translations(@cooking_process_order))
+  end
+
+  private
+
+  def  build_food_products_response_with_translations(food_products)
+    food_products.map do |food_product|
+      json = food_product.as_json(include: %i[group cooking_process_order])
+      json['translated_name'] = translate_to_ja(food_product.name)
+
+      next json if food_product.cooking_process_order.blank?
+
+      json['cooking_process_order']['translated_tent'] = translate_to_ja(food_product.cooking_process_order.tent)
+      json
+    end
+  end
+
+  def build_cooking_process_order_response_with_translations(cooking_process_order)
+    return cooking_process_order if cooking_process_order.blank?
+
+    response = cooking_process_order.deep_dup
+    food_product = response[:food_product]
+    order = response[:cooking_process_order]
+
+    response[:food_product] = food_product.as_json.merge(
+      'translated_name' => translate_to_ja(food_product.name)
+    )
+
+    if order.present?
+      response[:cooking_process_order] = order.as_json.merge(
+        'translated_tent' => translate_to_ja(order.tent)
+      )
+    end
+
+    response
   end
 end
