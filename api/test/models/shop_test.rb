f3607f3805b640485cfa02967d303edadad3559b
batcho0428 <25.t.inaba.nutfes@gmail.com>
2026-05-11 15:56:55 +0900

    [fix] code rabbitからの指摘の修正

--- api/test/models/shop_test.rb
+++ api/test/models/shop_test.rb
@@ -26,4 +26,16 @@ class ShopTest < ActiveSupport::TestCase
       assert_equal 1, Shop.next_regular_id
     end
   end
+
+  test 'next_regular_id raises when all regular ids are exhausted' do
+    relation = Object.new
+
+    def relation.pluck(*)
+      (1..999).to_a
+    end
+
+    Shop.stub(:where, relation) do
+      assert_raises(ActiveRecord::RecordNotUnique) { Shop.next_regular_id }
+    end
+  end
 end
