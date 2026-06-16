bfd4fe5e329b2c27c662624f9840c52b9f027870
unsigned <naotoanzai50@gmail.com>
2026-06-16 17:00:58 +0900

    chage File Name validation (_id)

--- admin_view/nuxt-project/pages/venue_maps/_id.vue
+++ admin_view/nuxt-project/pages/venue_maps/_id.vue
@@ -160,7 +160,7 @@ export default {
         const validFileName = ["png", "jpeg", "jpg"];
         const fileName = file.name.split(".").pop().toLowerCase();
         this.isInvalidFile = !validFileName.includes(fileName);
-        const fileNameRegex = /^[^\\/:*?"<>|\r\n]+_[^\\/:*?"<>|\r\n]+$/;
+        const fileNameRegex = /^[^\\/:*?"<>|\r\n]+$/;
         const fileSizeLimit = 20 * 1024 * 1024; // 20MB
         this.isFileSizeCheck = file.size > fileSizeLimit;
         
@@ -171,16 +171,16 @@ export default {
           );
           this.isFileSizeCheck = true;
         // ファイル形式のバリデーション
-        if (this.isInvalidFile) {
+        } else if (this.isInvalidFile) {
           this.openSnackBar(
             "ファイル形式は[.pngか.jpeg又は.jpg]にしてください。"
           );
           this.isInvalidFile = true;
           return;
-          // ファイル名のチェック。"_"で区切られているかどうかのチェック
+          // ファイル名のバリデーション。「^\/:*?"<>|\r\n」が含まれているかどうかのチェック
         } else if (!fileNameRegex.test(file.name)) {
           this.openSnackBar(
-            "ファイル名は「参加形式_団体名」の形式で入力してください"
+            "ファイル名は「^[^\\/:*?\"<>|\r\n]+$」を含まない形式で入力してください"
           );
           this.isFileCheck = true;
           return;
