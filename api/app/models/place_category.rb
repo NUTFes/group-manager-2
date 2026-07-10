b5b63721564a5764a257168a53738f5f6ca55e20
batcho0428 <25.t.inaba.nutfes@gmail.com>
2026-07-10 14:49:51 +0900

    refactor: simplify hierarchy_for_index logic and remove redundant hierarchy_ordered method

--- api/app/models/place_category.rb
+++ api/app/models/place_category.rb
@@ -9,68 +9,59 @@ class PlaceCategory < ApplicationRecord
   validate :parent_cannot_be_self_or_children
   validate :parent_id_must_be_nil_or_integer
 
-  def self.hierarchy_ordered
+
+  def self.hierarchy_for_index
     categories = order(:id).to_a
+    categories_by_id = categories.index_by(&:id)
     children_by_parent_id = categories.group_by(&:parent_id)
+    stocker_places_counts = StockerPlace
+                            .where(place_category_id: categories.map(&:id))
+                            .group(:place_category_id)
+                            .count
+
+    descendant_ids_by_id = {}
+
+    build_descendant_ids = lambda do |category|
+      descendant_ids_by_id[category.id] ||=
+      children_by_parent_id[category.id].to_a.flat_map do |child|
+        [child.id] + build_descendant_ids.call(child)
+      end
+    end
+
+    build_formatted_name = lambda do |category|
+      names = []
+      current = category
+
+      while current
+        names.unshift(current.name)
+        current = categories_by_id[current.parent_id]
+      end
+
+      names.join(' / ')
+    end
+
     ordered_categories = []
 
     append_children = lambda do |parent_id|
-      children = children_by_parent_id[parent_id].to_a.sort_by(&:name)
-      children.each do |category|
+      children_by_parent_id[parent_id].to_a.sort_by(&:name).each do |category|
         ordered_categories << category
         append_children.call(category.id)
       end
     end
 
     append_children.call(nil)
-    ordered_categories
-  end
 
-  def self.hierarchy_for_index
-    categories = order(:id).to_a
-    categories_by_id = categories.index_by(&:id)
-    children_by_parent_id = categories.group_by(&:parent_id)
+    ordered_categories.map do |category|
+      descendant_ids = build_descendant_ids.call(category)
 
-    # Calculate descendant_ids for each category without N+1 queries
-    descendants_map = {}
-    calc_descendants = lambda do |cat_id|
-      return descendants_map[cat_id] if descendants_map.key?(cat_id)
-
-      child_ids = children_by_parent_id[cat_id]&.map(&:id) || []
-      all_descendants = child_ids.dup
-      child_ids.each do |child_id|
-        all_descendants.concat(calc_descendants.call(child_id))
-      end
-      
-      descendants_map[cat_id] = all_descendants
+      category.attributes.merge(
+        'formatted_name' => build_formatted_name.call(category),
+        'parent_name' => categories_by_id[category.parent_id]&.name || '未指定',
+        'children_count' => descendant_ids.size,
+        'descendant_ids' => descendant_ids,
+        'stocker_places_count' => stocker_places_counts[category.id] || 0
+      )
     end
-    categories.each { |c| calc_descendants.call(c.id) }
-
-    # Batch retrieve stocker_places counts
-    stocker_places_counts = StockerPlace.group(:place_category_id).count
-
-    ordered_categories = []
-    
-    append_children = lambda do |parent_id, ancestor_names|
-      children = children_by_parent_id[parent_id].to_a.sort_by(&:name)
-      children.each do |category|
-        current_ancestor_names = ancestor_names + [category.name]
-        parent_name = category.parent_id ? (categories_by_id[category.parent_id]&.name || '未指定') : '未指定'
-        desc_ids = descendants_map[category.id]
-        
-        ordered_categories << category.as_json.merge({
-          'formatted_name' => current_ancestor_names.join(' / '),
-          'parent_name' => parent_name,
-          'children_count' => desc_ids.size,
-          'descendant_ids' => desc_ids,
-          'stocker_places_count' => stocker_places_counts[category.id] || 0
-        })
-        append_children.call(category.id, current_ancestor_names)
-      end
-    end
-
-    append_children.call(nil, [])
-    ordered_categories
   end
 
   def formatted_name
