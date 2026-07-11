e72b61f6545ea188427d06683a40e73a64a2fbad
unsigned <naotoanzai50@gmail.com>
2026-07-11 17:25:52 +0900

    chore: 未使用部分の削除

--- admin_view/nuxt-project/utils/imgur_upload.js
+++ admin_view/nuxt-project/utils/imgur_upload.js
@@ -1,20 +1,3 @@
-export const convertImageToDataUrl = (file) => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      if (event.target?.result) {
-        resolve(event.target.result);
-      } else {
-        reject(new Error("Failed to convert image to Data URL"));
-      }
-    };
-    reader.onerror = (event) => {
-      reject(event);
-    };
-    reader.readAsDataURL(file);
-  });
-};
-
 export const uploadImageToImgur = async (
   file,
   imgurClientId,
