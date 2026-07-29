bf8f829903328fbd4aab06658cba540649d32c63
hikahana <22.h.hanada.nutfes@gmail.com>
2026-07-30 03:53:58 +0900

    fix: Railsを7.2.3.2へ更新

--- api/config/initializers/devise_token_auth.rb
+++ api/config/initializers/devise_token_auth.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 DeviseTokenAuth.setup do |config|
+  # API-only mode has no session middleware. Use Warden without persisting the
+  # authenticated resource in a session.
+  config.bypass_sign_in = false
+
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
@@ -42,11 +46,12 @@ DeviseTokenAuth.setup do |config|
   # config.default_callbacks = true
 
   # Makes it possible to change the headers names
-  config.headers_names = {:'access-token' => 'access-token',
-                         :'client' => 'client',
-                         :'expiry' => 'expiry',
-                         :'uid' => 'uid',
-                         :'token-type' => 'token-type' }
+  config.headers_names = { authorization: 'Authorization',
+                           'access-token': 'access-token',
+                           client: 'client',
+                           expiry: 'expiry',
+                           uid: 'uid',
+                           'token-type': 'token-type' }
 
   # By default, only Bearer Token authentication is implemented out of the box.
   # If, however, you wish to integrate with legacy Devise authentication, you can
