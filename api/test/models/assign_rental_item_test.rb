fb4784803669cc0b417f06cbbb502acb935b1515
unsigned <naotoanzai50@gmail.com>
2026-08-07 15:39:10 +0900

    test: 在庫場所・貸出場所の名称取得のテストを追加

--- api/test/models/assign_rental_item_test.rb
+++ api/test/models/assign_rental_item_test.rb
@@ -43,4 +43,52 @@ class AssignRentalItemTest < ActiveSupport::TestCase
     assert_not assign_rental_item.valid?
     assert assign_rental_item.errors[:rental_place].present?
   end
+
+  # 書類出力用の場所名
+  test 'stock_place_name and rental_place_name return place names' do
+    assign_rental_item = AssignRentalItem.new(
+      group: @group,
+      rental_item: @rental_item,
+      num: 1,
+      stocker_place: stocker_places(:with_name_en),
+      rental_place: stocker_places(:without_name_en)
+    )
+    assert_equal '体育館倉庫', assign_rental_item.stock_place_name
+    assert_equal '第1体育館前', assign_rental_item.rental_place_name
+  end
+
+  test 'place names use english with fallback to japanese' do
+    assign_rental_item = AssignRentalItem.new(
+      group: @group,
+      rental_item: @rental_item,
+      num: 1,
+      stocker_place: stocker_places(:with_name_en),
+      rental_place: stocker_places(:without_name_en)
+    )
+    assert_equal 'Gymnasium Storage', assign_rental_item.stock_place_name(use_english: true)
+    # 英語名が未登録なので日本語名にフォールバックする
+    assert_equal '第1体育館前', assign_rental_item.rental_place_name(use_english: true)
+  end
+
+  test 'rental_place_name is blank when rental place is not assigned' do
+    assign_rental_item = AssignRentalItem.new(
+      group: @group,
+      rental_item: @rental_item,
+      num: 1,
+      stocker_place: stocker_places(:with_name_en),
+      rental_place: nil
+    )
+    assert_equal '', assign_rental_item.rental_place_name
+    assert_equal '', assign_rental_item.rental_place_name(use_english: true)
+  end
+
+  test 'stock_place_name is blank when stocker place is not assigned' do
+    assign_rental_item = AssignRentalItem.new(
+      group: @group,
+      rental_item: @rental_item,
+      num: 1,
+      stocker_place: nil
+    )
+    assert_equal '', assign_rental_item.stock_place_name
+  end
 end
--- api/test/models/stocker_place_test.rb
+++ api/test/models/stocker_place_test.rb
@@ -3,7 +3,26 @@
 require 'test_helper'
 
 class StockerPlaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :stocker_places
+
+  test 'display_name returns japanese name by default' do
+    place = stocker_places(:with_name_en)
+    assert_equal '体育館倉庫', place.display_name
+  end
+
+  test 'display_name returns english name when use_english is true' do
+    place = stocker_places(:with_name_en)
+    assert_equal 'Gymnasium Storage', place.display_name(use_english: true)
+  end
+
+  test 'display_name falls back to japanese name when name_en is blank' do
+    place = stocker_places(:without_name_en)
+    assert_equal '第1体育館前', place.display_name(use_english: true)
+  end
+
+  test 'display_name returns empty string when name is nil' do
+    place = StockerPlace.new(name: nil)
+    assert_equal '', place.display_name
+    assert_equal '', place.display_name(use_english: true)
+  end
 end
