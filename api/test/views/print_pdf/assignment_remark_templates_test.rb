a8601d9a7f97d36b3f456c5cf16fbbfb5f0c0379
hikahana <22.h.hanada.nutfes@gmail.com>
2026-08-25 01:21:24 +0900

    test: 団体情報PDFの物品列幅を検証

--- api/test/views/print_pdf/assignment_remark_templates_test.rb
+++ api/test/views/print_pdf/assignment_remark_templates_test.rb
@@ -14,7 +14,17 @@ class AssignmentRemarkTemplatesTest < ActiveSupport::TestCase
   test 'all groups info adds a remark heading and assignment value' do
     template = Rails.root.join('app/views/print_pdf/output_all_groups_info.pdf.erb').read
 
+    assert_includes template, '<table class="items all-groups-info-items">'
+    assert_includes template, '<col class="remark-column">'
     assert_includes template, '<th>備考</th>'
     assert_includes template, '<td><%= item.remark %></td>'
+  end
+
+  test 'all groups info rental items columns have widths totaling one hundred percent' do
+    stylesheet = Rails.root.join('app/views/print_pdf/output_rental_items_pdf.css').read
+    widths = stylesheet.scan(/\.all-groups-info-items .+? \{\s+width: ([\d.]+)%;\s+\}/).flatten.map(&:to_f)
+
+    assert_equal 4, widths.length
+    assert_in_delta 100, widths.sum
   end
 end
