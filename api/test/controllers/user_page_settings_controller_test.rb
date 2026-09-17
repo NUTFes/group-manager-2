c7f8c5d13e7512ba3bbacdbb2044d3ef0d9a20cf
timdaik <hirokix0608@gmail.com>
2026-09-18 01:39:33 +0900

    feat: #2125 設定更新APIの成否を正しく返す
    部分更新の成功を200、検証失敗を422、対象なしを404で返す。OpenAPIと対応するテストを更新する。

--- api/test/controllers/user_page_settings_controller_test.rb
+++ api/test/controllers/user_page_settings_controller_test.rb
@@ -14,10 +14,10 @@ class UserPageSettingsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create user_page_setting' do
     assert_difference('UserPageSetting.count') do
-      post user_page_settings_url, params: { user_page_setting: { is_edit_employee: @user_page_setting.is_edit_employee, is_edit_food_product: @user_page_setting.is_edit_food_product, is_edit_group: @user_page_setting.is_edit_group, is_edit_place: @user_page_setting.is_edit_place, is_edit_power_order: @user_page_setting.is_edit_power_order, is_edit_purchase_list: @user_page_setting.is_edit_purchase_list, is_edit_rental_order: @user_page_setting.is_edit_rental_order, is_edit_stage_order: @user_page_setting.is_edit_stage_order, is_edit_sub_rep: @user_page_setting.is_edit_sub_rep, is_regist_food_product: @user_page_setting.is_regist_food_product, is_regist_group: @user_page_setting.is_regist_group } }, as: :json
+      post user_page_settings_url, params: { fes_year_id: fes_years(:one).id, is_edit_employee: @user_page_setting.is_edit_employee, is_edit_food_product: @user_page_setting.is_edit_food_product, is_edit_group: @user_page_setting.is_edit_group, is_edit_place: @user_page_setting.is_edit_place, is_edit_power_order: @user_page_setting.is_edit_power_order, is_edit_purchase_list: @user_page_setting.is_edit_purchase_list, is_edit_rental_order: @user_page_setting.is_edit_rental_order, is_edit_stage_order: @user_page_setting.is_edit_stage_order, is_edit_sub_rep: @user_page_setting.is_edit_sub_rep, is_regist_food_product: @user_page_setting.is_regist_food_product, is_regist_group: @user_page_setting.is_regist_group }, as: :json
     end
 
-    assert_response :created
+    assert_response :ok
   end
 
   test 'should show user_page_setting' do
@@ -26,8 +26,36 @@ class UserPageSettingsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update user_page_setting' do
-    patch user_page_setting_url(@user_page_setting), params: { user_page_setting: { is_edit_employee: @user_page_setting.is_edit_employee, is_edit_food_product: @user_page_setting.is_edit_food_product, is_edit_group: @user_page_setting.is_edit_group, is_edit_place: @user_page_setting.is_edit_place, is_edit_power_order: @user_page_setting.is_edit_power_order, is_edit_purchase_list: @user_page_setting.is_edit_purchase_list, is_edit_rental_order: @user_page_setting.is_edit_rental_order, is_edit_stage_order: @user_page_setting.is_edit_stage_order, is_edit_sub_rep: @user_page_setting.is_edit_sub_rep, is_regist_food_product: @user_page_setting.is_regist_food_product, is_regist_group: @user_page_setting.is_regist_group } }, as: :json
+    patch user_page_setting_url(@user_page_setting), params: { is_edit_employee: true }, as: :json
     assert_response :ok
+    assert_equal 200, response.parsed_body.dig('status', 'code')
+    assert @user_page_setting.reload.is_edit_employee
+    assert_not @user_page_setting.is_edit_food_product
+  end
+
+  test 'should update festival year without changing toggle flags' do
+    patch user_page_setting_url(@user_page_setting), params: { fes_year_id: fes_years(:two).id }, as: :json
+
+    assert_response :ok
+    assert_equal fes_years(:two).id, @user_page_setting.reload.fes_year_id
+    assert_not @user_page_setting.is_edit_employee
+  end
+
+  test 'should reject an invalid festival year without changing the setting' do
+    previous_year_id = @user_page_setting.fes_year_id
+
+    patch user_page_setting_url(@user_page_setting), params: { fes_year_id: FesYear.maximum(:id) + 1 }, as: :json
+
+    assert_response :unprocessable_entity
+    assert_equal 422, response.parsed_body.dig('status', 'code')
+    assert_equal previous_year_id, @user_page_setting.reload.fes_year_id
+  end
+
+  test 'should return not found for an unknown setting' do
+    patch user_page_setting_url(UserPageSetting.maximum(:id) + 1), params: { is_edit_employee: true }, as: :json
+
+    assert_response :not_found
+    assert_equal 404, response.parsed_body.dig('status', 'code')
   end
 
   test 'should destroy user_page_setting' do
@@ -35,6 +63,6 @@ class UserPageSettingsControllerTest < ActionDispatch::IntegrationTest
       delete user_page_setting_url(@user_page_setting), as: :json
     end
 
-    assert_response :no_content
+    assert_response :ok
   end
 end
